fd61b6b0e922a24c8f07ef6be467bf77981f673c
Johnathan Burchill <105376941+JohnathanBurchill@users.noreply.github.com>
2024-05-31 14:25:42 -0600

    Print list of available telecommands (#31)

--- firmware/Core/Inc/telecommands/telecommand_definitions.h
+++ firmware/Core/Inc/telecommands/telecommand_definitions.h
@@ -3,7 +3,6 @@
 #define __INCLUDE_GUARD__TELECOMMAND_DEFINITIONS_H
 
 #include <stdint.h>
-#include "telecommands/telecommand_definitions.h"
 
 typedef enum {
     TCMD_TelecommandChannel_DEBUG_UART,
@@ -40,6 +39,9 @@ uint8_t TCMDEXEC_echo_back_uint32_args(const uint8_t *args_str, TCMD_Telecommand
                         char *response_output_buf, uint16_t response_output_buf_len);
 
 uint8_t TCMDEXEC_run_all_unit_tests(const uint8_t *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len);
+
+uint8_t TCMDEXEC_available_telecommands(const uint8_t *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                         char *response_output_buf, uint16_t response_output_buf_len);
 
 #endif // __INCLUDE_GUARD__TELECOMMAND_DEFINITIONS_H
