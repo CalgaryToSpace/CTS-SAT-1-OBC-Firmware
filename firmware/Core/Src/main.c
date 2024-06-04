b7f7d58dd47d4e4d6d45f15a620d4911e5c0d537
Johnathan K Burchill <jkburchi@ucalgary.ca>
2024-06-04 15:13:53 -0600

    Ensure LFS init function is compiled

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -950,23 +950,6 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-
-
 void LFS_init_filesystem(void) {
 
   //LittleFS initialization (TODO: use INIT function)
@@ -1002,5 +985,23 @@ void LFS_init_filesystem(void) {
   return;
 
 }
+
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+
 
 #endif /* USE_FULL_ASSERT */
