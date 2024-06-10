b14ce94bebd3c98cfc9e4a6c2111dbb86816fb5d
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-06-09 19:25:07 -0600

    Initial Function to split string by delimeter, setup for configuration variables

--- firmware/Core/Src/helpers/helpers.c
+++ firmware/Core/Src/helpers/helpers.c
@@ -1,18 +1,45 @@
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 #include "helpers/helpers.h"
 
-uint8_t split_string_by_delimiter(const uint8_t *str, const uint8_t str_len, const uint8_t delim, uint8_t **result)
+uint8_t split_string_by_delimiter(const uint8_t *input_str, const uint8_t input_str_len, const uint8_t delim, uint8_t result[][20], const uint8_t max_segments)
 {
-    uint8_t i = 0;
-    uint8_t j = 0;
-    while (str[i] != '\0' && i < str_len)
+    if (input_str == NULL || result == NULL || max_segments == 0 || input_str_len == 0)
     {
-        if (str[i] == delim)
-        {
-            result[j] = &str[i + 1];
-            j++;
-        }
-        i++;
+        return 0;
     }
-    return 0;
+
+    uint8_t count = 0;
+    uint8_t start_of_word = 0;
+
+    for (uint8_t pos_in_str = 0; pos_in_str <= input_str_len; pos_in_str++)
+    {
+        if (pos_in_str != input_str_len && input_str[pos_in_str] != delim)
+        {
+            continue;
+        }
+
+        if (count >= max_segments)
+        {
+            break;
+        }
+
+        // copy over word
+        const uint8_t word_len = pos_in_str - start_of_word;
+        uint8_t copy[word_len + 1]; // account for null terminator
+        strncpy((char *)copy, (char *)(input_str + start_of_word), word_len);
+        copy[word_len] = '\0';
+
+        // add word to result
+        strncpy((char *)result[count], (char *)copy, word_len);
+
+        count++;
+
+        start_of_word = pos_in_str + 1;
+    }
+
+    return count;
 }
