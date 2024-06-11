a9f19410a3a01b4f53a81e384165dae6ac1a167c
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-06-10 19:24:29 -0600

    rename files, move them around, use char for null-terminated strings

--- firmware/Core/Inc/unit_tests/test_helpers.h
+++
@@ -1,6 +0,0 @@
-#ifndef __INCLUDE_GUARD__TEST__HELPERS__H__
-#define __INCLUDE_GUARD__TEST__HELPERS__H__
-#include <stdint.h>
-uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter();
-
-#endif
--- firmware/Core/Src/unit_tests/test_string_helpers.c
+++ firmware/Core/Src/unit_tests/test_string_helpers.c
@@ -1,22 +1,22 @@
 
-#include "helpers/helpers.h"
+#include "transforms/string_helpers.h"
 #include "unit_tests/unit_test_helpers.h"
 
 #include <stdint.h>
 #include <string.h>
 
 // Function which tests the split_string_by_delimiter function
-uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter()
+uint8_t TEST_EXEC__HELPER_split_string_by_delimiter()
 {
     // Test case 1: Normal case
     const uint8_t max_string_num = 4;
     const uint8_t max_string_len = 20;
-    uint8_t fruit_output[max_string_num][max_string_len];
+    char fruit_output[max_string_num][max_string_len];
     for (uint8_t i = 0; i < max_string_num; i++)
     {
         memset(fruit_output[i], 0, sizeof(fruit_output[i]));
     }
-    const uint8_t *fruits = (uint8_t *)"apple,banana,cherry,date\0";
+    const char *fruits = "apple,banana,cherry,date\0";
     const uint8_t num_strings_split = split_string_by_delimiter(fruits, strlen((char *)fruits), ',', fruit_output, max_string_num);
     TEST_ASSERT_TRUE(num_strings_split == 4);
     TEST_ASSERT_TRUE(strncmp((char *)fruit_output[0], "apple\0", 7) == 0);
@@ -27,12 +27,12 @@ uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter()
     // Test case 2: Empty string
     const char empty[] = "";
     TEST_ASSERT_TRUE(strlen(empty) == 0);
-    uint8_t empty_output[max_string_num][max_string_len];
+    char empty_output[max_string_num][max_string_len];
     for (uint8_t i = 0; i < max_string_num; i++)
     {
         memset(empty_output[i], 0, sizeof(empty_output[i]));
     }
-    const uint8_t num_strings_split_empty = split_string_by_delimiter((uint8_t *)empty, 0, ',', empty_output, 0);
+    const uint8_t num_strings_split_empty = split_string_by_delimiter(empty, 0, ',', empty_output, 0);
     TEST_ASSERT_TRUE(num_strings_split_empty == 0);
 
     for (uint8_t i = 0; i < max_string_num; i++)
@@ -44,8 +44,8 @@ uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter()
     }
 
     // Test case 3: Single element
-    const uint8_t *single = (uint8_t *)"single\0";
-    uint8_t single_output[max_string_num][max_string_len];
+    const char *single = "single\0";
+    char single_output[max_string_num][max_string_len];
     for (uint8_t i = 0; i < max_string_num; i++)
     {
         memset(single_output[i], 0, sizeof(single_output[i]));
@@ -55,8 +55,8 @@ uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter()
     TEST_ASSERT_TRUE(strncmp((char *)single_output[0], "single\0", 8) == 0);
 
     // Test case 4: Trailing comma
-    const uint8_t *trailing = (uint8_t *)"one,two,\0";
-    uint8_t trailing_output[max_string_num][max_string_len];
+    const char *trailing = "one,two,\0";
+    char trailing_output[max_string_num][max_string_len];
     for (uint8_t i = 0; i < max_string_num; i++)
     {
         memset(trailing_output[i], 0, sizeof(trailing_output[i]));
@@ -67,8 +67,8 @@ uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter()
     TEST_ASSERT_TRUE(strncmp((char *)trailing_output[0], "one\0", 5) == 0);
 
     // Test case 5: Leading comma
-    const uint8_t *leading = (uint8_t *)",one,two,three\0";
-    uint8_t leading_output[max_string_num][max_string_len];
+    const char *leading = ",one,two,three\0";
+    char leading_output[max_string_num][max_string_len];
     for (uint8_t i = 0; i < max_string_num; i++)
     {
         memset(leading_output[i], 0, sizeof(leading_output[i]));
@@ -81,8 +81,8 @@ uint8_t TEST_EXEC__HELPER_Split_String_By_Delimiter()
     TEST_ASSERT_TRUE(strncmp((char *)leading_output[2], "three\0", 7) == 0);
 
     // Test case 6: Multiple consecutive commas
-    const uint8_t *consecutive = (uint8_t *)"one,,two\0";
-    uint8_t consecutive_output[max_string_num][max_string_len];
+    const char *consecutive = "one,,two\0";
+    char consecutive_output[max_string_num][max_string_len];
     for (uint8_t i = 0; i < max_string_num; i++)
     {
         memset(consecutive_output[i], 0, sizeof(consecutive_output[i]));
