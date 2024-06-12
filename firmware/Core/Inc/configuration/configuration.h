29a4a54859eaff398f4985958066bf33ba50549c
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-06-11 19:16:12 -0600

    Update Configuration Variable Design

--- firmware/Core/Inc/configuration/configuration.h
+++ firmware/Core/Inc/configuration/configuration.h
@@ -3,15 +3,22 @@
 
 #include <stdint.h>
 
-typedef enum
+typedef struct
 {
-    CTS_Subsystem_EPS
-} CTS_Subsystems;
+    char *variable_name;
+    void *variable_pointer; // pointer to a uint8_t, uint16_t, etc.
+    uint8_t width_bytes;    // width of the config variable (e.g., 1 for uint8_t, 2 for uint16_t, etc.)
+} CONFIG_integer_config_entry_t;
 
 typedef struct
 {
-    void *var;
-    CTS_Subsystems subsystem;
-} Configuration_Variable;
+    char *variable_name;
+    char *variable_pointer;
+    uint8_t max_length; // max length, including the null terminator
+} CONFIG_string_config_entry_t;
+
+extern CONFIG_integer_config_entry_t CTS1_Integer_Configuration_Variables[];
+
+extern CONFIG_string_config_entry_t CTS1_String_Configuration_Variables[];
 
 #endif
