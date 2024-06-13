dcbb28742827b028dc024a820555d43a3dbad963
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-06-12 22:57:31 -0600

    IOC Update: Set UART5 baud, name, interrupt for EPS (#43)
    * IOC Update: Set UART5 baud, name, interrupt for EPS
    
    * Update IOC docs build

--- firmware/Core/Src/stm32l4xx_it.c
+++ firmware/Core/Src/stm32l4xx_it.c
@@ -58,6 +58,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern UART_HandleTypeDef hlpuart1;
+extern UART_HandleTypeDef huart5;
 extern TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN EV */
@@ -190,6 +191,20 @@ void TIM1_UP_TIM16_IRQHandler(void)
   /* USER CODE BEGIN TIM1_UP_TIM16_IRQn 1 */
 
   /* USER CODE END TIM1_UP_TIM16_IRQn 1 */
+}
+
+/**
+  * @brief This function handles UART5 global interrupt.
+  */
+void UART5_IRQHandler(void)
+{
+  /* USER CODE BEGIN UART5_IRQn 0 */
+
+  /* USER CODE END UART5_IRQn 0 */
+  HAL_UART_IRQHandler(&huart5);
+  /* USER CODE BEGIN UART5_IRQn 1 */
+
+  /* USER CODE END UART5_IRQn 1 */
 }
 
 /**
