1e8c41d15cc63f9a726ab0341669034d20c2cb8b
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-06-12 22:53:19 -0600

    Add eps_get_system_status_json command

--- firmware/Core/Inc/eps_drivers/eps_types_to_json.h
+++ firmware/Core/Inc/eps_drivers/eps_types_to_json.h
@@ -9,5 +9,7 @@ uint8_t EPS_vpid_eng_TO_json(const EPS_vpid_eng_t *data, char json_output_str[],
 uint8_t EPS_battery_pack_datatype_raw_TO_json(const EPS_battery_pack_datatype_raw_t *data, char json_output_str[], uint16_t json_output_str_len);
 uint8_t EPS_battery_pack_datatype_eng_TO_json(const EPS_battery_pack_datatype_eng_t *data, char json_output_str[], uint16_t json_output_str_len);
 
+uint8_t EPS_result_system_status_TO_json(const EPS_result_system_status_t *data, char json_output_str[], uint16_t json_output_str_len);
+
 
 #endif // __INCLUDE_GUARD__EPS_TYPES_TO_JSON_H__
