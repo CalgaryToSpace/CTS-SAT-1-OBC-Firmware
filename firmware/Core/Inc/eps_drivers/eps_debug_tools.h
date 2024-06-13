1f3dfb7b07e0af8368de0772387f5e8c74be7605
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-06-12 23:17:11 -0600

    Remove unused/unfinished packing tool

--- firmware/Core/Inc/eps_drivers/eps_debug_tools.h
+++ firmware/Core/Inc/eps_drivers/eps_debug_tools.h
@@ -6,6 +6,4 @@
 
 void EPS_debug_uart_print_system_status(EPS_result_system_status_t* system_status);
 
-void EPS_result_pdu_housekeeping_data_eng_to_json(const EPS_result_pdu_housekeeping_data_eng_t *data, char json_output_str[]);
-
 #endif /* __INCLUDE_GUARD__EPS_DEBUG_TOOLS_H__ */
