dc9ba10ee7f942a0367135871d7fb535a268fc24
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-06-12 22:53:19 -0600

    Add eps_get_system_status_json command

--- firmware/Core/Src/telecommands/eps_telecommands.c
+++ firmware/Core/Src/telecommands/eps_telecommands.c
@@ -1,5 +1,6 @@
 #include <eps_drivers/eps_commands.h>
 #include <eps_drivers/eps_types.h>
+#include <eps_drivers/eps_types_to_json.h>
 
 #include <telecommands/eps_telecommands.h>
 
@@ -12,13 +13,39 @@ uint8_t TCMDEXEC_eps_watchdog(const uint8_t *args_str, TCMD_TelecommandChannel_e
     
     const uint8_t result = EPS_CMD_watchdog();
 
-    if (result == 0) {
-        snprintf(response_output_buf, response_output_buf_len, "EPS watchdog triggered successfully.");
-        return 0;
-    } else {
+    if (result != 0) {
         snprintf(
             response_output_buf, response_output_buf_len,
             "EPS watchdog trigger failed (err %d)", result);
         return 1;
     }
+
+    snprintf(response_output_buf, response_output_buf_len, "EPS watchdog triggered successfully.");
+    return 0;
+}
+
+/// @brief Gets the EPS system status, and returns it as a JSON string.
+/// @return 0 on success, >0 on failure.
+uint8_t TCMDEXEC_eps_get_system_status_json(const uint8_t *args_str,
+                        TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len) {
+    
+    EPS_result_system_status_t status;
+    const uint8_t result = EPS_CMD_get_system_status(&status);
+
+    if (result != 0) {
+        snprintf(response_output_buf, response_output_buf_len,
+            "EPS get system status failed (err %d)", result);
+        return 1;
+    }
+
+    const uint8_t result_json = EPS_result_system_status_TO_json(
+        &status, response_output_buf, response_output_buf_len);
+
+    if (result_json != 0) {
+        snprintf(response_output_buf, response_output_buf_len,
+            "EPS get system status JSON failed (err %d)", result_json);
+        return 2;
+    }
+    return 0;
 }
