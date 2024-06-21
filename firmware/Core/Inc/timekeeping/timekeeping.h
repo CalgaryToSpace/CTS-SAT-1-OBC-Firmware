63db2e7888a95d5d1efdb1214fa5ccf046afd56d
Johnathan K Burchill <jkburchi@ucalgary.ca>
2024-06-21 14:21:31 -0600

    Uptime-based timekeeping system
    Provides functions for storing in a string the system time, synchronization source and time since last synchronization in a log-friendly format.
    
    Includes telecommands for reporting and setting the system time.

---
+++ firmware/Core/Inc/timekeeping/timekeeping.h
@@ -0,0 +1,24 @@
+#ifndef __INCLUDE_GUARD__TIMEKEEPING_H_
+#define __INCLUDE_GUARD__TIMEKEEPING_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define TIM_EPOCH_DECIMAL_STRING_LEN 14
+
+typedef enum TIM_SYNC_SOURCE {
+    TIM_SOURCE_NONE = 0,
+    TIM_SOURCE_GNSS,
+    TIM_SOURCE_TELECOMMAND,
+} TIM_sync_source_t;
+
+uint32_t TIM_get_current_system_uptime_ms(void);
+void TIM_set_current_unix_epoch_time_ms(uint64_t current_unix_epoch_time_ms, TIM_sync_source_t source);
+uint64_t TIM_get_current_unix_epoch_time_ms();
+
+void TIM_get_timestamp_string(char *log_str, size_t max_len); 
+void TIM_get_timestamp_string_datetime(char *log_str, size_t max_len);
+char TIM_synchronization_source_letter(TIM_sync_source_t source);
+void TIM_epoch_ms_to_decimal_string(char *str, size_t len);
+
+#endif // __INCLUDE_GUARD__TIMEKEEPING_H_
