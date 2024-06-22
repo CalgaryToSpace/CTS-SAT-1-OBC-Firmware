4c79cb228f78695184b35eafdb831f72e971b821
Saksham-P <Saksham-P@users.noreply.github.com>
2024-06-22 19:08:20 +0000

    deploy: 407a78fab48120e8c2c394dc5e0b5efa7602b2b0

--- search/defines_4.js
+++ search/defines_4.js
@@ -3,6 +3,9 @@ var searchData=
   ['flash_5fchip_5fblock_5fsize_5fbytes_0',['FLASH_CHIP_BLOCK_SIZE_BYTES',['../littlefs__helper_8c.html#aba360b6434c4dc5aee8820c295c78c4a',1,'littlefs_helper.c']]],
   ['flash_5fchip_5fpage_5fsize_5fbytes_1',['FLASH_CHIP_PAGE_SIZE_BYTES',['../littlefs__helper_8c.html#a2b42956c8eb850f8255575b7f7b015a4',1,'littlefs_helper.c']]],
   ['flash_5fchip_5fsize_5fbytes_2',['FLASH_CHIP_SIZE_BYTES',['../flash__driver_8h.html#a504e6d0f67ffa257475887ad97fa24e8',1,'flash_driver.h']]],
-  ['flash_5fnumber_5fof_5fflash_5fdevices_3',['FLASH_NUMBER_OF_FLASH_DEVICES',['../flash__driver_8h.html#acf3393113eabce578e1e3348d22fce37',1,'flash_driver.h']]],
-  ['flash_5ftimeout_5fms_4',['FLASH_TIMEOUT_MS',['../flash__driver_8c.html#a5e35814490c8f20d26577568f0355266',1,'flash_driver.c']]]
+  ['flash_5fhal_5ftimeout_5fms_3',['FLASH_HAL_TIMEOUT_MS',['../flash__driver_8c.html#a36c02677f7073c3faf8e96f8f5bada10',1,'flash_driver.c']]],
+  ['flash_5floop_5fregister_5fchange_5ftimeout_5fms_4',['FLASH_LOOP_REGISTER_CHANGE_TIMEOUT_MS',['../flash__driver_8c.html#a62c50c6d18c4d6c6018ced26ff0c9e66',1,'flash_driver.c']]],
+  ['flash_5floop_5fsector_5ferase_5ftimeout_5fms_5',['FLASH_LOOP_SECTOR_ERASE_TIMEOUT_MS',['../flash__driver_8c.html#ac72e9a983814feda90495f16c38d3b45',1,'flash_driver.c']]],
+  ['flash_5floop_5fwrite_5ftimeout_5fms_6',['FLASH_LOOP_WRITE_TIMEOUT_MS',['../flash__driver_8c.html#acfc3f681464146e2ea89db1bbb158d2d',1,'flash_driver.c']]],
+  ['flash_5fnumber_5fof_5fflash_5fdevices_7',['FLASH_NUMBER_OF_FLASH_DEVICES',['../flash__driver_8h.html#acf3393113eabce578e1e3348d22fce37',1,'flash_driver.h']]]
 ];
