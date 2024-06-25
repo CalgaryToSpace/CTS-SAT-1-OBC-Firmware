6b88a33ed8348ec2622d7fcd53a91c0e0dcd89c4
vaibhav kapoor <vaibhav.kapoor@ucalgary.ca>
2024-06-25 13:38:32 -0600

    Helper methods added, syntax and convention improvements
    Improved mpi telecommand send method return to mpi response completely instead of success / failure codes
    Helper method to parse hex inputs added (needs to be reviewed and tested still)

--- firmware/Core/Src/telecommands/mpi_telecommand_definitions.c
+++ firmware/Core/Src/telecommands/mpi_telecommand_definitions.c
@@ -12,30 +12,30 @@
 /// @brief Send a command to the MPI
 /// @param commandCode CommandCode to identify command being sent to the MPI
 /// @param commandParam Parameter part of the command being sent to the MPI
-/// @return 0 if successful, 1 if NULL input, 2 if invalid hex string, 3 if other
+/// @return 0 if successful, 1 if NULL input, 2 if invalid hex input, 3 error from MPI
 uint8_t TCMDEXEC_mpi_send_command_hex(const uint8_t *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                                       char *response_output_buf, uint16_t response_output_buf_len)
 {
     // Get the length of the input string
-    size_t arg_str_length = strlen((const char *)args_str);
+    size_t arg_str_len = strlen((const char *)args_str);
 
     // Invalid argument: NULL
     if (args_str == NULL)
     {
         snprintf(response_output_buf, response_output_buf_len, "No commands received. Sample MPI command: 0204, sets HV_INNER_DOME_SCAN_MODE(02) to sawtooth with beam tracking (04)");
-        return 1;
+        return 1; // Error code for null arg
     }
 
     // Invalid argument: Invalid hex length
-    if (arg_str_length % 2 != 0)
+    if (arg_str_len % 2 != 0)
     {
         snprintf(response_output_buf, response_output_buf_len, "Invalid hex string length. Must be even.\n");
-        return 2;
+        return 2; // Error code for invalid hex input
     }
 
     // Invalid argument: Invalid hex character
     char *endptr;
-    for (size_t i = 0; i < arg_str_length; i += 2)
+    for (size_t i = 0; i < arg_str_len; i += 2)
     {
         // Convert two characters at a time to ensure they form a valid hex byte
         char hex_byte[3] = {args_str[i], args_str[i + 1], '\0'};
@@ -43,17 +43,35 @@ uint8_t TCMDEXEC_mpi_send_command_hex(const uint8_t *args_str, TCMD_TelecommandC
         if (*endptr != '\0')
         {
             snprintf(response_output_buf, response_output_buf_len, "Invalid character in hex string: %s\n", hex_byte);
-            return 3; // Error code for invalid character
+            return 2; // Error code for invalid hex input
         }
     }
 
-    // Send command to MPI
-    uint8_t commandResponse = sendTelecommandHex((uint8_t *)args_str);
+    // Allocate space to receive incoming MPI response
+    size_t mpi_cmd_response_len = 50;
+    uint8_t mpi_cmd_response[mpi_cmd_response_len];        // Max possible size for an MPI command+parameters can be 7 bytes + 2^N bytes of variable payload. To account for a buffer zone, 50 bytes will be allocated
+    memset(mpi_cmd_response, 0, sizeof(mpi_cmd_response)); // Initialize all elements to 0
 
-    // Verify mpi response to command
-    if (commandResponse == 0)
+    // Send command to MPI
+    uint8_t cmd_response = sendTelecommandHex(args_str, arg_str_len, mpi_cmd_response, mpi_cmd_response_len);
+
+    // Verify successful echo response from the mpi
+    if (cmd_response != 0)
     {
-        snprintf(response_output_buf, response_output_buf_len, "MPI failed to execute command. MPI echoed: %u\n", commandResponse);
-        return 3;
+        snprintf(response_output_buf, response_output_buf_len, "MPI failed to execute command. MPI echoed response code: %u\n", cmd_response);
+
+        // Print MPI response buffer as HEX
+        snprintf(response_output_buf, response_output_buf_len, "MPI response buffer: ");
+        for (size_t i = 0; i < mpi_cmd_response_len; i++)
+        {
+            snprintf(response_output_buf, response_output_buf_len, "%02X", mpi_cmd_response[i]);
+        }
+
+        return 3; // Error code for mpi side error
+    }
+    // Successful MPI response
+    else
+    {
+        snprintf(response_output_buf, response_output_buf_len, "MPI successfully executed the command. MPI echoed response code: %u\n", cmd_response);
     }
 }
