679c3893a43ae4061cbb53d39bd81ae141c52769
parkerg-13 <parkerg@shaw.ca>
2024-06-27 15:22:58 -0600

    Added telecommand to scan inputed i2c bus devices
    - unsure why the prints inside the for loop aren't being seen in the terminal? When I debug it I see that code getting hit?

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -70,6 +70,11 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .tcmd_func = TCMDEXEC_available_telecommands,
         .number_of_args = 0,
     },
+    {
+        .tcmd_name = "scan_i2c_device",
+        .tcmd_func = TCMDEXEC_scan_i2c_device,
+        .number_of_args = 1,
+    },
 
     // ****************** SECTION: flash_telecommand_defs ******************
     {
@@ -231,6 +236,54 @@ uint8_t TCMDEXEC_available_telecommands(const char *args_str, TCMD_TelecommandCh
         }
     }
     snprintf(response_output_buf, response_output_buf_len, "%s", response);
+
+    return 0;
+}
+
+uint8_t TCMDEXEC_scan_i2c_device(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len) {
+
+    snprintf(response_output_buf, response_output_buf_len, "Starting I2C scan...\n");
+
+    I2C_HandleTypeDef* hi2c;
+    int bus_to_scan = atoi(args_str);
+
+    // Figure out which bus we want to scan
+    switch(bus_to_scan){
+        case 1:
+            hi2c = &hi2c1;
+            break;
+        case 2:
+            hi2c = &hi2c2;
+            break;
+        case 3: 
+            hi2c = &hi2c3;
+            break;
+        case 4:
+            hi2c = &hi2c4;
+            break;
+        default:
+            hi2c = NULL;
+            snprintf(response_output_buf, response_output_buf_len, "Enter a valid I2C bus!\n");
+            return 0;
+    }
+
+
+	char msg[5];
+	// Go through all possible i2c addresses
+	for (uint8_t i = 0; i < 128; i++) {
+
+		if (HAL_I2C_IsDeviceReady(hi2c, (uint16_t)(i<<1), 3, 5) == HAL_OK) {
+			// We got an ack
+			sprintf(msg, "%2x ", i);
+			snprintf(response_output_buf, response_output_buf_len, "%s", msg);
+		} else {
+			snprintf(response_output_buf, response_output_buf_len, "-- ");
+		}
+
+	    if (i > 0 && (i + 1) % 16 == 0) snprintf(response_output_buf, response_output_buf_len, "\n");
+
+	}
 
     return 0;
 }
