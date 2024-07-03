e5187cee1a139ecc80aa6e40ef58fd0346dcbdff
DeemDeem52 <42725035+DeemDeem52@users.noreply.github.com>
2024-07-02 18:50:12 -0600

    minor type change to remove pointer signedness warnings
    uint8_t from telecommands was different from char in the helper functions

--- firmware/Core/Inc/telecommands/telecommand_args_helpers.h
+++ firmware/Core/Inc/telecommands/telecommand_args_helpers.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-uint8_t TCMD_ascii_to_uint64(const char *str, uint32_t str_len, uint64_t *result);
-uint8_t TCMD_extract_uint64_arg(const char *str, uint32_t str_len, uint8_t arg_index, uint64_t *result);
+uint8_t TCMD_ascii_to_uint64(const uint8_t *str, uint32_t str_len, uint64_t *result);
+uint8_t TCMD_extract_uint64_arg(const uint8_t *str, uint32_t str_len, uint8_t arg_index, uint64_t *result);
 
 #endif // __INCLUDE_GUARD__TELECOMMAND_ARGS_HELPERS_H__
