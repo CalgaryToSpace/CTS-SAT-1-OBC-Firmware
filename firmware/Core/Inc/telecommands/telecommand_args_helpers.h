e1838d547aff28bd43530a32baf51e7d473b7fe6
DeemDeem52 <42725035+DeemDeem52@users.noreply.github.com>
2024-07-02 19:03:41 -0600

    Add telecommand ASCII to double functions

--- firmware/Core/Inc/telecommands/telecommand_args_helpers.h
+++ firmware/Core/Inc/telecommands/telecommand_args_helpers.h
@@ -7,4 +7,7 @@ uint8_t TCMD_ascii_to_uint64(const uint8_t *str, uint32_t str_len, uint64_t *res
 uint8_t TCMD_extract_uint64_arg(const uint8_t *str, uint32_t str_len, uint8_t arg_index, uint64_t *result);
 uint8_t TCMD_extract_string_arg(const char *str, uint8_t arg_index, char *result, uint16_t result_max_len);
 
+uint8_t TCMD_ascii_to_double(const char *str, uint32_t str_len, double *result);
+uint8_t TCMD_extract_double_arg(const char *str, uint32_t str_len, uint8_t arg_index, double *result);
+
 #endif // __INCLUDE_GUARD__TELECOMMAND_ARGS_HELPERS_H__
