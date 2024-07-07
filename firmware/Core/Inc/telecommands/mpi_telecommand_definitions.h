bb4c204ae4d5de3833aeed41f855eeef268ab6b8
vaibhav kapoor <vaibhav.kapoor@ucalgary.ca>
2024-07-06 19:20:16 -0600

    Resolved syntax build errors after rebase with main

--- firmware/Core/Inc/telecommands/mpi_telecommand_definitions.h
+++ firmware/Core/Inc/telecommands/mpi_telecommand_definitions.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "telecommands/telecommand_definitions.h"
 
-uint8_t TCMDEXEC_mpi_send_command_hex(const uint8_t *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+uint8_t TCMDEXEC_mpi_send_command_hex(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                                       char *response_output_buf, uint16_t response_output_buf_len);
 
 #endif /* __INCLUDE_GUARD__MPI_TELECOMMAND_DEFINITIONS_H__ */
