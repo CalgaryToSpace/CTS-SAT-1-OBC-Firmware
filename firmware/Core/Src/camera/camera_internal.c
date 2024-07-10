ef26b1766e87a7a1fcae1a00c0df38461370f0c3
MrBear101 <64572053+MrBear101@users.noreply.github.com>
2024-07-09 19:49:59 -0600

    copied and cleaned some code
    Need to adjust buffers and add littleFS implementation still

--- firmware/Core/Src/camera/camera_internal.c
+++ firmware/Core/Src/camera/camera_internal.c
@@ -59,7 +59,7 @@ enum Capture_Status{Transmit_Success, Wrong_input};
   	  			  HAL_UART_Transmit(&huart4, (uint8_t*)"s", 1, HAL_MAX_DELAY);
   	  			  HAL_Delay(25);
   	  		  }
-  	  rec2();
+  	  capture_Image();
   	  return Transmit_Success;
   	  default:
   		  return Wrong_input;
@@ -67,3 +67,19 @@ enum Capture_Status{Transmit_Success, Wrong_input};
 
 
     }
+
+// needs to be updated with littleFS
+void capture_Image(){
+	  uint8_t sen[67*300]  = {'\0'};
+	  uint8_t sen1[67*300] = {'\0'};
+	  uint8_t sen2[67*300] = {'\0'};
+	  uint8_t sen3[67*300] = {'\0'};
+		  HAL_UART_Receive(&huart4, sen, 67*300, 10000);
+		  HAL_UART_Receive(&huart4, sen1, 67*300, 10000);
+		  HAL_UART_Receive(&huart4, sen2, 67*300, 10000);
+		  HAL_UART_Receive(&huart4, sen3, 67*300, 10000);
+		//   HAL_UART_Transmit(&hlpuart1, (uint8_t*)sen, 67*300, 10000);
+		//   HAL_UART_Transmit(&hlpuart1, (uint8_t*)sen1, 67*300, 10000);
+		//   HAL_UART_Transmit(&hlpuart1, (uint8_t*)sen2, 67*300, 10000);
+		//   HAL_UART_Transmit(&hlpuart1, (uint8_t*)sen3, 67*300, 10000);
+  }
