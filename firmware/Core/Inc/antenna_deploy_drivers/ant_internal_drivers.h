d864145fc0567122bb982f92d68e9f798bfd56ed
Kale <kale.fordham@ucalgary.ca>
2024-07-13 14:13:55 -0600

    Created source files for antenna deploy system
    Initial creation of source files for antenna deploy system. Creation of some function definitions.

---
+++ firmware/Core/Inc/antenna_deploy_drivers/ant_internal_drivers.h
@@ -0,0 +1,12 @@
+#ifndef __INCLUDE_GUARD_ANT_INTERNAL_DRIVERS_H__
+#define __INCLUDE_GUARD_ANT_INTERNAL_DRIVERS_H__
+
+#include <stdint.h>
+
+
+uint8_t ANT_send_cmd_get_response(
+    const uint8_t cmd_buf[], uint8_t cmd_len,
+    uint8_t rx_buf[], uint16_t rx_len
+    );
+
+#endif /* __INCLUDE_GUARD_ANT_INTERNAL_DRIVERS_H__ */
