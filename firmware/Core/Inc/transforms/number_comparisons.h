59a3b97e290939118cbdc7c0b1681a4bbfef46bf
parker-research <166864283+parker-research@users.noreply.github.com>
2024-07-13 14:50:28 -0600

    Fix docstrings

--- firmware/Core/Inc/transforms/number_comparisons.h
+++ firmware/Core/Inc/transforms/number_comparisons.h
@@ -3,24 +3,8 @@
 
 #include <stdint.h>
 
-/*
-compare_doubles
-REQUIRES:
-    - a, b, epsilon are all doubles
-PROMISES:
-    - returns 1 if a and b are approximately equal to each other (within epsilon)
-    - returns 0 otherwise
-*/
-uint8_t compare_doubles(double a, double b, double epsilon);
+uint8_t GEN_compare_doubles(double a, double b, double epsilon);
 
-/*
-compare_floats
-REQUIRES:
-    - a, b, epsilon are all floats
-PROMISES:
-    - returns 1 if a and b are approximately equal to each other (within epsilon)
-    - returns 0 otherwise
-*/
-uint8_t compare_floats(float a, float b, float epsilon);
+uint8_t GEN_compare_floats(float a, float b, float epsilon);
 
 #endif // __INCLUDE_GUARD__NUMBER_COMPARISONS_H__
--- firmware/Core/Src/transforms/number_comparisons.c
+++ firmware/Core/Src/transforms/number_comparisons.c
@@ -1,28 +1,16 @@
 #include "transforms/number_comparisons.h"
 #include <math.h>
 
-/*
-compare_doubles
-REQUIRES:
-    - a, b, epsilon are all doubles
-PROMISES:
-    - returns 1 if a and b are approximately equal to each other (within epsilon)
-    - returns 0 otherwise
-*/
-uint8_t compare_doubles(double a, double b, double epsilon)
+/// @brief Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
+/// @return Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
+uint8_t GEN_compare_doubles(double a, double b, double epsilon)
 {
     return fabs(a - b) < epsilon;
 }
 
-/*
-compare_floats
-REQUIRES:
-    - a, b, epsilon are all floats
-PROMISES:
-    - returns 1 if a and b are approximately equal to each other (within epsilon)
-    - returns 0 otherwise
-*/
-uint8_t compare_floats(float a, float b, float epsilon)
+/// @brief Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
+/// @return Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
+uint8_t GEN_compare_floats(float a, float b, float epsilon)
 {
     return fabs(a - b) < epsilon;
 }
