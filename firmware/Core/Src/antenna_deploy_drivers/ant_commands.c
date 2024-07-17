b8c60591ba9aa43b11dbd786794ee5b9252e0801
Kale <kale.fordham@ucalgary.ca>
2024-07-16 19:55:06 -0600

    Added Functionality for internal drivers
    Added Functionality for sending a command and receiving response.

--- firmware/Core/Src/antenna_deploy_drivers/ant_commands.c
+++ firmware/Core/Src/antenna_deploy_drivers/ant_commands.c
@@ -8,11 +8,41 @@
 uint8_t ANT_CMD_measure_temp() {
     const uint8_t CC = 0xC0;
 
+    const uint8_t cmd_len = 1;
+    const uint8_t cmd_buf[cmd_len] = {CC};
+
+    const uint8_t rx_len = 2;
+    const rx_buf[rx_len];
+
+    const uint8_t comms_err = ANT_send_cmd_get_response(cmd_buf, cmd_len, rx_buf, rx_len);
+    return comms_err;
+}
+
+uint8_t ANT_CMD_deploy_antenna1() {
+    const uint8_t CC = 0xA1;
+    const uint8_t CC_param = 0x05; // Test with 5 second max activation time
+
+    const uint8_t cmd_len = 2;
+    uint8_t cmd_buf[cmd_len];
+
+    cmd_buf[0] = CC;
+    cmd_buf[1] = CC_param;
+
+    const uint8_t rx_len = 0;
+    uint8_t rx_buf[] = NULL;
+
+    const uint8_t comms_err = ANT_send_cmd_get_response(cmd_buf, cmd_len, rx_buf, rx_len);
+    return comms_err;
+}
+
+uint8_t ANT_CMD_arm_antenna_system() {
+    const uint8_t CC = 0xAD;
+
     const uint8_t cmd_buf[] = {CC};
     const uint8_t cmd_len = sizeof(cmd_buf);
 
-    const uint8_t rx_len = ANT_DEFAULT_RX_LEN_MIN;
-    const rx_buf[rx_len];
+    const uint8_t rx_len = 0;
+    uint8_t rx_buf[] = NULL;
 
     const uint8_t comms_err = ANT_send_cmd_get_response(cmd_buf, cmd_len, rx_buf, rx_len);
     return comms_err;
