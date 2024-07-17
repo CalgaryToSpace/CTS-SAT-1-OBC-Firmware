eeda4b4a701d7b52f901ef220f9ad2e6f4f2cc84
parker-research <166864283+parker-research@users.noreply.github.com>
2024-07-17 01:18:48 -0600

    Header tweaks

--- firmware/Core/Inc/unit_tests/test_telecommand_arg_helpers.h
+++ firmware/Core/Inc/unit_tests/test_telecommand_arg_helpers.h
@@ -1,8 +1,7 @@
 #ifndef __INCLUDE_GUARD__TEST_COMMAND_ARG_HELPERS_H__
 #define __INCLUDE_GUARD__TEST_COMMAND_ARG_HELPERS_H__
 
-#include "main.h"
-
 uint8_t TEST_EXEC__TCMD_extract_hex_array_arg();
 
-#endif // __INCLUDE_GUARD__TEST_BYTE_TRANSFORMS_H__
+#endif // __INCLUDE_GUARD__TEST_COMMAND_ARG_HELPERS_H__
+
