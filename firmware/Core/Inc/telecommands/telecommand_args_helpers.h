567ee1b41f07b828bda3c290899c3c6ceb869f87
vaibhav kapoor <vaibhav.kapoor@ucalgary.ca>
2024-07-18 16:34:31 -0600

    Minor bug fixes and prep to utilize string to bytes helper method

--- firmware/Core/Inc/telecommands/telecommand_args_helpers.h
+++ firmware/Core/Inc/telecommands/telecommand_args_helpers.h
@@ -2,6 +2,7 @@
 #define __INCLUDE_GUARD__TELECOMMAND_ARGS_HELPERS_H__
 
 #include <stdint.h>
+#include <stddef.h>
 
 uint8_t TCMD_ascii_to_uint64(const char *str, uint32_t str_len, uint64_t *result);
 uint8_t TCMD_extract_uint64_arg(const char *str, uint32_t str_len, uint8_t arg_index, uint64_t *result);
