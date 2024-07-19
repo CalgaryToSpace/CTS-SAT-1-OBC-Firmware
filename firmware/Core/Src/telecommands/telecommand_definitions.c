ed6bbcee93975d08a101b4b17880be74b5995fd4
Shaan Samresh <shaan.samresh@ucalgary.ca>
2024-07-18 19:14:38 -0600

    Initil commit for CRC functionality

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -78,7 +78,12 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .tcmd_name = "available_telecommands",
         .tcmd_func = TCMDEXEC_available_telecommands,
         .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
+    },
+    {
+        .tcmd_name = "crc",
+        .tcmd_func = TCMDEXEC_crc,
+        .number_of_args = 1,
+
     },
 
     // ****************** SECTION: flash_telecommand_defs ******************
@@ -278,5 +283,21 @@ uint8_t TCMDEXEC_available_telecommands(const char *args_str, TCMD_TelecommandCh
         remaining_space -= line_length;
     }
 
+    return 0;
+}
+uint8_t TCMDEXEC_crc(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,char *response_output_buf,
+                        uint16_t response_output_buf_len){
+    char input_arr[100];
+    uint32_t crc_result;
+    if (sscanf(args_str, "%s", &input_arr) != 1)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error: Invalid argument. Expected 1 String argument.");
+        return 1;
+    }
+
+    GEN_crc32_checksum(input_arr, sizeof(input_arr) - 1, &crc_result);
+    DEBUG_uart_print_str("CRC32 Checksum: %08X\n", crc_result);
+
+    snprintf(response_output_buf, response_output_buf_len, "The crc result of %s is %s.\n", input_arr, crc_result);
     return 0;
 }
