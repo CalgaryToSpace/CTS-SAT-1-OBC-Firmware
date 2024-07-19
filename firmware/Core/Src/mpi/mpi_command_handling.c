a286dcadfdb840a5eea09b5f05d716f24770e6ba
vaibhav kapoor <vaibhav.kapoor@ucalgary.ca>
2024-07-18 19:43:07 -0600

    Updated to utilize TCMD_extract_hex_array_arg helper method

--- firmware/Core/Src/mpi/mpi_command_handling.c
+++ firmware/Core/Src/mpi/mpi_command_handling.c
@@ -6,21 +6,25 @@
 
 /**
  * @brief Sends commandcode+params to the MPI as bytes
- * @param command data (command) being sent to the MPI
- * @returns 0 - success, 1 - failure from the MPI, 2 - failed UART transmission, 3 - failed UART reception
+ * 
+ * @param bytes_to_send Buffer containing the CMD + Params (IF ANY) as hex bytes
+ * @param bytes_to_send_len Size of CMD buffer
+ * @param mpi_cmd_response Buffer to store incoming response from the MPI
+ * @param mpi_cmd_response_len Size of response buffer
+ * @return 0: Success, 1: Failed UART transmission, 2: Failed UART reception, 3: MPI failed to execute CMD
  */
-uint8_t MPI_send_telecommand_hex(const uint8_t *bytes_to_send, size_t bytes_to_send_len, uint8_t *mpi_cmd_response, size_t mpi_cmd_response_len)
+uint8_t MPI_send_telecommand_hex(const uint8_t *bytes_to_send, size_t bytes_to_send_len, uint8_t mpi_response[], size_t mpi_response_size)
 {
-	// Buffers to store outgoing & incoming data from the MPI
-	uint8_t UART1_txBuffer[bytes_to_send_len + 2];	   // +2 accounts for 'TC' command identifier being appended
-	memset(UART1_txBuffer, 0, sizeof(UART1_txBuffer)); // Initialize all elements to 0
+	// Transmit Buffer to store CMD+Params formatted to be sent to the MPI
+	uint8_t UART1_txBuffer[bytes_to_send_len + 2];	   	// +2 accounts for 'TC' command identifier being appended
+	memset(UART1_txBuffer, 0, sizeof(UART1_txBuffer)); 	// Initialize all elements to 0
 
 	// All commands to the MPI must begin with 'TC'
-	UART1_txBuffer[0] = 0x54; // Hex representation of 'T'
-	UART1_txBuffer[1] = 0x43; // Hex representation of 'C'
+	UART1_txBuffer[0] = 0x54;	// Hex representation of 'T'
+	UART1_txBuffer[1] = 0x43;	// Hex representation of 'C'
 
-	// Add the command bytes / parameters to the buffer
-	if (bytes_to_send_len != 0)
+	// Add the command bytes & parameters to the Transmit Buffer
+	if (bytes_to_send_len > 0)
 	{
 		for (size_t i = 0; i < bytes_to_send_len; i++)
 		{
@@ -29,35 +33,29 @@ uint8_t MPI_send_telecommand_hex(const uint8_t *bytes_to_send, size_t bytes_to_s
 	}
 
 	// Transmit the MPI command
-	HAL_StatusTypeDef transmit_status = HAL_UART_Transmit(&huart1, (uint8_t *)UART1_txBuffer, sizeof(UART1_txBuffer), 100);
+	uint32_t tx_timeout = 100;
+	HAL_StatusTypeDef transmit_status = HAL_UART_Transmit(&huart1, (uint8_t *)UART1_txBuffer, sizeof(UART1_txBuffer), tx_timeout);
 
 	// Check UART transmission
 	if (transmit_status != HAL_OK)
 	{
-		return 2; // Error code for failed UART transmission
+		return 1; // Error code: Failed UART transmission
 	}
 
 	// Receive MPI response
-	HAL_StatusTypeDef receive_status = HAL_UART_Receive_DMA(&huart1, (uint8_t *)mpi_cmd_response, mpi_cmd_response_len);
+	HAL_StatusTypeDef receive_status = HAL_UART_Receive_DMA(&huart1, mpi_response, mpi_response_size);
 
 	// Check UART reception
 	if (receive_status != HAL_OK)
 	{
-		return 3; // Error Code for failed UART reception
+		return 2; // Error code: Failed UART reception
 	}
 
-	// Verify MPI response
-	if (memcmp(UART1_txBuffer, mpi_cmd_response, *bytes_to_send + 2) != 0)
+	// Check MPI response for errors: It should be an echo of the cmd sent along with an appended response code
+	if (memcmp(UART1_txBuffer, mpi_response, bytes_to_send_len+2) != 0 || mpi_response[bytes_to_send_len+3] != 0x01)
 	{
-		return 1; // Error code for invalid response from the MPI
+		return 3; // Error code: Invalid response from the MPI
 	}
-	// Check if the byte after (cmd echo) is 0x01
-	if (mpi_cmd_response[bytes_to_send_len + 3] == 0x01)
-	{
-		return 0; // Valid response from the MPI
-	}
-	else
-	{
-		return 1; // Error code for invalid response from the MPI
-	}
+	
+	return 0; // Valid response from the MPI
 }
--- firmware/Core/Src/telecommands/mpi_telecommand_defs.c
+++ firmware/Core/Src/telecommands/mpi_telecommand_defs.c
@@ -16,7 +16,7 @@
  * @param tcmd_channel The channel on which the telecommand was received, and on which the response should be sent
  * @param response_output_buf The buffer to write the response to
  * @param response_output_buf_len The maximum length of the response_output_buf (its size)
- * @return 0: if successful, 1: if invalid input, 2: error transmitting, 3: MPI responded with an error
+ * @return 0: if successful, 1: if invalid input, 2: error transmitting, 3: MPI failed to execute cmd
  */
 uint8_t TCMDEXEC_mpi_send_command_hex(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                                       char *response_output_buf, uint16_t response_output_buf_len)
@@ -24,63 +24,51 @@ uint8_t TCMDEXEC_mpi_send_command_hex(const char *args_str, TCMD_TelecommandChan
     // Get the length of the input string
     size_t args_str_len = strlen((const char *)args_str);
 
-    // Invalid argument: NULL
+    // Validate command argument
     if (args_str == NULL)
     {
         snprintf(response_output_buf, response_output_buf_len, "No commands received. Sample MPI command: 0204, sets HV_INNER_DOME_SCAN_MODE(02) to sawtooth with beam tracking (04)");
-        return 1; // Error code for null arg
+        return 1; // Error code: NULL input
     }
 
-    // Invalid argument: Invalid hex length
     if (args_str_len % 2 != 0)
     {
-        snprintf(response_output_buf, response_output_buf_len, "Invalid hex string length. Must be even.\n");
-        return 2; // Error code for invalid hex input
+        snprintf(response_output_buf, response_output_buf_len, "Invalid hex-encoded string length. Must be even.\n");
+        return 2; // Error code: Invalid hex input
     }
 
-    // Invalid argument: Invalid hex character
-    char *endptr;
-    for (size_t i = 0; i < args_str_len; i += 2)
-    {
-        // Convert two characters at a time to ensure they form a valid hex byte
-        char hex_byte[3] = {args_str[i], args_str[i + 1], '\0'};
-        strtol(hex_byte, &endptr, 16);
-        if (*endptr != '\0')
-        {
-            snprintf(response_output_buf, response_output_buf_len, "Invalid character in hex string: %s\n", hex_byte);
-            return 2; // Error code for invalid hex input
-        }
+    // Parse hex-encoded string to bytes
+    uint16_t args_bytes_size = args_str_len/2;
+    uint16_t args_bytes_len;
+    uint8_t args_bytes[args_bytes_size];
+    uint8_t bytes_parse_result = TCMD_extract_hex_array_arg(args_str, 0, args_bytes, args_bytes_size, &args_bytes_len);
+
+    // Check for hex parsing errors
+    if(bytes_parse_result != 0){
+        snprintf(response_output_buf, response_output_buf_len, "Invalid character found in hex-encoded string");
+        return 2; // Error code: Invalid hex input
     }
-
-    // Parse command from string to hex
-    const size_t args_str_hex_size = 50;    //TODO: Find right max size / expected size
-    uint8_t args_str_hex[args_str_hex_size];
-    size_t args_str_hex_len = 0;  
-    uint8_t parse_args_str_result = TCMD_hex_string_to_bytes(args_str, 0, args_str_hex, args_str_hex_size, &args_str_hex_len);
-
-    // TODO: Add error checking for parse result
 
     // Allocate space to receive incoming MPI response
-    const size_t mpi_cmd_response_len = 50;
-    uint8_t mpi_cmd_response[mpi_cmd_response_len];        // Max possible size for an MPI command+parameters can be 7 bytes + 2^N bytes of variable payload. To account for a buffer zone, 50 bytes will be allocated
-    memset(mpi_cmd_response, 0, sizeof(mpi_cmd_response)); // Initialize all elements to 0
+    const size_t mpi_response_size = 50;
+    uint8_t mpi_response[mpi_response_size];     // Max possible size for an MPI command+parameters can be 7 bytes + 2^N bytes of variable payload. To account for a buffer zone, 50 bytes will be allocated
+    memset(mpi_response, 0, mpi_response_size);  // Initialize all elements to 0
 
-    // Send command to MPI
-    uint8_t cmd_response = MPI_send_telecommand_hex(args_str_hex, args_str_hex_len, mpi_cmd_response, mpi_cmd_response_len);
+    // Send command to MPI and receive back the response
+    uint8_t cmd_response = MPI_send_telecommand_hex(args_bytes, args_bytes_len, mpi_response, mpi_response_size);
 
     // Verify successful echo response from the mpi
     if (cmd_response != 0)
     {
-        snprintf(&response_output_buf[strlen(response_output_buf)], response_output_buf_len - strlen(response_output_buf) - 1, "MPI failed to execute command. MPI echoed response code: %u\n", cmd_response);
-
-        // Send back complete response from the MPI
+        // Send back complete response from the MPI incase of an error
+        snprintf(response_output_buf, response_output_buf_len, "MPI failed to execute command. MPI echoed response code: %u\n", cmd_response);        
         snprintf(&response_output_buf[strlen(response_output_buf)], response_output_buf_len - strlen(response_output_buf) - 1, "MPI response buffer: ");
-        for (size_t i = 0; i < mpi_cmd_response_len; i++)
+        for (size_t i = 0; i < mpi_response_size; i++)
         {
-            snprintf(&response_output_buf[strlen(response_output_buf)], response_output_buf_len - strlen(response_output_buf) - 1, "%02X", mpi_cmd_response[i]);
+            snprintf(&response_output_buf[strlen(response_output_buf)], response_output_buf_len - strlen(response_output_buf) - 1, "%02X", mpi_response[i]);
         }
 
-        return 3; // Error code: MPI responded with an error
+        return 3; // Error code: MPI failed to execute cmd
     }
 
     // Successful MPI response
