d6ba04307f6b42f6b63f77a10d2202ad1b82b8da
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-07-19 22:57:40 -0600

    Fix telecommand registered name

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -82,7 +82,7 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
     {
-        .tcmd_name = "scan_i2c_device",
+        .tcmd_name = "scan_i2c_bus",
         .tcmd_func = TCMDEXEC_scan_i2c_bus,
         .number_of_args = 1,
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
