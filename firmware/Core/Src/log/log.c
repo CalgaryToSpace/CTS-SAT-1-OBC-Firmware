d49757625f2749c30a83fc74525574a2012f4ebd
Johnathan K Burchill <jkburchi@ucalgary.ca>
2024-07-20 15:46:04 -0600

    Move channel-specific code into a separate translation unit

--- firmware/Core/Src/log/log.c
+++ firmware/Core/Src/log/log.c
@@ -1,7 +1,6 @@
 #include "log/log.h"
+#include "log/log_sinks.h"
 #include "debug_tools/debug_uart.h"
-#include "littlefs/lfs.h"
-#include "littlefs/littlefs_helper.h"
 #include "timekeeping/timekeeping.h"
 
 #include <complex.h>
@@ -13,9 +12,6 @@
 
 // Inspired by uLog: https://github.com/rdpoor/ulog
 
-extern UART_HandleTypeDef hlpuart1;
-extern uint8_t LFS_is_lfs_mounted;
-
 // Internal interfaces and variables
 
 #define LOG_TIMESTAMP_MAX_LENGTH 30
@@ -25,7 +21,6 @@ extern uint8_t LFS_is_lfs_mounted;
 #define LOG_FORMATTED_MESSAGE_MAX_LENGTH 256
 // Includes prefix, with cushion for delimiters and newline
 #define LOG_FULL_MESSAGE_MAX_LENGTH ( LOG_FORMATTED_MESSAGE_MAX_LENGTH + LOG_TIMESTAMP_MAX_LENGTH + LOG_CHANNEL_NAME_MAX_LENGTH + LOG_SYSTEM_NAME_MAX_LENGTH + 1 )
-#define LOG_UART_TRANSMIT_TIMEOUT 200
 
 static char LOG_timestamp_string[LOG_TIMESTAMP_MAX_LENGTH] = {0};
 static char LOG_formatted_log_message[LOG_FORMATTED_MESSAGE_MAX_LENGTH] = {0};
@@ -361,74 +356,5 @@ const char* LOG_get_severity_name(LOG_severity_enum_t severity)
         default:
             return "UNKNOWN SEVERITY";
     }
-}
-
-// Internal functions
-
-/// @brief Sends a log message to a log file
-/// @param filename full path of the log file
-/// @param msg The message to be logged
-/// @return void
-void LOG_to_file(const char filename[], const char msg[])
-{
-    if (!LFS_is_lfs_mounted) {
-        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
-                "Error writing to system log file: LFS not mounted.\n");
-        return;
-    }
-
-    // We cannot use LFS_append_file due to recursion of the logging system
-    lfs_file_t file;
-    const int8_t open_result = lfs_file_opencfg(&LFS_filesystem, &file, filename, LFS_O_RDWR | LFS_O_CREAT | LFS_O_APPEND, &LFS_file_cfg);
-	if (open_result < 0)
-	{
-        // This error cannot be logged, except via UART or during an overpass 
-        // of the ground station
-        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
-                "Error opening system log file.\n");
-		return;
-	}
-    const lfs_soff_t offset = lfs_file_seek(&LFS_filesystem, &file, 0, LFS_SEEK_END);
-    if (offset < 0) {
-        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
-                "Error seeking to end of system log file.\n");
-        return;
-    }
-
-	const lfs_ssize_t bytes_written = lfs_file_write(&LFS_filesystem, &file, msg, strlen(msg));
-	if (bytes_written < 0) {
-        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
-                "Error writing to system log file.\n");
-		return;
-	}
-	
-	// Close the File, the storage is not updated until the file is closed successfully
-	const int8_t close_result = lfs_file_close(&LFS_filesystem, &file);
-	if (close_result < 0) {
-        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
-                "Error closing system log file.\n");
-		return;
-	}
-	
-	return;
-}
-
-/// @brief Sends a log message to the umbilical UART
-/// @param msg The message to be logged
-/// @return void
-void LOG_to_umbilical_uart(const char msg[]) 
-{
-    HAL_UART_Transmit(&hlpuart1, (uint8_t *)msg, strlen(msg),
-                      LOG_UART_TRANSMIT_TIMEOUT);
-    return;
-}
-
-/// @brief Sends a log message to the UHF radio
-/// @param msg The message to be logged
-/// @return void
-void LOG_to_uhf_radio(const char msg[])
-{
-    LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_CHANNEL_UMBILICAL_UART, "TODO: replace this with a UHF RADIO transmission");
-    return;
 }
 
---
+++ firmware/Core/Src/log/log_sinks.c
@@ -0,0 +1,77 @@
+#include "log/log.h"
+#include "log/log_sinks.h"
+#include "littlefs/lfs.h"
+#include "littlefs/littlefs_helper.h"
+
+extern UART_HandleTypeDef hlpuart1;
+extern uint8_t LFS_is_lfs_mounted;
+
+
+/// @brief Sends a log message to a log file
+/// @param filename full path of the log file
+/// @param msg The message to be logged
+/// @return void
+void LOG_to_file(const char filename[], const char msg[])
+{
+    if (!LFS_is_lfs_mounted) {
+        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
+                "Error writing to system log file: LFS not mounted.\n");
+        return;
+    }
+
+    // We cannot use LFS_append_file due to recursion of the logging system
+    lfs_file_t file;
+    const int8_t open_result = lfs_file_opencfg(&LFS_filesystem, &file, filename, LFS_O_RDWR | LFS_O_CREAT | LFS_O_APPEND, &LFS_file_cfg);
+	if (open_result < 0)
+	{
+        // This error cannot be logged, except via UART or during an overpass 
+        // of the ground station
+        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
+                "Error opening system log file.\n");
+		return;
+	}
+    const lfs_soff_t offset = lfs_file_seek(&LFS_filesystem, &file, 0, LFS_SEEK_END);
+    if (offset < 0) {
+        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
+                "Error seeking to end of system log file.\n");
+        return;
+    }
+
+	const lfs_ssize_t bytes_written = lfs_file_write(&LFS_filesystem, &file, msg, strlen(msg));
+	if (bytes_written < 0) {
+        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
+                "Error writing to system log file.\n");
+		return;
+	}
+	
+	// Close the File, the storage is not updated until the file is closed successfully
+	const int8_t close_result = lfs_file_close(&LFS_filesystem, &file);
+	if (close_result < 0) {
+        LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_all_channels_except(LOG_CHANNEL_FILE), 
+                "Error closing system log file.\n");
+		return;
+	}
+	
+	return;
+}
+
+/// @brief Sends a log message to the umbilical UART
+/// @param msg The message to be logged
+/// @return void
+void LOG_to_umbilical_uart(const char msg[]) 
+{
+    HAL_UART_Transmit(&hlpuart1, (uint8_t *)msg, strlen(msg),
+                      LOG_UART_TRANSMIT_TIMEOUT);
+    return;
+}
+
+/// @brief Sends a log message to the UHF radio
+/// @param msg The message to be logged
+/// @return void
+void LOG_to_uhf_radio(const char msg[])
+{
+    // FIXME: replace with UHF radio transmission
+    LOG_message(LOG_SYSTEM_LOG, LOG_SEVERITY_CRITICAL, LOG_CHANNEL_UMBILICAL_UART, "TODO: replace this with a UHF RADIO transmission");
+    return;
+}
+
