b9dd1442690e3261ef433f96531ccd6e834c7431
parker-research <166864283+parker-research@users.noreply.github.com>
2024-07-31 02:33:01 -0600

    Refactor: move testing telecommand definitions to new file together

--- firmware/Core/Inc/telecommands/freertos_telecommand_defs.h
+++ firmware/Core/Inc/telecommands/freertos_telecommand_defs.h
@@ -2,6 +2,8 @@
 #ifndef __INCLUDE_GUARD__FREERTOS_TELECOMMAND_DEFINITIONS_H
 #define __INCLUDE_GUARD__FREERTOS_TELECOMMAND_DEFINITIONS_H
 
+#include "telecommands/telecommand_types.h"
+
 #include <stdint.h>
 
 uint8_t TCMDEXEC_freetos_list_tasks_jsonl(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
---
+++ firmware/Core/Inc/telecommands/testing_telecommand_defs.h
@@ -0,0 +1,17 @@
+
+#ifndef INCLUDE_GUARD__TESTING_TELECOMMAND_DEFINITIONS_H
+#define INCLUDE_GUARD__TESTING_TELECOMMAND_DEFINITIONS_H
+
+#include "telecommands/telecommand_types.h"
+#include <stdint.h>
+
+uint8_t TCMDEXEC_echo_back_args(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len);
+
+uint8_t TCMDEXEC_echo_back_uint32_args(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len);
+
+uint8_t TCMDEXEC_run_all_unit_tests(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len);
+
+#endif // INCLUDE_GUARD__TESTING_TELECOMMAND_DEFINITIONS_H
