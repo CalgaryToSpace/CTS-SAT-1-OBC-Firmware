575aa4821f3e94d424bc24108012baa530528f35
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-07-31 01:47:43 -0600

    Increase FreeRTOS task max name length

--- firmware/Core/Inc/FreeRTOSConfig.h
+++ firmware/Core/Inc/FreeRTOSConfig.h
@@ -73,7 +73,7 @@
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configTOTAL_HEAP_SIZE                    ((size_t)32768)
-#define configMAX_TASK_NAME_LEN                  ( 16 )
+#define configMAX_TASK_NAME_LEN                  ( 64 )
 #define configGENERATE_RUN_TIME_STATS            1
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
