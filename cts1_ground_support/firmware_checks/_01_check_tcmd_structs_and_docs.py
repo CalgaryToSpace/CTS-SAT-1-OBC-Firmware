963a96431e238da67452808bc7c846fdb3a9c511
parker-research <166864283+parker-research@users.noreply.github.com>
2024-07-31 04:03:31 -0600

    Add checks for tcmd structs
    Create check-telecommands workflow
    
    Reorganize GitHub Actions

---
+++ cts1_ground_support/firmware_checks/_01_check_tcmd_structs_and_docs.py
@@ -0,0 +1,64 @@
+"""Check that the telecommand definitions are good."""
+
+from cts1_ground_support.telecommand_array_parser import parse_telecommand_list_from_repo
+from loguru import logger
+
+
+def check_tcmd_struct_fields() -> None:
+    """Check that the telecommand struct fields are all present."""
+    success = True
+    tcmd_list = parse_telecommand_list_from_repo()
+
+    for tcmd in tcmd_list:
+        if not tcmd.has_required_fields():
+            logger.warning(
+                f"The struct for the '{tcmd.name}' telecommand is missing required fields."
+            )
+            success = False
+
+    if success:
+        logger.success("All telecommand structs have the required fields.")
+    else:
+        msg = "One or more telecommand structs are missing required fields."
+        raise ValueError(msg)
+
+
+def check_tcmd_arg_lists() -> None:
+    """Check that the telecommand docstrings have the correct number of arguments."""
+    success = True
+    tcmd_list = parse_telecommand_list_from_repo()
+
+    for tcmd in tcmd_list:
+        if tcmd.argument_descriptions is None:
+            if tcmd.number_of_args > 0:
+                logger.warning(
+                    f"The '{tcmd.name}' telecommand is missing the `args_str` param in its "
+                    "docstring."
+                )
+                success = False
+
+            # Can't continue on to the next check if the argument descriptions are missing.
+            continue
+
+        if tcmd.number_of_args != len(tcmd.argument_descriptions):
+            logger.warning(
+                f"The '{tcmd.name}' telecommand has number_of_args={tcmd.number_of_args} args in "
+                f"the struct, and {len(tcmd.argument_descriptions)} args in the docstring. "
+                "These should be equal."
+            )
+            success = False
+
+    if success:
+        logger.success("All telecommands have appropriate argument docstrings!")
+    else:
+        logger.error(
+            "Please look at the `docs/C_General_Guidelines.md` file to see the docstring "
+            "specification for telecommands."
+        )
+        msg = "One or more telecommand structs are missing required fields."
+        raise ValueError(msg)
+
+
+if __name__ == "__main__":
+    check_tcmd_struct_fields()
+    check_tcmd_arg_lists()
