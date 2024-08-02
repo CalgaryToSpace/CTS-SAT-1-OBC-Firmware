eb624dd6c5561d82e8370afdc9a0ccba9cb4b677
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-08-01 22:12:11 -0600

    Created unit test files

---
+++ firmware/Core/Inc/unit_tests/test_flash_driver.h
@@ -0,0 +1,7 @@
+#ifndef __INCLUDE_GUARD__TEST_FLASH_DRIVER__
+#define __INCLUDE_GUARD__TEST_FLASH_DRIVER__
+
+#include <stdint.h>
+
+
+#endif // __INCLUDE_GUARD__TTEST_FLASH_DRIVER__
---
+++ firmware/Core/Src/unit_tests/test_flash_driver.c
@@ -0,0 +1,11 @@
+#include "unit_tests/unit_test_helpers.h"
+#include "unit_tests/test_flash_driver.h"
+
+
+
+uint8_t TEST_EXEC__FLASH_read() {
+    // TODO: implement unit test later
+    TEST_ASSERT_TRUE(1 == 1);
+
+    return 0; // THIS LINE MUST BE INCLUDED to indicate the test passed
+}
