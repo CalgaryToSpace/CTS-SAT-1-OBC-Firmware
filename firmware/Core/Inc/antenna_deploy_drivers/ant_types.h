08faffc414a23170d8e1bdb878f03111aa8fda4e
KaleF07 <kalefordham@gmail.com>
2024-08-06 18:40:21 -0700

    Updated based on PR comments
    Based on PR comments, updated the ant_commands, ant_internal_drivers and antenna_telecommand_defs files.

--- firmware/Core/Inc/antenna_deploy_drivers/ant_types.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INCLUDE_GUARD_ANT_TYPES_H__
-#define __INCLUDE_GUARD_ANT_TYPES_H__
-
-#include <stdint.h>
-
-
-
-#endif /* __INCLUDE_GUARD_ANT_TYPES_H__ */
--- firmware/Core/Src/antenna_deploy_drivers/ant_commands.c
+++ firmware/Core/Src/antenna_deploy_drivers/ant_commands.c
@@ -3,12 +3,10 @@
 #include "antenna_deploy_drivers/ant_commands.h"
 #include "antenna_deploy_drivers/ant_internal_drivers.h"
 #include "stm32l4xx_hal_i2c.h"
-#include "debug_tools/debug_i2c.h"
 #include "debug_tools/debug_uart.h"
 
 #include <stdint.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 /// @brief  Arm the antenna deploy system
 /// @return 0 if successful, >0 if error occurred
@@ -20,15 +18,12 @@ uint8_t ANT_CMD_arm_antenna_system() {
 
     cmd_buf[0] = CC;
 
-    uint8_t rx_len = 0;
-    uint8_t rx_buf[] = {0};
-
-    const uint8_t comms_err = ANT_send_cmd_get_response(cmd_buf, cmd_len, rx_buf, rx_len);
+    const uint8_t comms_err = ANT_send_cmd(cmd_buf, cmd_len);
     return comms_err;
 }
 
 /// @brief Deploys antenna 1
-/// @param activation_time_seconds Activation time in seconds
+/// @param[in] activation_time_seconds Activation time in seconds
 /// @return 0 if successful, >0 if error occurred
 uint8_t ANT_CMD_deploy_antenna1(uint8_t activation_time_seconds) {
     uint8_t CC = 0xA1;
@@ -40,15 +35,13 @@ uint8_t ANT_CMD_deploy_antenna1(uint8_t activation_time_seconds) {
     cmd_buf[0] = CC;
     cmd_buf[1] = CC_param;
 
-    uint8_t rx_len = 0;
-    uint8_t rx_buf[] = {0};
-
-    const uint8_t comms_err = ANT_send_cmd_get_response(cmd_buf, cmd_len, rx_buf, rx_len);
+    const uint8_t comms_err = ANT_send_cmd(cmd_buf, cmd_len);
     return comms_err;
 }
 
 /// @brief Measure the temperature of the antenna controller system
 /// @return 0 if successful, >0 if error occurred
+/// @note Upon success, the temperature is printed to the debug UART
 uint8_t ANT_CMD_measure_temp() {
     uint8_t CC = 0xC0;
 
@@ -60,7 +53,12 @@ uint8_t ANT_CMD_measure_temp() {
     uint8_t rx_len = 2;
     uint8_t rx_buf[rx_len];
 
-    const uint8_t comms_err = ANT_send_cmd_get_response(cmd_buf, cmd_len, rx_buf, rx_len);
+    uint8_t comms_err = ANT_send_cmd(cmd_buf, cmd_len);
+    if (comms_err == 0) {
+        comms_err = ANT_get_response(rx_buf, rx_len);
+    }
+
+
     DEBUG_uart_print_str("Received raw bytes: ");
     DEBUG_uart_print_array_hex(rx_buf, rx_len);
     DEBUG_uart_print_str("\n");
