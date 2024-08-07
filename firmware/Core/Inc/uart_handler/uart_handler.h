7affae7649e09789c8f3303d48db0737268208c7
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-08-06 18:55:53 -0600

    Updated uart_handler.h, Added some psuedo code for the rtos

--- firmware/Core/Inc/uart_handler/uart_handler.h
+++ firmware/Core/Inc/uart_handler/uart_handler.h
@@ -10,6 +10,12 @@ extern volatile uint16_t UART_telecommand_buffer_write_idx; // Write index for U
 extern volatile uint32_t UART_telecommand_last_write_time_ms; // Last write time in milliseconds for UART telecommand
 extern volatile uint8_t UART_telecommand_buffer_last_rx_byte; // Last received byte for UART telecommand
 
+extern const uint16_t UART_gps_buffer_len; // Length of the UART telecommand buffer
+extern volatile uint8_t UART_gps_buffer[];          // Buffer for UART telecommands
+extern volatile uint16_t UART_gps_buffer_write_idx; // Write index for UART telecommand buffer
+extern volatile uint32_t UART_gps_last_write_time_ms; // Last write time in milliseconds for UART telecommand
+extern volatile uint8_t UART_gps_buffer_last_rx_byte; // Last received byte for UART telecommand
+
 
 void UART_init_uart_handlers(void);
 
