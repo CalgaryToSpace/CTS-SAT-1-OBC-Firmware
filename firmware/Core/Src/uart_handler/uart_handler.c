2c58ff724a3b4d78f63179c0bc0c85eefd20f7a0
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-08-06 18:19:54 -0600

    Added a UART buffer in the uart handler, need to test this when I create the rtos thread

--- firmware/Core/Src/uart_handler/uart_handler.c
+++ firmware/Core/Src/uart_handler/uart_handler.c
@@ -2,12 +2,22 @@
 #include "debug_tools/debug_uart.h"
 #include "main.h"
 
+// UART interfaces
+UART_HandleTypeDef *UART_gps_port_handle = &huart3; // TODO: update this to the appropriate one
+
 // UART telecommand buffer
 const uint16_t UART_telecommand_buffer_len = 256; // extern
 volatile uint8_t UART_telecommand_buffer[256]; // extern // TODO: confirm that this volatile means that the contents are volatile but the pointer is not
 volatile uint16_t UART_telecommand_buffer_write_idx = 0; // extern
 volatile uint32_t UART_telecommand_last_write_time_ms = 0; // extern
 volatile uint8_t UART_telecommand_buffer_last_rx_byte = 0; // extern
+
+// UART GPS buffer
+const uint16_t UART_gps_buffer_len = 256; // extern
+volatile uint8_t UART_gps_buffer[256]; // extern // TODO: confirm that this volatile means that the contents are volatile but the pointer is not
+volatile uint16_t UART_gps_buffer_write_idx = 0; // extern
+volatile uint32_t UART_gps_last_write_time_ms = 0; // extern
+volatile uint8_t UART_gps_buffer_last_rx_byte = 0; // extern
 
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
@@ -33,6 +43,23 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
         UART_telecommand_last_write_time_ms = HAL_GetTick();
         HAL_UART_Receive_IT(&hlpuart1, (uint8_t*) &UART_telecommand_buffer_last_rx_byte, 1);
     }
+    else if (huart->Instance == UART_gps_port_handle) {
+        // add the byte to the buffer
+        if (UART_gps_buffer_write_idx >= UART_gps_buffer_len) {
+            DEBUG_uart_print_str("HAL_UART_RxCpltCallback() -> UART gps buffer is full\n");
+            
+            // shift all bytes left by 1
+            for (uint16_t i = 1; i < UART_gps_buffer_len; i++) {
+                UART_gps_buffer[i - 1] = UART_gps_buffer[i];
+            }
+
+            // reset to a valid index
+            UART_gps_buffer_write_idx = UART_gps_buffer_len - 1;
+        }
+        UART_gps_buffer[UART_gps_buffer_write_idx++] = UART_gps_buffer_last_rx_byte;
+        UART_gps_last_write_time_ms = HAL_GetTick();
+        HAL_UART_Receive_IT(UART_gps_port_handle, (uint8_t*) &UART_gps_buffer_last_rx_byte, 1);
+    }
     else {
         // FIXME: add the rest
         DEBUG_uart_print_str("HAL_UART_RxCpltCallback() -> unknown UART instance\n"); // FIXME: remove
@@ -44,6 +71,7 @@ void UART_init_uart_handlers(void)
 {
     // enable the UART interrupt
     HAL_UART_Receive_IT(&hlpuart1, (uint8_t*) &UART_telecommand_buffer_last_rx_byte, 1);
+    HAL_UART_Receive_IT(UART_gps_port_handle, (uint8_t*) &UART_gps_buffer_last_rx_byte, 1);
 
     // TODO: add the rest
 }
