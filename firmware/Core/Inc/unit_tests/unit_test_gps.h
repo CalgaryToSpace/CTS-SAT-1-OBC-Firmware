6e6b6e50cdacdcf84641f87f9eb2564bb826e75d
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-08-16 22:20:14 -0600

    Created Response Header unit test

---
+++ firmware/Core/Inc/unit_tests/unit_test_gps.h
@@ -0,0 +1,8 @@
+#ifndef __INCLUDE_GUARD__UNIT_TEST_GPS_H__
+#define __INCLUDE_GUARD__UNIT_TEST_GPS_H__
+
+#include <stdint.h>
+
+uint8_t TEST_EXEC__GPS_Pack_to_header();
+
+#endif // __INCLUDE_GUARD__UNIT_TEST_GPS_H__
