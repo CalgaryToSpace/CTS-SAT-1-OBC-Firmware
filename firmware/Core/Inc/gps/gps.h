1b1c5dec0d9d33c5de56a0a7fe4fd3d9b8f0050b
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-08-17 16:11:01 -0600

    Fixing build issues

--- firmware/Core/Inc/gps/gps.h
+++ firmware/Core/Inc/gps/gps.h
@@ -7,6 +7,6 @@ uint32_t crc32_value(uint8_t i);
 uint32_t calculate_block_crc32( uint32_t ulCount, uint8_t *ucBuffer );
 
 uint8_t parse_gps_header(const char* data_received, gps_response_header *result);
-uint8_t parse_bestxyza_data(const char* data_received, gps_response_header *result);
+uint8_t parse_bestxyza_data(const char* data_received, bestxyza_response *result);
 
 #endif // __INCLUDE_GUARD__GPS_H__
