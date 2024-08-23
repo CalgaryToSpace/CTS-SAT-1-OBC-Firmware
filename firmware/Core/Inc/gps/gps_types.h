4d66b0310a32b0f905e4c3d0fee3c400ee3b3643
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-08-22 21:54:10 -0600

    Changed my types to enums instead

--- firmware/Core/Inc/gps/gps_types.h
+++ firmware/Core/Inc/gps/gps_types.h
@@ -10,10 +10,20 @@
 /// @brief  This refers to the status that indicates how well a time is known
 /// All reported receiver times are subject to a qualifying time status
 /// Refer to page 51-53 of the OEM7 Commands and Logs Reference Manual
-typedef struct {
-    int gps_reference_time_status_decimal;
-    const char *gps_reference_time_status_ascii;
-} gps_reference_time_status;
+typedef enum {
+    GPS_UNKNOWN = 20,
+    GPS_APPROXIMATE = 60,
+    GPS_COARSEADJUSTING = 80,
+    GPS_COARSE = 100,
+    GPS_COARSESTEERING = 120,
+    GPS_FREEWHEELING = 130,
+    GPS_FINEADJUSTING = 140,
+    GPS_FINE = 160,
+    GPS_FINEBACKUPSTEERING = 170,
+    GPS_FINESTEERING = 180,
+    GPS_SATTIME = 200
+} GPS_reference_time_status_t;
+
 
 /// @brief This is the first part of the response that every command response from the GNSS receiver has.
 /// Refer to page 34-35 of the OEM7 Commands and Logs Reference Manual
@@ -31,7 +41,7 @@ typedef struct {
     uint32_t idle_time;
 
     /// @brief Indicates the quality of the GPS reference time
-    gps_reference_time_status time_status;
+    GPS_reference_time_status_t time_status;
 
     /// @brief GPS reference week number
     uint32_t week;
@@ -57,26 +67,73 @@ typedef struct {
 
 /// @brief  This refers to the solution status
 /// Refer to table 90 page 500-501 of the OEM7 Commands and Logs Reference Manual
-typedef struct {
-    int binary;
-    const char *gps_solution_status_ascii;
-} gps_solution_status;
+typedef enum {
+    GPS_SOL_COMPUTED = 0,
+    GPS_INSUFFICIENT_OBS = 1,
+    GPS_NO_CONVERGENCE = 2,
+    GPS_SINGULARITY = 3,
+    GPS_COV_TRACE = 4,
+    GPS_TEST_DIST = 5,
+    GPS_COLD_START = 6,
+    GPS_V_H_LIMIT = 7,
+    GPS_VARIANCE = 8,
+    GPS_RESIDUALS = 9,
+    GPS_RESERVED_10_12 = 10,  // Since 10-12 are reserved
+    GPS_INTEGRITY_WARNING = 13
+    // 14-17 are reserved
+} GPS_solution_status_enum_t;
+
 
 /// @brief  This refers to the position or velocity type
 /// Refer to table 91 page 501-503 of the OEM7 Commands and Logs Reference Manual
-typedef struct {
-    int gps_type_binary;
-    const char *gps_type_ascii;
-} gps_position_velocity_time_status;
+typedef enum {
+    GPS_TYPE_NONE = 0,
+    GPS_TYPE_FIXEDPOS = 1,
+    GPS_TYPE_FIXEDHEIGHT = 2,
+    GPS_TYPE_RESERVED_3_7 = 3,  // Values from 3 to 7 are reserved
+    GPS_TYPE_DOPPLER_VELOCITY = 8,
+    GPS_TYPE_RESERVED_9_15 = 9, // Values from 9 to 15 are reserved
+    GPS_TYPE_SINGLE = 16,
+    GPS_TYPE_PSDIFF = 17,
+    GPS_TYPE_WAAS = 18,
+    GPS_TYPE_PROPAGATED = 19,
+    GPS_TYPE_RESERVED_20_31 = 20, // Values from 20 to 31 are reserved
+    GPS_TYPE_L1_FLOAT = 32,
+    GPS_TYPE_RESERVED_33 = 33,
+    GPS_TYPE_NARROW_FLOAT = 34,
+    GPS_TYPE_RESERVED_35_47 = 35, // Values from 35 to 47 are reserved
+    GPS_TYPE_L1_INT = 48,
+    GPS_TYPE_WIDE_INT = 49,
+    GPS_TYPE_NARROW_INT = 50,
+    GPS_TYPE_RTK_DIRECT_INS = 51,
+    GPS_TYPE_INS_SBAS = 52,
+    GPS_TYPE_INS_PSRSP = 53,
+    GPS_TYPE_INS_PSRDIFF = 54,
+    GPS_TYPE_INS_RTKFLOAT = 55,
+    GPS_TYPE_INS_RTKFIXED = 56,
+    GPS_TYPE_RESERVED_57_67 = 57, // Values from 57 to 67 are reserved
+    GPS_TYPE_PPP_CONVERGING = 68,
+    GPS_TYPE_PPP = 69,
+    GPS_TYPE_OPERATIONAL = 70,
+    GPS_TYPE_WARNING = 71,
+    GPS_TYPE_OUT_OF_BOUNDS = 72,
+    GPS_TYPE_INS_PPP_CONVERGING = 73,
+    GPS_TYPE_INS_PPP = 74,
+    GPS_TYPE_PPP_BASIC_CONVERGING = 77,
+    GPS_TYPE_PPP_BASIC = 78,
+    GPS_TYPE_INS_PPP_BASIC_CONVERGING = 79,
+    GPS_TYPE_INS_PPP_BASIC = 80
+} GPS_position_velocity_type_enum_t;
+
 
 /// @brief This is the struct for the BESTXYZA Command response.
 /// Refer to page 515-517 of the OEM7 Commands and Logs Reference Manual
 typedef struct {
     /// @brief Position solution status
-    gps_solution_status position_solution_status;
+    GPS_solution_status_enum_t position_solution_status;
 
     /// @brief Position type.
-    gps_position_velocity_time_status position_type;
+    GPS_position_velocity_type_enum_t position_type;
 
     /// @brief Documentation says the point coordinates come as a double. 
     /// We store as an int32 because the eatrths radius(6.10^6 m) fits within an int32.
@@ -92,10 +149,10 @@ typedef struct {
     uint32_t position_z_std_m;
 
     /// @brief Velocity Solution status.
-    gps_solution_status velocity_solution_status;
+    GPS_solution_status_enum_t velocity_solution_status;
 
     /// @brief Velocity type. 
-    gps_position_velocity_time_status velocity_type;
+    GPS_position_velocity_type_enum_t velocity_type;
 
     /// @brief Documentation says the velocity vector come as a double. 
     /// The Velocity Vectors along the axes
