d5c67f674ca89aa783f8a1668918339a3b98bc15
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2024-08-26 22:05:20 -0600

    fixed bug in test code

--- firmware/Core/Src/transforms/sha256/sha256.c
+++ firmware/Core/Src/transforms/sha256/sha256.c
@@ -2,8 +2,6 @@
 #include "debug_tools/debug_uart.h"
 #include <stdint.h>
 
-
-// function is currently not working. This is most likely because the CRC periheral is not properly reset.
 uint8_t compute_sha256_hash(uint8_t* message, size_t message_length, uint8_t* digest) {
     if (cmox_initialize(NULL) != CMOX_INIT_SUCCESS)
     {
@@ -13,8 +11,7 @@ uint8_t compute_sha256_hash(uint8_t* message, size_t message_length, uint8_t* di
     }
 
     cmox_hash_retval_t ret;
-    size_t digest_size;
-    ret = cmox_hash_compute(CMOX_SHA256_ALGO, message, message_length , digest, CMOX_SHA256_SIZE, &digest_size);         
+    ret = cmox_hash_compute(CMOX_SHA256_ALGO, message, message_length , digest, CMOX_SHA256_SIZE, NULL);         
     
     if (ret != CMOX_HASH_SUCCESS)
     {
