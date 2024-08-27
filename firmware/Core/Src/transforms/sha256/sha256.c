8c32f0db922d7d0abbb05ef76aeb371a3ee84187
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2024-08-26 19:58:50 -0600

    added path to library for linking

--- firmware/Core/Src/transforms/sha256/sha256.c
+++ firmware/Core/Src/transforms/sha256/sha256.c
@@ -2,6 +2,8 @@
 #include "debug_tools/debug_uart.h"
 #include <stdint.h>
 
+
+// function is currently not working. This is most likely because the CRC periheral is not properly reset.
 uint8_t compute_sha256_hash(uint8_t* message, size_t message_length, uint8_t* digest) {
     if (cmox_initialize(NULL) != CMOX_INIT_SUCCESS)
     {
@@ -9,9 +11,11 @@ uint8_t compute_sha256_hash(uint8_t* message, size_t message_length, uint8_t* di
         DEBUG_uart_print_str("cmox crypto library initialization failed \n");
         return 1;
     }
+
     cmox_hash_retval_t ret;
     size_t digest_size;
     ret = cmox_hash_compute(CMOX_SHA256_ALGO, message, message_length , digest, CMOX_SHA256_SIZE, &digest_size);         
+    
     if (ret != CMOX_HASH_SUCCESS)
     {
         DEBUG_uart_print_str("hash failed \n");
