7c73d40e156580619f3ff01dc02b243951cb6066
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2024-08-26 22:05:20 -0600

    fixed bug in test code

--- firmware/Core/Src/unit_tests/test_sha256.c
+++ firmware/Core/Src/unit_tests/test_sha256.c
@@ -2,14 +2,15 @@
 #include "transforms/sha256.h"
 #include <string.h>
 #include "debug_tools/debug_uart.h"
-#include <stdalign.h>
+#include <stdint.h>
 uint8_t TEST_EXEC__compute_sha256_hash() {
-    const uint8_t message = "abc";
-    const uint8_t expected[] = "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad";
+    const uint8_t message[] = "abc" ;
+    const uint8_t expected[] = {0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 
+                                0x41, 0x41, 0x40, 0xde, 0x5d, 0xae, 0x22, 0x23, 
+                                0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 
+                                0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad};
     uint8_t digest[32];
     compute_sha256_hash(message, 3, digest);
-    DEBUG_uart_print_str("SHA256 TEST \n");
-    DEBUG_uart_print_array_hex(digest, 32);
     TEST_ASSERT_TRUE(memcmp(digest, expected, 32) == 0);
     return 0;
 }
