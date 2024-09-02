dac7d3eeb77d9d2c094a9cbb0afdad4285527074
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-09-02 09:52:47 -0600

    Initial internal flash bank telecommands

---
+++ firmware/Core/Src/telecommands/flash_bank_telecommands_defs.c
@@ -0,0 +1,12 @@
+#include "flash_bank_telecommands_defs.h"
+
+uint8_t TCMDEXEC_Flash_Bank_Write(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel, char *response_output_buf, uint16_t response_output_buf_len)
+{
+
+    return 0;
+}
+
+uint8_t TCMDEXEC_Flash_Bank_Read(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel, char *response_output_buf, uint16_t response_output_buf_len)
+{
+    return 0;
+}
