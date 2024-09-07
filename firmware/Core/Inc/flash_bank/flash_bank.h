d7cf1fe946238c867c9c6890ed45cff23d7938d0
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-09-07 10:30:51 -0600

    verify partitions, update comment

--- firmware/Core/Inc/flash_bank/flash_bank.h
+++ firmware/Core/Inc/flash_bank/flash_bank.h
@@ -10,8 +10,8 @@ typedef enum
     Flash_Partitions_RAM = 0x20000000,
     Flash_Partitions_RAM_2 = 0x10000000,
     Flash_Partitions_RAM_3 = 0x20040000,
-    Flash_Partitions_FLASH_BANK1 = 0x8000000,
-    Flash_Partitions_FLASH_BANK2 = 0x08100000 // default FLASH address
+    Flash_Partitions_FLASH_BANK1 = 0x8000000, // default FLASH address
+    Flash_Partitions_FLASH_BANK2 = 0x8100000
 } Flash_Partition_Adressess;
 
 uint32_t Flash_Bank_Write(uint32_t address, uint8_t *data, uint32_t length);
