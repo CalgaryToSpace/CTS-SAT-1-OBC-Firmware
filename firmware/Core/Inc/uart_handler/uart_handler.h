2b741d5155ca953bc97aa8ad7b4663527170440f
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-09-14 13:36:46 -0600

    Fixing rebase

--- firmware/Core/Inc/uart_handler/uart_handler.h
+++ firmware/Core/Inc/uart_handler/uart_handler.h
@@ -15,6 +15,12 @@ extern volatile uint16_t UART_eps_buffer_write_idx;
 extern volatile uint32_t UART_eps_last_write_time_ms;
 extern volatile uint8_t UART_eps_is_expecting_data; // Set to 1 when a command is sent, and we're awaiting a response
 
+extern const uint16_t UART_mpi_rx_buffer_len; // Length of the UART MPI response buffer (for telecommand responses only & NOT SCIENCE DATA)
+extern volatile uint8_t UART_mpi_rx_buffer[]; // Buffer for UART MPI response
+extern volatile uint8_t UART_mpi_rx_last_byte; // Last received byte from the MPI response
+extern volatile uint32_t UART_mpi_rx_last_byte_write_time_ms; // Last write time in milliseconds for MPI response
+extern volatile uint16_t UART_mpi_rx_buffer_write_idx; // Write index for MPI response buffer
+
 extern const uint16_t UART_gps_buffer_len; // Length of the UART telecommand buffer
 extern volatile uint8_t UART_gps_buffer[];          // Buffer for UART telecommands
 extern volatile uint16_t UART_gps_buffer_write_idx; // Write index for UART telecommand buffer
