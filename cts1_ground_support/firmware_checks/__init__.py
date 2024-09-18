dcf4e2226ed633c9c1986fcbfc89c70ec0ace53d
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-09-18 14:07:48 -0600

    Move the `cts1_ground_support` folder to its own repo

--- cts1_ground_support/firmware_checks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Checks for the `firmware` part of this project.
-
-These checks are written in Python, and use the documentation-parsing aspects of the
-`cts1_ground_support` package to verify that the firmware is correctly documented, as well as
-various other checks.
-"""
--- cts1_ground_support/src/cts1_ground_support/terminal_app/app_config.py
+++
@@ -1,5 +0,0 @@
-"""Configuration file for the terminal application."""
-
-# Constants
-UART_BAUD_RATE = 115200
-MAX_RX_TX_LOG_ENTRIES = 1000
