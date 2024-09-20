3caf738545337231ef3c806fc6f0fb0dc5ba0f39
vaibhav kapoor <vaibhav.kapoor@ucalgary.ca>
2024-09-19 23:15:30 -0600

    Updated to match naming conventions & code clean up post rebase

--- firmware/Core/Inc/uart_handler/uart_handler.h
+++ firmware/Core/Inc/uart_handler/uart_handler.h
@@ -2,53 +2,53 @@
 #ifndef __INCLUDE_GUARD__UART_HANDLER_H__
 #define __INCLUDE_GUARD__UART_HANDLER_H__
 
+#include "stm32l4xx_hal.h"
 #include <stdint.h>
 
-extern const uint16_t UART_telecommand_buffer_len; // Length of the UART telecommand buffer
-extern volatile uint8_t UART_telecommand_buffer[]; // Buffer for UART telecommands
-extern volatile uint16_t UART_telecommand_buffer_write_idx; // Write index for UART telecommand buffer
-extern volatile uint32_t UART_telecommand_last_write_time_ms; // Last write time in milliseconds for UART telecommand
+// Name the UART interfaces
+extern UART_HandleTypeDef *UART_telecommand_port_handle;  
+extern UART_HandleTypeDef *UART_mpi_port_handle;                
+extern UART_HandleTypeDef *UART_lora_port_handle;              
+extern UART_HandleTypeDef *UART_gps_port_handle;
+extern UART_HandleTypeDef *UART_camera_port_handle;
+extern UART_HandleTypeDef *UART_eps_port_handle;
+
+extern const uint16_t UART_telecommand_buffer_len;              // Length of the UART telecommand buffer
+extern volatile uint8_t UART_telecommand_buffer[];              // Buffer for UART telecommands
+extern volatile uint16_t UART_telecommand_buffer_write_idx;     // Write index for UART telecommand buffer
+extern volatile uint32_t UART_telecommand_last_write_time_ms;   // Last write time in milliseconds for UART telecommand
+
+extern const uint16_t UART_mpi_buffer_len;                      // Length of the UART MPI response buffer (for telecommand responses only & NOT SCIENCE DATA)
+extern volatile uint8_t UART_mpi_buffer[];                      // Buffer for UART MPI response
+extern volatile uint8_t UART_mpi_last_rx_byte;                  // Last received byte from the MPI response
+extern volatile uint32_t UART_mpi_last_write_time_ms;           // Last write time in milliseconds for MPI response
+extern volatile uint16_t UART_mpi_buffer_write_idx;             // Write index for MPI response buffer
+
+extern const uint16_t UART_lora_buffer_len;                     // Length of the LORA response buffer
+extern volatile uint8_t UART_lora_buffer[];                     // Buffer for LORA response
+extern volatile uint16_t UART_lora_buffer_write_idx;            // Write index for LORA response buffer
+extern volatile uint32_t UART_lora_last_write_time_ms;          // Last write time in milliseconds for LORA response
+extern volatile uint8_t UART_lora_is_expecting_data;            // Set to 1 when a data is sent, and we're awaiting a response
+extern volatile uint8_t UART_lora_buffer_last_rx_byte;          // Last received byte for LORA response
+
+extern const uint16_t UART_gps_buffer_len;                      // Length of the GPS response buffer
+extern volatile uint8_t UART_gps_buffer[];                      // Buffer for GPS response
+extern volatile uint16_t UART_gps_buffer_write_idx;             // Write index for GPS response buffer
+extern volatile uint32_t UART_gps_last_write_time_ms;           // Last write time in milliseconds for GPS response
+extern volatile uint8_t UART_gps_buffer_last_rx_byte;           // Last received byte for GPS response
+
+extern const uint16_t UART_camera_buffer_len;                   // Length of the CAMERA response buffer
+extern volatile uint8_t UART_camera_buffer[];                   // Buffer for CAMERA response
+extern volatile uint16_t UART_camera_buffer_write_idx;          // Write index for CAMERA response buffer
+extern volatile uint32_t UART_camera_last_write_time_ms;        // Last write time in milliseconds for CAMERA response
+extern volatile uint8_t UART_camera_is_expecting_data;          // Set to 1 when a data is sent, and we're awaiting a response
+extern volatile uint8_t UART_camera_buffer_last_rx_byte;        // Last received byte for CAMERA response
 
 extern const uint16_t UART_eps_buffer_len;
 extern volatile uint8_t UART_eps_buffer[];
 extern volatile uint16_t UART_eps_buffer_write_idx;
 extern volatile uint32_t UART_eps_last_write_time_ms;
 extern volatile uint8_t UART_eps_is_expecting_data; // Set to 1 when a command is sent, and we're awaiting a response
-
-// UART2 (MPI) config command response buffer
-extern const uint16_t UART_mpi_rx_buffer_len; // Length of the UART MPI response buffer (for telecommand responses only & NOT SCIENCE DATA)
-extern volatile uint8_t UART_mpi_rx_buffer[]; // Buffer for UART MPI response
-extern volatile uint8_t UART_mpi_rx_last_byte; // Last received byte from the MPI response
-extern volatile uint32_t UART_mpi_rx_last_byte_write_time_ms; // Last write time in milliseconds for MPI response
-extern volatile uint16_t UART_mpi_rx_buffer_write_idx; // Write index for MPI response buffer
-
-// UART2 (LORA) response buffer
-extern const uint16_t UART2_rx_buffer_len; // Length of the UART2 response buffer
-extern volatile uint8_t UART2_rx_buffer[]; // Buffer for UART2 response
-extern volatile uint16_t UART2_rx_buffer_write_idx; // Write index for UART2 response buffer
-extern volatile uint32_t UART2_rx_last_write_time_ms; // Last write time in milliseconds for UART2 response
-extern volatile uint8_t UART2_rx_buffer_last_rx_byte; // Last received byte for UART2 response
-
-// UART3 (GPS) response buffer
-extern const uint16_t UART3_rx_buffer_len; // Length of the UART3 response buffer
-extern volatile uint8_t UART3_rx_buffer[]; // Buffer for UART3 response
-extern volatile uint16_t UART3_rx_buffer_write_idx; // Write index for UART3 response buffer
-extern volatile uint32_t UART3_rx_last_write_time_ms; // Last write time in milliseconds for UART3 response
-extern volatile uint8_t UART3_rx_buffer_last_rx_byte; // Last received byte for UART3 response
-
-// UART4 (CAMERA) response buffer
-extern const uint16_t UART4_rx_buffer_len; // Length of the UART4 response buffer
-extern volatile uint8_t UART4_rx_buffer[]; // Buffer for UART4 response
-extern volatile uint16_t UART4_rx_buffer_write_idx; // Write index for UART4 response buffer
-extern volatile uint32_t UART4_rx_last_write_time_ms; // Last write time in milliseconds for UART4 response
-extern volatile uint8_t UART4_rx_buffer_last_rx_byte; // Last received byte for UART4 response
-
-// UART5 (EPS) response buffer
-extern const uint16_t UART5_rx_buffer_len; // Length of the UART5 response buffer
-extern volatile uint8_t UART5_rx_buffer[]; // Buffer for UART5 response
-extern volatile uint16_t UART5_rx_buffer_write_idx; // Write index for UART5 response buffer
-extern volatile uint32_t UART5_rx_last_write_time_ms; // Last write time in milliseconds for UART5 response
-extern volatile uint8_t UART5_rx_buffer_last_rx_byte; // Last received byte for UART5 response
 
 void UART_init_uart_handlers(void);
 
