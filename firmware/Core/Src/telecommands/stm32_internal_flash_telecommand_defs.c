31205b8df0866e9952dcbd11c81b0ae6903f0777
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-09-26 12:46:41 -0600

    Create API which can read, write, and erase the internal flash memory (#174)
    * Initial internal flash bank api
    
    * Initial internal flash bank telecommands
    
    * Update internal flash API, add telecommands
    
    * Update readiness level to ground useage only
    
    * Add comment for flash partition enum
    
    * add erase params to telecommand
    
    * print out data in hex
    
    * accept arbitrary number of bytes
    
    * Update comments for telecommands
    
    * verify partitions, update comment
    
    * Remove comments
    
    * update include pathing
    
    * update include paths
    
    * Update comments for internal flash memory functions
    
    * rename functions to match standard
    
    * Rename files to stm32_internal_flash
    
    * Update Flash memory region address enum names
    
    * clean up write function, update docs, remove dead lines, update return
    
    * update read function, remove unnecessary address condition, update doc
    
    * update docs for function, clean up error handling for erase
    
    * slight doc update
    
    * remove unnecessary null terminator addition
    
    * update comments and return types
    
    * give status default value
    
    * add comma, update section header
    
    * Update internal flash tcmds to take starting address
    
    * Update contributers list
    
    * update function names
    
    * update return on writing to flash
    
    * Update docs, return explicit codes for writing to flash
    
    * Update erase function returns
    
    * add casting to fix warnings
    
    * Update function names to follow naming guidelines
    
    * rename folders and files
    
    * update includes and define guards
    
    * update comments, update function names
    
    * rename functions to captalize stm
    
    * Use struct to track errors in writing to flash
    
    * Update comment
    
    * Add include
    
    * Update Deployment Final Check

---
+++ firmware/Core/Src/telecommands/stm32_internal_flash_telecommand_defs.c
@@ -0,0 +1,117 @@
+#include "telecommands/stm32_internal_flash_telecommand_defs.h"
+#include "telecommands/telecommand_args_helpers.h"
+#include "stm32_internal_flash_drivers/stm32_internal_flash_drivers.h"
+
+#include "stm32l4xx_hal.h"
+#include <stdio.h>
+#include <string.h>
+
+/// @brief Write data to the internal flash bank starting from address 0x08100000
+/// @param args_str
+/// - Arg 0: The data in hex format to write
+/// - Arg 1: The offset to start writing from
+/// @note This telecommand is only for testing purposes, it is purposfully not fully fleshed out
+/// as there is no intention on using this. Update as needed
+//// @return 0 on success, > 0 on error
+uint8_t TCMDEXEC_stm32_internal_flash_write(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel, char *response_output_buf, uint16_t response_output_buf_len)
+{
+    uint8_t write_hex_buffer[PAGESIZE] = {0};
+    uint16_t write_hex_buffer_len = 0;
+    const uint8_t parse_hex_buffer_res = TCMD_extract_hex_array_arg(args_str, 0, write_hex_buffer, sizeof(write_hex_buffer), &write_hex_buffer_len);
+    if (parse_hex_buffer_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error Parsing Arg 0: %u", parse_hex_buffer_res);
+        return 1;
+    }
+
+    uint64_t offset = 0;
+    const uint8_t parse_offset_res = TCMD_extract_uint64_arg(args_str, strlen(args_str), 1, &offset);
+    if (parse_offset_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error Parsing Arg 1: %u", parse_offset_res);
+        return 1;
+    }
+
+    STM32_Internal_Flash_Write_Status_t status;
+    const uint8_t write_res = STM32_internal_flash_write(STM32_INTERNAL_FLASH_MEMORY_REGION_GOLDEN_COPY_ADDRESS + offset, write_hex_buffer, write_hex_buffer_len, &status);
+    if (write_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error writing to flash: %u\nLock Status: %u\nUnlock Status: %u\nWrite Status: %u", write_res, status.lock_status, status.unlock_status, status.write_status);
+        return 1;
+    }
+    return 0;
+}
+
+/// @brief Read data from the internal flash bank
+/// @param args_str
+/// - Arg 0: The address to start reading from
+/// - Arg 1: The number of bytes to read as a uint64_t
+//// @return 0 on success, > 0 on error
+uint8_t TCMDEXEC_stm32_internal_flash_read(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel, char *response_output_buf, uint16_t response_output_buf_len)
+{
+
+    uint64_t address = 0;
+    const uint8_t parse_address_res = TCMD_extract_uint64_arg(args_str, strlen(args_str), 0, &address);
+    if (parse_address_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error Parsing Arg 0: %u", parse_address_res);
+        return 1;
+    }
+
+    uint64_t number_of_bytes_to_read = 0;
+    const uint8_t parse_res = TCMD_extract_uint64_arg(args_str, strlen(args_str), 1, &number_of_bytes_to_read);
+    if (parse_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error Parsing Arg 1: %u", parse_res);
+        return 1;
+    }
+
+    uint8_t read_buffer[number_of_bytes_to_read];
+    const uint8_t res = STM32_internal_flash_read(address, read_buffer, sizeof(read_buffer));
+    if (res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error: %u", res);
+        return 1;
+    }
+    for (uint64_t i = 0; i < number_of_bytes_to_read; i++)
+    {
+        snprintf(response_output_buf + (i * 2), response_output_buf_len - (i * 2), "%02X", read_buffer[i]);
+    }
+
+    return 0;
+}
+
+/// @brief Erase a range of pages in the internal flash bank.
+/// Only Erases for Flash Bank 2.
+/// @param args_str
+/// - Arg 0: The starting page to erase as a uint64_t
+/// - Arg 1: The number of pages to erase as a uint64_t
+//// @return 0 on success, > 0 on error
+uint8_t TCMDEXEC_stm32_internal_flash_erase(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel, char *response_output_buf, uint16_t response_output_buf_len)
+{
+    uint64_t start_page_erase = 0;
+
+    const uint8_t start_page_erase_parse_res = TCMD_extract_uint64_arg(args_str, strlen(args_str), 0, &start_page_erase);
+    if (start_page_erase_parse_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error Parsing arg 0: %u", start_page_erase_parse_res);
+        return 1;
+    }
+
+    uint64_t number_of_pages_to_erase = 0;
+    const uint8_t number_of_pages_to_erase_parse_res = TCMD_extract_uint64_arg(args_str, strlen(args_str), 1, &number_of_pages_to_erase);
+    if (number_of_pages_to_erase_parse_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error Parsing arg 1: %u", number_of_pages_to_erase_parse_res);
+        return 1;
+    }
+
+    uint32_t page_error = 0;
+    const uint8_t erase_res = STM32_internal_flash_erase((uint16_t)start_page_erase, (uint16_t)number_of_pages_to_erase, &page_error);
+    if (erase_res != 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Error erasing pages: %u - %u, error: %u, page error: %lu", (uint16_t)start_page_erase, (uint16_t)start_page_erase + (uint16_t)number_of_pages_to_erase, erase_res, page_error);
+        return 1;
+    }
+    return 0;
+}
--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -20,10 +20,11 @@
 #include "telecommands/telecommand_executor.h"
 #include "telecommands/agenda_telecommands_defs.h"
 #include "telecommands/mpi_telecommand_defs.h"
+#include "telecommands/eps_telecommands.h"
+#include "telecommands/stm32_internal_flash_telecommand_defs.h"
 #include "timekeeping/timekeeping.h"
 #include "littlefs/littlefs_helper.h"
 #include "stm32/stm32_reboot_reason.h"
-#include "telecommands/eps_telecommands.h"
 
 #include <stdio.h>
 #include <stdint.h>
@@ -697,11 +698,11 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
 
-   // ****************** END SECTION: log_telecommand_defs ******************
+    // ****************** END SECTION: log_telecommand_defs ******************
 
-   // ****************** SECTION: freertos_telecommand_defs ******************
+    // ****************** SECTION: freertos_telecommand_defs ******************
 
-   {
+    {
         .tcmd_name = "freetos_list_tasks_jsonl",
         .tcmd_func = TCMDEXEC_freetos_list_tasks_jsonl,
         .number_of_args = 0,
@@ -828,7 +829,7 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
     
     // ****************** SECTION: agenda_telecommand_defs ******************
 
-   {
+    {
         .tcmd_name = "agenda_delete_all",
         .tcmd_func = TCMDEXEC_agenda_delete_all,
         .number_of_args = 0,
@@ -841,7 +842,7 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .number_of_args = 1,
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
-    
+
     {
         .tcmd_name = "agenda_fetch_jsonl",
         .tcmd_func = TCMDEXEC_agenda_fetch_jsonl,
@@ -866,6 +867,30 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION
     },
     // ****************** END: MPI_telecommand_definitions ********************
+    // ****************** START SECTION: stm32_internal_flash_telecommand_defs ******************
+
+    {
+        .tcmd_name = "stm32_internal_flash_read",
+        .tcmd_func = TCMDEXEC_stm32_internal_flash_read,
+        .number_of_args = 2,
+        .readiness_level = TCMD_READINESS_LEVEL_GROUND_USAGE_ONLY,
+    },
+
+    {
+        .tcmd_name = "stm32_internal_flash_write",
+        .tcmd_func = TCMDEXEC_stm32_internal_flash_write,
+        .number_of_args = 2,
+        .readiness_level = TCMD_READINESS_LEVEL_GROUND_USAGE_ONLY,
+    },
+
+    {
+        .tcmd_name = "stm32_internal_flash_erase",
+        .tcmd_func = TCMDEXEC_stm32_internal_flash_erase,
+        .number_of_args = 2,
+        .readiness_level = TCMD_READINESS_LEVEL_GROUND_USAGE_ONLY,
+    },
+
+    // ****************** END SECTION: stm32_internal_flash_telecommand_defs ******************
 
     // ****************** SECTION: antenna_telecommand_defs ******************
     {
@@ -1037,8 +1062,8 @@ uint8_t TCMDEXEC_available_telecommands(const char *args_str, TCMD_TelecommandCh
 
 uint8_t TCMDEXEC_reboot(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                         char *response_output_buf, uint16_t response_output_buf_len) {
-    DEBUG_uart_print_str("Rebooting by telecommand request...\n"); 
-    
+    DEBUG_uart_print_str("Rebooting by telecommand request...\n");
+
     // Delay to flush UART buffer
     HAL_Delay(100);
 
