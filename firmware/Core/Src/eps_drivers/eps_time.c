1d3fdaa0e3a57aac2603e755d53c3902dfd77231
Makiv1 <mvujic@hotmail.com>
2024-10-10 19:11:19 -0600

    Fixed requested changes in pr

--- firmware/Core/Src/eps_drivers/eps_time.c
+++ firmware/Core/Src/eps_drivers/eps_time.c
@@ -1,13 +1,23 @@
 #include "eps_drivers/eps_commands.h"
 #include "timekeeping/timekeeping.h"
+
+/**
+ * @brief Sync's eps time to obc time (+/- 1 second)
+ * @return 0 on success, > 0 on failure.
+ */
 uint8_t EPS_TIME_sync_eps_time_to_obc_time() {
     EPS_struct_system_status_t status;
     uint8_t result = EPS_CMD_get_system_status(&status);
-    if (result != 0) return result;
+    if (result != 0) {
+        return 1;
+    }
     
-    int64_t delta_seconds = (int64_t)((float)TIM_get_current_unix_epoch_time_ms() / 1000.0f )
-                            -(int64_t)(status.unix_time_sec);
+    int64_t delta_ms = TIM_get_current_unix_epoch_time_ms() - ((uint64_t)status.unix_time_sec * 1000);
+    int32_t delta_seconds = delta_ms / 1000;
                             
     result = EPS_CMD_correct_time(delta_seconds);
-    return result;
+    if (result != 0) {
+        return 2;
+    }
+    return 0;
 }
