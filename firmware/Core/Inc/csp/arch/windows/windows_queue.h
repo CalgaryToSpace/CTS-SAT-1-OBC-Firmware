2281d7a3f3526a28c124407f73c327d17635107f
parker-research <166864283+parker-research@users.noreply.github.com>
2024-10-21 20:37:30 -0600

    Add part to move .h files to Inc folder

--- firmware/Core/Inc/csp/arch/windows/windows_queue.h
+++ firmware/Core/Inc/csp/arch/windows/windows_queue.h
@@ -1,7 +1,7 @@
 /*
 Cubesat Space Protocol - A small network-layer protocol designed for Cubesats
 Copyright (C) 2012 Gomspace ApS (http://www.gomspace.com)
-Copyright (C) 2012 AAUSAT3 Project (http://aausat3.space.aau.dk)
+Copyright (C) 2012 AAUSAT3 Project (http://aausat3.space.aau.dk) 
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -18,20 +18,27 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _SRC_CSP_PROMISC_H_
-#define _SRC_CSP_PROMISC_H_
+#ifndef _WINDOWS_QUEUE_H_
+#define _WINDOWS_QUEUE_H_
 
-#include <csp/csp_promisc.h>
+#include <csp/arch/csp_queue.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/**
- * Add packet to promiscuous mode packet queue
- * @param packet Packet to add to the queue
- */
-void csp_promisc_add(csp_packet_t * packet);
+#define WINDOWS_QUEUE_ERROR CSP_QUEUE_ERROR
+#define WINDOWS_QUEUE_EMPTY CSP_QUEUE_ERROR
+#define WINDOWS_QUEUE_FULL CSP_QUEUE_ERROR
+#define WINDOWS_QUEUE_OK CSP_QUEUE_OK
+
+typedef struct windows_queue_s windows_queue_t;
+
+windows_queue_t * windows_queue_create(int length, size_t item_size);
+void windows_queue_delete(windows_queue_t * q);
+int windows_queue_enqueue(windows_queue_t * queue, const void * value, int timeout);
+int windows_queue_dequeue(windows_queue_t * queue, void * buf, int timeout);
+int windows_queue_items(windows_queue_t * queue);
 
 #ifdef __cplusplus
 }
