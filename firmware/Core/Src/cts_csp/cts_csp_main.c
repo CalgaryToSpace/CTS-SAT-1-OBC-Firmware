b6d942a070e9efbc75a7b71c7e60133b34d1a0c3
parker-research <166864283+parker-research@users.noreply.github.com>
2024-10-21 20:21:17 -0600

    Set include paths better

--- firmware/Core/Src/cts_csp/cts_csp_main.c
+++ firmware/Core/Src/cts_csp/cts_csp_main.c
@@ -1,8 +1,8 @@
 #include "cts_csp/cts_csp_main.h"
 
 #include "csp/csp.h"
-#include "csp_init.h" // FIXME: why doesn't it like "csp/csp_init.h"? Because it's in the Src folder right now. 
-#include "csp_conn.h"
+#include "csp/csp_init.h" // FIXME: why doesn't it like "csp/csp_init.h"? Because it's in the Src folder right now. 
+#include "csp/csp_conn.h"
 
 #include "log/log.h"
 
