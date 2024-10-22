603f8e01d6ef35b0b286dce3aaf80f4f5790c34c
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-10-22 01:16:41 +0000

    IOC: Enable ST Cryptolib, related fixes (#217)
    * Convert number_comparisons to not use math.h
    
    * IOC: Enable ST Cryptolib
    
    Steps:
    1. Open CubeMX.
    2. Go to Software Packs > Select Software Packs.
    3. Enable `STMicroelectronics.X-CUBE-SAFEA1`
    4. Enable `ST_Cryptolib`.
    5. Click Ok.
    6. Go to "Middleware and Software Packs".
    7. Enable checkbox in `X-CUBE-SAFEA1`.
    
    * Add Cryptolib parts to CI Makefile
    
    #### Post-IOC Code Generation Steps:
    
    * Revert linker script change
    
    * Remove `x509.c` and `x509_crypto.c` bad files

--- firmware/Core/Src/transforms/number_comparisons.c
+++ firmware/Core/Src/transforms/number_comparisons.c
@@ -1,16 +1,27 @@
 #include "transforms/number_comparisons.h"
-#include <math.h>
+
+/// @brief Returns the absolute value of a.
+/// @param a 
+/// @return The absolute value of `a`.
+/// @note This is a reimplementation of `fabs` in C, because Cryptolib creates a file named math.h.
+double GEN_double_abs(double a) {
+    if (a < 0) {
+        return -a;
+    }
+    return a;
+}
+
 
 /// @brief Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
 /// @return Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
 uint8_t GEN_compare_doubles(double a, double b, double epsilon)
 {
-    return fabs(a - b) < epsilon;
+    return GEN_double_abs(a - b) < epsilon;
 }
 
 /// @brief Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
 /// @return Returns 1 if a and b are approximately equal to each other (within epsilon), 0 otherwise
 uint8_t GEN_compare_floats(float a, float b, float epsilon)
 {
-    return fabs(a - b) < epsilon;
+    return GEN_double_abs(a - b) < epsilon;
 }
---
+++ firmware/X-CUBE-SAFEA1/Target/math.h
@@ -0,0 +1,33 @@
+/**
+  ******************************************************************************
+  * @file           :  math.h
+  * @brief          :  This file is to aviod clash between system math.h
+  *                    math.h for X-Cube-Cryptolib.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MATH_H_
+#define __MATH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef float float_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MATH_H_ */
