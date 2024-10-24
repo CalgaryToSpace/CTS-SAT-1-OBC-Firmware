51ffc9dcb3bb780b3857d06d35c8cfde8d2d5427
Makiv1 <mvujic@hotmail.com>
2024-10-24 13:33:41 -0600

    Fix github actions makefile

--- firmware/Makefile.GitHub.mk
+++ firmware/Makefile.GitHub.mk
@@ -154,7 +154,8 @@ C_INCLUDES =  \
 -IMiddlewares/ST/ST_Cryptolib/Inc/TDES/CBC \
 -IMiddlewares/ST/ST_Cryptolib/Inc/TDES/Common \
 -IMiddlewares/ST/ST_Cryptolib/Inc/TDES/ECB \
--IMiddlewares/ST/ST_Cryptolib/X509/Inc
+-IMiddlewares/ST/ST_Cryptolib/X509/Inc \
+-IMiddlewares/Third_Party/STM32_Cryptographic/include \
 
 
 # compile gcc flags
