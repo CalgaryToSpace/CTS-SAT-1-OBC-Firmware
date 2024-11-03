a3a11710e405f1dcaaf8e7dc74b2a9421507de0c
KaleF07 <kalefordham@gmail.com>
2024-11-03 15:07:55 -0800

    Updated some comments and error messages for clarity

--- firmware/Core/Inc/telecommands/telecommand_parser.h
+++ firmware/Core/Inc/telecommands/telecommand_parser.h
@@ -10,6 +10,7 @@
 
 #define TCMD_PREFIX_STR "CTS1+"
 #define TCMD_PREFIX_STR_LEN 5
+#define TCMD_HEX_ARRAY_LEN 32
 
 uint8_t TCMD_is_char_alphanumeric(char c);
 uint8_t TCMD_is_char_valid_telecommand_name_char(char c);
