721f1692dda08b9fd0b866319b0918fbfd15545e
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-11-12 21:31:56 -0700

    Remove redundant command: `TCMDEXEC_eps_sync_time_to_obc_time`

--- firmware/Core/Src/telecommands/eps_telecommands.c
+++ firmware/Core/Src/telecommands/eps_telecommands.c
@@ -520,20 +520,3 @@ uint8_t TCMDEXEC_eps_get_piu_housekeeping_data_run_avg_json(
     }
     return 0;
 }
-
-/// @brief Sync's eps time to obc time (+/- 1 second)
-/// @return 0 on success, >0 on failure.
-uint8_t TCMDEXEC_eps_sync_time_to_obc_time(
-    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
-    char *response_output_buf, uint16_t response_output_buf_len) 
-{
-    const uint8_t result = EPS_set_eps_time_based_on_obc_time();
-    if (result != 0 ) {
-        snprintf(response_output_buf, response_output_buf_len,
-        "syncing eps time failed");
-        return 1;
-    }
-    snprintf(response_output_buf, response_output_buf_len,
-    "success syncing eps time");
-    return 0;
-}
