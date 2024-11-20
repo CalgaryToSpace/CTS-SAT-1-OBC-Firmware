f500cd72c986eaec752f0dcda79887dcc99f1f4d
christopherkinyua <christopher.kinyua@ucalgary.ca>
2024-11-19 20:25:29 -0700

    Created log command verification function. Yet to implement

--- firmware/Core/Inc/gps/gps_internal_drivers.h
+++ firmware/Core/Inc/gps/gps_internal_drivers.h
@@ -7,4 +7,6 @@
 uint8_t GPS_send_cmd_get_response(const char *cmd_buf, uint8_t cmd_buf_len, uint8_t rx_buf[],
                                   uint16_t rx_buf_len, const uint16_t rx_buf_max_size);
 
+uint8_t GPS_validate_log_response(const char *gps_response_buf);
+
 #endif /* INCLUDE_GUARD__GPS_INTERNAL_DRIVERS_H__ */
