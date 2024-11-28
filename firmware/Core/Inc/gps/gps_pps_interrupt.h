f7c9a59b060fc3eef26f9988058a7c4470ae847e
2BlackCats <903036002alex@gmail.com>
2024-11-28 13:26:30 -0700

    Simplified code and made functions so values can not be changed except through interrupt
    Added additional functions to ensure variable are private and can't be changed through other functions.

--- firmware/Core/Inc/gps/gps_pps_interrupt.h
+++ firmware/Core/Inc/gps/gps_pps_interrupt.h
@@ -4,13 +4,24 @@
 
 #include "stm32l4xx_hal.h"
 
-extern uint32_t pps_counter;
-extern volatile uint8_t gps_pps_enabled;
+uint32_t pps_counter;
+
+uint8_t gps_pps_enabled;
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
 
 void GPIO_EXTI_Init(void);
 
 void Disable_GPIO_EXTI(void);
+
+uint8_t get_gps_pps_enabled(void);
+
+void set_gps_pps_enabled(void);
+
+void set_gps_pps_disabled(void);
+
+uint32_t get_pps_counter(void);
+
+void reset_pps_counter(void);
 
 #endif // __INCLUDE_GUARD__GPS_PPS_INTERRUPT_H
