7f7f9baaa33e9fecb040c882c74afa6a69a2be7d
Makiv1 <mvujic@hotmail.com>
2024-11-30 14:42:30 -0700

    Create bootup task which runs bootup procedure

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -123,6 +123,13 @@ const osThreadAttr_t TASK_time_sync_Attributes = {
   .priority = (osPriority_t) osPriorityNormal, //TODO: Figure out which priority makes sense for this task
 };
 
+osThreadId_t TASK_bootup_Handle;
+const osThreadAttr_t TASK_bootup_Attributes = {
+  .name = "TASK_bootup",
+  .stack_size = 1024, //in bytes. 512 is tool small and will cause crashes when lfs_close() is called!
+  .priority = (osPriority_t) osPriorityNormal, //TODO: Figure out which priority makes sense for this task
+};
+
 osThreadId_t TASK_monitor_freertos_memory_Handle;
 const osThreadAttr_t TASK_monitor_freertos_memory_Attributes = {
   .name = "TASK_monitor_freertos_memory",
@@ -259,7 +266,6 @@ int main(void)
   // Initialise the ADCS CRC8 checksum (required for ADCS operation).
   ADCS_initialise_crc8_checksum();
 
-  START_antenna_deploy();
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -297,6 +303,8 @@ int main(void)
   TASK_service_eps_watchdog_Handle = osThreadNew(TASK_service_eps_watchdog, NULL, &TASK_service_eps_watchdog_Attributes);
 
   TASK_time_sync_Handle = osThreadNew(TASK_time_sync, NULL, &TASK_time_sync_Attributes);
+
+  TASK_bootup_Handle = osThreadNew(TASK_bootup, NULL, &TASK_bootup_Attributes);
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
