ed56aacc6fe51e6f426fd817e2b5e2b520afcbe2
NuclearTea <muhammad.ali4@ucalgary.ca>
2024-12-07 21:39:59 -0700

    Update bootloader to reset all peripherals

--- firmware/Core/Src/bootloader/bootloader.c
+++ firmware/Core/Src/bootloader/bootloader.c
@@ -10,74 +10,121 @@ typedef void (*pFunction)(void);
 void BOOTLOADER_deinitialize_peripherals()
 {
 
-    HAL_UART_DeInit(&huart1);
-    HAL_UART_DeInit(&huart2);
-    HAL_UART_DeInit(&huart3);
-    HAL_UART_DeInit(&huart4);
-    HAL_UART_DeInit(&huart5);
+    // Disable all interrupts
+    __disable_irq();
 
+    // Deinitialize all peripherals
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOF, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOG, GPIO_PIN_All);
+    HAL_GPIO_DeInit(GPIOH, GPIO_PIN_All);
+
+    HAL_DMA_DeInit(&hdma_uart4_rx);  // Replace with your DMA handlers
+    HAL_DMA_DeInit(&hdma_usart1_rx);  // Replace with your DMA handlers
+    
     HAL_I2C_DeInit(&hi2c1);
     HAL_I2C_DeInit(&hi2c2);
     HAL_I2C_DeInit(&hi2c3);
     HAL_I2C_DeInit(&hi2c4);
-
+    HAL_UART_DeInit(&hlpuart1);
+    HAL_UART_DeInit(&huart4);
+    HAL_UART_DeInit(&huart5);
+    HAL_UART_DeInit(&huart1);
+    HAL_UART_DeInit(&huart2);
+    HAL_UART_DeInit(&huart3);
     HAL_SPI_DeInit(&hspi1);
-
-    // think about adding hcrc in main.c to main.h
-    // HAL_CRC_DeInit(&hcrc);
-
+    HAL_CRC_DeInit(&hcrc);
     HAL_TIM_Base_DeInit(&htim16);
 
-    // Disable all interrupts
-    __disable_irq();
+    // Reset all peripherals
+    __HAL_RCC_GPIOA_FORCE_RESET();
+    __HAL_RCC_GPIOB_FORCE_RESET();
+    __HAL_RCC_GPIOC_FORCE_RESET();
+    __HAL_RCC_GPIOD_FORCE_RESET();
+    __HAL_RCC_GPIOE_FORCE_RESET();
+    __HAL_RCC_GPIOF_FORCE_RESET();
+    __HAL_RCC_GPIOG_FORCE_RESET();
+    __HAL_RCC_GPIOH_FORCE_RESET();
+    __HAL_RCC_DMA1_FORCE_RESET();
+    __HAL_RCC_DMA2_FORCE_RESET();
+    __HAL_RCC_I2C1_FORCE_RESET();
+    __HAL_RCC_I2C2_FORCE_RESET();
+    __HAL_RCC_I2C3_FORCE_RESET();
+    __HAL_RCC_I2C4_FORCE_RESET();
+    __HAL_RCC_LPUART1_FORCE_RESET();
+    __HAL_RCC_UART4_FORCE_RESET();
+    __HAL_RCC_UART5_FORCE_RESET();
+    __HAL_RCC_USART1_FORCE_RESET();
+    __HAL_RCC_USART2_FORCE_RESET();
+    __HAL_RCC_USART3_FORCE_RESET();
+    __HAL_RCC_SPI1_FORCE_RESET();
+    __HAL_RCC_CRC_FORCE_RESET();
+    __HAL_RCC_TIM16_FORCE_RESET();
 
-    // Reset the RCC clock configuration to the default reset state
-    HAL_RCC_DeInit();
+    // Release peripheral resets
+    __HAL_RCC_GPIOA_RELEASE_RESET();
+    __HAL_RCC_GPIOB_RELEASE_RESET();
+    __HAL_RCC_GPIOC_RELEASE_RESET();
+    __HAL_RCC_GPIOD_RELEASE_RESET();
+    __HAL_RCC_GPIOE_RELEASE_RESET();
+    __HAL_RCC_GPIOF_RELEASE_RESET();
+    __HAL_RCC_GPIOG_RELEASE_RESET();
+    __HAL_RCC_GPIOH_RELEASE_RESET();
+    __HAL_RCC_DMA1_RELEASE_RESET();
+    __HAL_RCC_DMA2_RELEASE_RESET();
+    __HAL_RCC_I2C1_RELEASE_RESET();
+    __HAL_RCC_I2C2_RELEASE_RESET();
+    __HAL_RCC_I2C3_RELEASE_RESET();
+    __HAL_RCC_I2C4_RELEASE_RESET();
+    __HAL_RCC_LPUART1_RELEASE_RESET();
+    __HAL_RCC_UART4_RELEASE_RESET();
+    __HAL_RCC_UART5_RELEASE_RESET();
+    __HAL_RCC_USART1_RELEASE_RESET();
+    __HAL_RCC_USART2_RELEASE_RESET();
+    __HAL_RCC_USART3_RELEASE_RESET();
+    __HAL_RCC_SPI1_RELEASE_RESET();
+    __HAL_RCC_CRC_RELEASE_RESET();
+    __HAL_RCC_TIM16_RELEASE_RESET();
 
-    // Deinitialize the HAL library
-    HAL_DeInit();
-    // Disable the SysTick interrupt and timer
+    // Reset the system tick
     SysTick->CTRL = 0;
     SysTick->LOAD = 0;
     SysTick->VAL = 0;
+
+    // Deinitialize the HAL
+    HAL_RCC_DeInit();
+    HAL_DeInit();
+
 }
 
 void BOOTLOADER_Jump_To_Application(uint32_t address)
 {
-    // // Check if there is a valid stack pointer in the app's vector table (first address)
-    // uint32_t appStack = *(__IO uint32_t *)address;
-    // if (((appStack & 0x2FFE0000) == 0x20000000) || (((appStack & 0x1FFF8000) == 0x10000000)))
-    // { // Check if within valid SRAM range
+    pFunction JumpToApp;    
 
-    //     BOOTLOADER_deinitialize_peripherals();
+    // Backup firmware's vector table address
+    uint32_t BackupSP = *(__IO uint32_t *)(address);
+    uint32_t BackupResetHandler = *(__IO uint32_t *)(address + 4);
 
-    //     // // Set the Vector Table Offset Register (VTOR) to the application's vector table
-    //     // SCB->VTOR = address;
+    // Validate the backup firmware
+    if (BackupSP == 0xFFFFFFFF || BackupResetHandler == 0xFFFFFFFF) {
+        // Invalid firmware, do nothing
+        return;
+    }
 
-    //     // // Load the new stack pointer and jump to the reset handler (address + 4)
-    //     // __set_MSP(*(__IO uint32_t *)address);                              // Set the stack pointer
-    //     // pFunction appEntry = (pFunction)(*(__IO uint32_t *)(address + 4)); // Reset handler
-    //     // appEntry();                                                        // Jump to the application
+    // Set vector table to backup firmware
+    SCB->VTOR = address;
 
-    //     // This should never be reached if the jump succeeds
-    // }
-    // else
-    // {
-    //     // Invalid stack pointer, handle error (e.g., blink an LED or reset)
-    //     while (1)
-    //     {
-    //         // Error loop
-    //     }
-    // }
-    BOOTLOADER_deinitialize_peripherals();
-    // const uint32_t first = address & 0xFFFF0000;
-    // const uint32_t second = address & 0x0000FFFF;
-    const Bootloader_Jump_To_Application_Args_t *vector_p = (Bootloader_Jump_To_Application_Args_t *)(address);
-    // asm("msr msp, %0; bx %1;" : : "r"(first), "r"(second));
-    __set_MSP(address);
-    pFunction appEntry = (pFunction)(*(__IO uint32_t *)(vector_p->function_ptr)); // Reset handler
-    appEntry();                                                                   // Jump to the application
-    // asm("msr msp, %0; bx %1;" : : "r"(vector_p->address), "r"(vector_p->function_ptr));
+    // Set the Main Stack Pointer
+    __set_MSP(BackupSP);
+
+    // Jump to the backup reset handler
+    JumpToApp = (pFunction)BackupResetHandler;
+    JumpToApp();
+
 }
 
 void BOOTLOADER_Jump_To_Golden_Copy()
