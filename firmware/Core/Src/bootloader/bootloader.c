c8202f5272f85f6d38c7b3d8c4f0949bcca5e6d2
NuclearTea <muhammad.ali4@ucalgary.ca>
2024-12-15 16:47:27 -0700

    Remove unnecessary functions, got jump working

--- firmware/Core/Src/bootloader/bootloader.c
+++ firmware/Core/Src/bootloader/bootloader.c
@@ -12,135 +12,12 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#include "debug_uart.h"
-
-void Deinitialize_RTOS(void) 
-{
-    // start backwards so that we eliminate the  default task handle last
-    for (int8_t task_idx = FREERTOS_task_handles_array_size - 1; task_idx >= 0; task_idx--) {
-        FREERTOS_task_info_struct_t task = FREERTOS_task_handles_array[task_idx];
-    
-        if (task.task_handle != NULL && *task.task_handle != NULL) {
-            vTaskDelete(*task.task_handle);
-            task.task_handle = NULL;
-            task.lowest_stack_bytes_remaining = UINT32_MAX;
-        }
-    }
-
-    // osKernelLock();
-    // osKernelSuspend(); // Stop the FreeRTOS scheduler
-    // osKernelDeinitialize(); // Deinitialize the kernel
-}
-
-
-void BOOTLOADER_deinitialize_peripherals()
-{
-    
-
-    
-
-    // // Reset all peripherals
-    
+#include "log.h"
 
 
 
-    HAL_TIM_Base_DeInit(&htim16);
 
-    HAL_CRC_DeInit(&hcrc);
-
-    HAL_SPI_DeInit(&hspi1);
-
-
-    HAL_UART_DeInit(&hlpuart1);
-    HAL_UART_DeInit(&huart4);
-    HAL_UART_DeInit(&huart5);
-    HAL_UART_DeInit(&huart1);
-    HAL_UART_DeInit(&huart2);
-    HAL_UART_DeInit(&huart3);
-
-    HAL_I2C_DeInit(&hi2c1);
-    HAL_I2C_DeInit(&hi2c2);
-    HAL_I2C_DeInit(&hi2c3);
-    HAL_I2C_DeInit(&hi2c4);
-
-
-    HAL_DMA_DeInit(&hdma_uart4_rx);  
-    HAL_DMA_DeInit(&hdma_usart1_rx);  
-
-    
-
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOF, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOG, GPIO_PIN_All);
-    HAL_GPIO_DeInit(GPIOH, GPIO_PIN_All);
-
-    // Reset the system tick
-    SysTick->CTRL = 0;
-    SysTick->LOAD = 0;
-    SysTick->VAL = 0;
-
-    // Deinitialize the HAL
-    HAL_RCC_DeInit();
-    HAL_DeInit();
-
-
-__HAL_RCC_GPIOA_FORCE_RESET();
-    __HAL_RCC_GPIOB_FORCE_RESET();
-    __HAL_RCC_GPIOC_FORCE_RESET();
-    __HAL_RCC_GPIOD_FORCE_RESET();
-    __HAL_RCC_GPIOE_FORCE_RESET();
-    __HAL_RCC_GPIOF_FORCE_RESET();
-    __HAL_RCC_GPIOG_FORCE_RESET();
-    __HAL_RCC_GPIOH_FORCE_RESET();
-    __HAL_RCC_DMA1_FORCE_RESET();
-    __HAL_RCC_DMA2_FORCE_RESET();
-    __HAL_RCC_I2C1_FORCE_RESET();
-    __HAL_RCC_I2C2_FORCE_RESET();
-    __HAL_RCC_I2C3_FORCE_RESET();
-    __HAL_RCC_I2C4_FORCE_RESET();
-    __HAL_RCC_LPUART1_FORCE_RESET();
-    __HAL_RCC_UART4_FORCE_RESET();
-    __HAL_RCC_UART5_FORCE_RESET();
-    __HAL_RCC_USART1_FORCE_RESET();
-    __HAL_RCC_USART2_FORCE_RESET();
-    __HAL_RCC_USART3_FORCE_RESET();
-    __HAL_RCC_SPI1_FORCE_RESET();
-    __HAL_RCC_CRC_FORCE_RESET();
-    __HAL_RCC_TIM16_FORCE_RESET();
-
-    // Release peripheral resets
-    __HAL_RCC_GPIOA_RELEASE_RESET();
-    __HAL_RCC_GPIOB_RELEASE_RESET();
-    __HAL_RCC_GPIOC_RELEASE_RESET();
-    __HAL_RCC_GPIOD_RELEASE_RESET();
-    __HAL_RCC_GPIOE_RELEASE_RESET();
-    __HAL_RCC_GPIOF_RELEASE_RESET();
-    __HAL_RCC_GPIOG_RELEASE_RESET();
-    __HAL_RCC_GPIOH_RELEASE_RESET();
-    __HAL_RCC_DMA1_RELEASE_RESET();
-    __HAL_RCC_DMA2_RELEASE_RESET();
-    __HAL_RCC_I2C1_RELEASE_RESET();
-    __HAL_RCC_I2C2_RELEASE_RESET();
-    __HAL_RCC_I2C3_RELEASE_RESET();
-    __HAL_RCC_I2C4_RELEASE_RESET();
-    __HAL_RCC_LPUART1_RELEASE_RESET();
-    __HAL_RCC_UART4_RELEASE_RESET();
-    __HAL_RCC_UART5_RELEASE_RESET();
-    __HAL_RCC_USART1_RELEASE_RESET();
-    __HAL_RCC_USART2_RELEASE_RESET();
-    __HAL_RCC_USART3_RELEASE_RESET();
-    __HAL_RCC_SPI1_RELEASE_RESET();
-    __HAL_RCC_CRC_RELEASE_RESET();
-    __HAL_RCC_TIM16_RELEASE_RESET();
-
-
-}
-
-void DeinitializePeripherals(void) {
+void BOOTLOADER_deinitialize_peripherals(void) {
     /* Deinitialize UART peripherals */
     HAL_UART_DeInit(&hlpuart1);
     HAL_UART_DeInit(&huart4);
@@ -242,66 +119,26 @@ void DeinitializePeripherals(void) {
 
 void BOOTLOADER_Jump_To_Application(uint32_t address)
 {
-    // pFunction JumpToApp;    
 
-    // // Backup firmware's vector table address
-    // uint32_t new_stack_pointer = *(__IO uint32_t *)(address);
-    // uint32_t new_reset_handler = *(__IO uint32_t *)(address + 4);
-
-    // // Validate the backup firmware
-    // if (new_stack_pointer == 0xFFFFFFFF || new_reset_handler == 0xFFFFFFFF) {
-    //     // Invalid firmware, do nothing
-    //     return;
-    // }
-
-    // // Disable all interrupts
-    // __disable_irq();
-
-    // Deinitialize_RTOS();
-    // // StopFreeRTOSKernel();
-    // // BOOTLOADER_deinitialize_peripherals();
-    // DeinitializePeripherals();
-    // // Set vector table to backup firmware
-    // SCB->VTOR = address;
-
-    // // Set the Main Stack Pointer
-    // __set_MSP(new_stack_pointer);
-
-    // // Jump to the backup reset handler
-    // JumpToApp = (pFunction)new_reset_handler;
-    // JumpToApp();
-
-     typedef void (*pFunction)(void);
     pFunction JumpToApp;    
 
-    // Backup firmware's vector table address
     uint32_t new_stack_pointer = *(__IO uint32_t *)(address);
     uint32_t new_reset_handler = *(__IO uint32_t *)(address + 4);
 
-    // Validate the application firmware
-    if (new_stack_pointer == 0xFFFFFFFF ||  // Check if stack pointer is in SRAM range
-        new_reset_handler == 0xFFFFFFFF) {                 // Check if reset handler is valid
-        DEBUG_uart_print_str("Invalid vals\n");
-        // Invalid firmware, do nothing
+
+    if (new_stack_pointer == 0xFFFFFFFF ||  
+        new_reset_handler == 0xFFFFFFFF) {                 
+        LOG_message(LOG_SYSTEM_OBC, LOG_SEVERITY_WARNING, LOG_SINK_ALL, "Invalid jump address. Values are not defined.");
         return;
     }
 
-    DeinitializePeripherals();
-    // Disable all interrupts
-    __disable_irq();
-
-    // Deinitialize FreeRTOS and peripherals
-    Deinitialize_RTOS();
-
-    // Clear pending interrupts and disable SysTick
-    // SCB->ICSR |= SCB_ICSR_PENDSVCLR_Msk | SCB_ICSR_PENDSTCLR_Msk;
-    SysTick->CTRL = 0;
+    BOOTLOADER_deinitialize_peripherals();
 
     // Disable all IRQs (set all NVIC IRQ enable registers to 0)
-    // for (uint32_t i = 0; i < 8; i++) {
-    //     NVIC->ICER[i] = 0xFFFFFFFF;
-    //     NVIC->ICPR[i] = 0xFFFFFFFF;
-    // }
+    for (uint32_t i = 0; i < 8; i++) {
+        NVIC->ICER[i] = 0xFFFFFFFF;
+        NVIC->ICPR[i] = 0xFFFFFFFF;
+    }
 
     // Set vector table to the application firmware
     SCB->VTOR = address;
