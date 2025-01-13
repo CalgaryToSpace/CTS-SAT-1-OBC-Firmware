35ad62e35d173f09b09c15d9ae009a754227f566
kev9268 <kevinhoang9268@gmail.com>
2025-01-13 12:34:01 -0700

    Update test_basic_telecommands.py
    Removing tests that need to be remade due to restructure of uart reading function

--- integration_tests/test_basic_telecommands.py
+++ integration_tests/test_basic_telecommands.py
@@ -31,17 +31,3 @@ def test_echo_args(port):
         open_port.write(b"CTS1+echo_back_args(hi)!")
         open_port.flush()
         assert output(open_port, b"SUCCESS: Echo Args: 'hi'\n")
-
-
-# def test_block_delay(port): # no longer works with how the current uart listnener works
-#     with port as open_port:
-#         arg = 2000
-#         open_port.write(b"CTS1+demo_blocking_delay(" + str(arg).encode("utf-8") + b")!")
-#         initial_time_ms = int(time.time() * 1000)
-#         output(port, b"")
-#         current_time_ms = int(time.time() * 1000)
-#         difference = abs(current_time_ms - initial_time_ms)
-#         if difference<100:
-#             assert True
-#         else:
-#             assert False    
