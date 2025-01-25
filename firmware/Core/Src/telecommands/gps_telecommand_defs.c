4002fc3ffb7f47cb5bbb42fed1789f69e3ffb301
christopherkinyua <christopher.kinyua@ucalgary.ca>
2025-01-25 13:19:24 -0700

    Cleaned gps telecommand again

--- firmware/Core/Src/telecommands/gps_telecommand_defs.c
+++ firmware/Core/Src/telecommands/gps_telecommand_defs.c
@@ -29,10 +29,10 @@ uint8_t TCMDEXEC_gps_send_log_cmd(const char *args_str, TCMD_TelecommandChannel_
 
     // TODO : Determine if I need to perform extra error checks on arg_str ie log command format etc
 
-    // Obtaining the length of the log command being sent
-    char cmd_buf[50];   //FIXME: Update to appropriate length to match all args in the future
-    snprintf(cmd_buf,50, "%s\n", args_str);
-    const uint16_t gps_cmd_len = strlen(cmd_buf);
+    // Adding a new line character to the log command
+    char gps_log_cmd[128];
+    snprintf(gps_log_cmd, sizeof(gps_log_cmd), "%s\n", args_str);
+    const uint16_t gps_log_cmd_len = strlen(gps_log_cmd);
 
     // Allocate space to receive incoming GPS response.
     const uint16_t GPS_rx_buffer_max_size = 512;
@@ -41,10 +41,10 @@ uint8_t TCMDEXEC_gps_send_log_cmd(const char *args_str, TCMD_TelecommandChannel_
     memset(GPS_rx_buffer, 0, GPS_rx_buffer_max_size); // Initialize all elements to 0
 
     // Send log command to GPS and receive response
-    const uint8_t gps_cmd_response = GPS_send_cmd_get_response(cmd_buf, gps_cmd_len, GPS_rx_buffer, GPS_rx_buffer_len, GPS_rx_buffer_max_size);
+    const uint8_t gps_cmd_response = GPS_send_cmd_get_response(gps_log_cmd, gps_log_cmd_len, GPS_rx_buffer, GPS_rx_buffer_len, GPS_rx_buffer_max_size);
 
     // Handle the gps_cmd_response: Perform the error checks
-    // TODO: Add GPS_validate_log_response function in here to validate response from the gps receiver
+    // TODO: Potentially add GPS_validate_log_response function in here to validate response from the gps receiver
 
     if (gps_cmd_response !=0){
 
@@ -52,6 +52,9 @@ uint8_t TCMDEXEC_gps_send_log_cmd(const char *args_str, TCMD_TelecommandChannel_
             LOG_SYSTEM_GPS,LOG_SEVERITY_NORMAL,LOG_SINK_ALL,
             "GPS Response Code: %d",
             gps_cmd_response);
+    }
+    else {
+        snprintf(response_output_buf, response_output_buf_len, "GPS Command: '%s' successfully transmitted", args_str);
     }
 
     return 0;
