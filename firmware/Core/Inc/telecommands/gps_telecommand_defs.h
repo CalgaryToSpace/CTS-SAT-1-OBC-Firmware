eb31103481025eea340e7189a462e2672de6fb8e
Christopher Kinyua <71351598+christopherkinyua@users.noreply.github.com>
2025-02-08 18:08:29 -0700

    GPS: Send log command and receive log response from gps receiver (#247)
    * Added relevant code from original branch to break down the PR size
    
    * Addressed PR comments
    
    * Fixed bug on partial data going into gps buffer
    
    * Addressed PR comments
    
    * Fixed build issue. PR done
    
    * Fix tabs vs spaces issue
    
    ---------
    
    Co-authored-by: DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>

---
+++ firmware/Core/Inc/telecommands/gps_telecommand_defs.h
@@ -0,0 +1,11 @@
+#ifndef __INCLUDE_GUARD__GPS_TELECOMMAND_DEFS_H
+#define __INCLUDE_GUARD__GPS_TELECOMMAND_DEFS_H
+
+#include "telecommands/telecommand_types.h"
+
+#include <stdint.h>
+
+uint8_t TCMDEXEC_gps_send_cmd_ascii(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                                 char *response_output_buf, uint16_t response_output_buf_len);
+
+#endif // __INCLUDE_GUARD__GPS_TELECOMMAND_DEFS_H
--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -24,6 +24,7 @@
 #include "telecommands/eps_telecommands.h"
 #include "telecommands/stm32_internal_flash_telecommand_defs.h"
 #include "telecommands/comms_telecommand_defs.h"
+#include "telecommands/gps_telecommand_defs.h"
 
 
 #include "timekeeping/timekeeping.h"
@@ -1083,6 +1084,14 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .readiness_level = TCMD_READINESS_LEVEL_FLIGHT_TESTING,
     },
     // ****************** END SECTION: comms_telecommand_defs ******************
+    // ****************** SECTION: gps_telecommand_defs ******************
+    {
+        .tcmd_name = "gps_send_cmd_ascii",
+        .tcmd_func = TCMDEXEC_gps_send_cmd_ascii,
+        .number_of_args = 1,
+        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
+    },
+    // ****************** END SECTION: gps_telecommand_defs ******************
 };
 
 // extern
