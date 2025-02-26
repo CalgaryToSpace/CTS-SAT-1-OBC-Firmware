e75523959d109d4ca15f1b1a730a438962a549b9
2BlackCats <903036002alex@gmail.com>
2025-02-25 17:31:52 -0700

    Changes to interrupt levels
    Did not push changes for .mxproject or Makerfile

--- firmware/Core/Src/stm32l4xx_hal_msp.c
+++ firmware/Core/Src/stm32l4xx_hal_msp.c
@@ -856,7 +856,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* Peripheral clock enable */
     __HAL_RCC_TIM16_CLK_ENABLE();
     /* TIM16 interrupt Init */
-    HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 15, 0);
+    HAL_NVIC_SetPriority(TIM1_UP_TIM16_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(TIM1_UP_TIM16_IRQn);
   /* USER CODE BEGIN TIM16_MspInit 1 */
 
