8f93dac10dc65081d554b1e12afa6509ad9ae180
2BlackCats <903036002alex@gmail.com>
2025-02-25 20:43:59 -0700

    Update gps_telecommand_defs.c

--- firmware/Core/Src/telecommands/gps_telecommand_defs.c
+++ firmware/Core/Src/telecommands/gps_telecommand_defs.c
@@ -67,17 +67,11 @@ uint8_t TCMDEXEC_gps_send_cmd_ascii(const char *args_str, TCMD_TelecommandChanne
 }
 
 /// @brief Telecommand: Delete all agendas
-
 /// @param args_str No arguments needed
-
 /// @param tcmd_channel The channel on which the telecommand was received, and on which the response should be sent
-
 /// @param response_output_buf The buffer to write the response to
-
 /// @param response_output_buf_len The maximum length of the response_output_buf (its size)
-
 /// @return 0 on success, 1 gps is off.
-
 uint8_t TCMDEXEC_gps_set_enabled_pps_tracking(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
 
     char *response_output_buf, uint16_t response_output_buf_len) {
@@ -85,45 +79,36 @@ uint8_t TCMDEXEC_gps_set_enabled_pps_tracking(const char *args_str, TCMD_Telecom
     static uint8_t GPS_pps_interrupt_enabled = 0;
 
     // Check if EXTI is active
-
     if (GPS_pps_interrupt_enabled) {
 
         // Disable EXTI tracking
-
         Disable_GPIO_EXTI();
 
         GPS_pps_interrupt_enabled = 0;
 
         snprintf(
-
             response_output_buf, response_output_buf_len,
-
-            "PPS tracking disabled");
-
+            "PPS tracking disabled"
+        );
+        
         return 0;
-
     } 
 
     // Check if EXTI is inactive
-
     else if (!GPS_pps_interrupt_enabled) {
 
         // Enable EXTI tracking
-
         GPIO_EXTI_Init();
 
         GPS_pps_interrupt_enabled = 1;
 
         snprintf(
-
             response_output_buf, response_output_buf_len,
-
-            "PPS tracking enabled");
+            "PPS tracking enabled"
+        );
 
         return 0;
-
     }
 
     return 1;
-
 }
