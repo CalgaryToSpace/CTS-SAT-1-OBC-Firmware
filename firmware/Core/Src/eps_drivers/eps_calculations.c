da62c9f994bd580d34b67e6364774d5bf6677b84
NuclearTea <muhammad.ali4@ucalgary.ca>
2025-03-08 11:41:53 -0700

    remove unnecessary multiplication, rename vars

--- firmware/Core/Src/eps_drivers/eps_calculations.c
+++ firmware/Core/Src/eps_drivers/eps_calculations.c
@@ -3,13 +3,13 @@
 
 float EPS_convert_battery_voltage_to_percent(EPS_battery_pack_datatype_eng_t battery) {
 
-    const uint8_t min_total_voltage_V = 12;
-    const uint8_t max_total_voltage_V = 16;
+    const uint32_t min_total_voltage_mV = 12000;
+    const uint32_t max_total_voltage_mV = 16000;
 
-    const float cell_total_voltage_V = (battery.vip_bp_input.voltage_mV) * 0.001; 
+    const int16_t battery_total_voltage_mV = battery.vip_bp_input.voltage_mV; 
     
-    const float calc = (float)(cell_total_voltage_V - min_total_voltage_V) / (max_total_voltage_V - min_total_voltage_V);
+    const float calc = ((float)(battery_total_voltage_mV - min_total_voltage_mV)) / ((float)(max_total_voltage_mV - min_total_voltage_mV));
 
     // convert to percent
-    return calc * 100;
+    return calc * 100.0;
 }
