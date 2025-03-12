9b7c146e67cc678644905f719ce730b09da4d8b2
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-03-12 10:04:45 -0600

    Add docs

--- firmware/Core/Src/eps_drivers/eps_calculations.c
+++ firmware/Core/Src/eps_drivers/eps_calculations.c
@@ -1,6 +1,10 @@
 #include "eps_drivers/eps_calculations.h"
 
-
+/// @brief Calculate the current EPS battery voltage from a PBU telemetry struct.
+/// @param battery Telemetry struct
+/// @return Battery percentage, as a float. Nominally, the battery percentage is between 0 and 100.
+///     However, the battery percentage can exceed 100% if the battery voltage is above the maximum
+///     voltage, and can be less than 0% if the battery voltage is below the minimum voltage.
 float EPS_convert_battery_voltage_to_percent(EPS_battery_pack_datatype_eng_t battery) {
 
     const uint16_t min_total_voltage_mV = 12000;
@@ -8,7 +12,10 @@ float EPS_convert_battery_voltage_to_percent(EPS_battery_pack_datatype_eng_t bat
 
     const int16_t battery_total_voltage_mV = battery.vip_bp_input.voltage_mV; 
     
-    const float calc = ((float)(battery_total_voltage_mV - min_total_voltage_mV)) / ((float)(max_total_voltage_mV - min_total_voltage_mV));
+    const float calc = (
+        ((float)(battery_total_voltage_mV - min_total_voltage_mV))
+        / ((float)(max_total_voltage_mV - min_total_voltage_mV))
+    );
 
     // convert to percent
     return calc * 100.0;
