7d5504539e4a3f80c8bf5a01ef8053d08db02332
2BlackCats <903036002alex@gmail.com>
2025-03-16 23:29:25 -0600

    Header file changes

--- firmware/Core/Inc/config/configuration.h
+++ firmware/Core/Inc/config/configuration.h
@@ -21,8 +21,7 @@ static const uint8_t CONFIG_MAX_VARIABLE_NAME_LENGTH = 50;
 static const uint8_t CONFIG_MAX_JSON_STRING_LENGTH = UINT8_MAX;
 
 // extern for EPS
-extern uint32_t toggleable_channels;
-extern uint32_t watchdog_timer, EPS_monitor_timer;
+extern uint32_t EPS_monitor_interval_ms;
 
 // extern
 extern const uint32_t CONFIG_freertos_min_remaining_stack_percent;
