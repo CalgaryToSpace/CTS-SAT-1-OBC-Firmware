abf420f34614231523f8e58c5bd813a2605d44af
Parker L <166864283+parker-research@users.noreply.github.com>
2025-03-19 18:32:07 -0600

    Create `GEN_byte_array_to_hex_str` function (#296)

--- firmware/Core/Inc/transforms/arrays.h
+++ firmware/Core/Inc/transforms/arrays.h
@@ -13,4 +13,8 @@ void GEN_int64_to_str(int64_t value, char *buffer);
 uint8_t GEN_hex_str_to_byte_array(const char *hex_str, uint8_t output_byte_array[],
     uint16_t output_byte_array_size, uint16_t *output_byte_array_len);
 
+void GEN_byte_array_to_hex_str(
+    const uint8_t *byte_array, uint32_t byte_array_len, char *dest_str, uint32_t dest_str_size
+);
+
 #endif // INCLUDE_GUARD__TRANSFORMS_ARRAYS_H__
