3cf424459434a155ea4d7a98ebd0c2d6bf005cb0
celinahy <113138309+celinahy@users.noreply.github.com>
2025-03-29 14:39:13 -0600

    telecommand function and setup for gps_enable_disable_command

--- firmware/Core/Inc/telecommands/gps_telecommand_defs.h
+++ firmware/Core/Inc/telecommands/gps_telecommand_defs.h
@@ -8,4 +8,6 @@
 uint8_t TCMDEXEC_gps_send_cmd_ascii(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                                  char *response_output_buf, uint16_t response_output_buf_len);
 
+uint8_t TCMDEXEC_gps_enable_disable_command(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                                    char *response_output_buf, uint16_t response_output_buf_len);
 #endif // __INCLUDE_GUARD__GPS_TELECOMMAND_DEFS_H
