f17e04d8f5b5b8fbee3b3eee7767157556f79930
celinahy <113138309+celinahy@users.noreply.github.com>
2025-04-04 17:21:07 -0600

    added new GPS telecommand defs

--- firmware/Core/Inc/gps/gps_internal_drivers.h
+++ firmware/Core/Inc/gps/gps_internal_drivers.h
@@ -7,5 +7,7 @@
 uint8_t GPS_send_cmd_get_response(const char *cmd_buf, uint8_t cmd_buf_len, uint8_t rx_buf[],
                                   uint16_t rx_buf_len, const uint16_t rx_buf_max_size);
 
+uint8_t GPS_enable_disable(const char *cmd_arg, uint16_t enable_disable_flag, uint8_t rx_buf[],
+                           uint16_t rx_buf_len, const uint16_t rx_buf_max_size);
 
 #endif /* INCLUDE_GUARD__GPS_INTERNAL_DRIVERS_H__ */
