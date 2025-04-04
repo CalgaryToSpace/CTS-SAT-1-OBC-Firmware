7041940691115b554a02811a9c66b0c0cb643a14
celinahy <113138309+celinahy@users.noreply.github.com>
2025-04-04 17:21:07 -0600

    added new GPS telecommand defs

--- firmware/Core/Inc/gps/gps_internal_drivers.h
+++ firmware/Core/Inc/gps/gps_internal_drivers.h
@@ -11,5 +11,7 @@ uint8_t GPS_send_cmd_get_response(
     uint16_t* rx_buf_len_dest
 );
 
+uint8_t GPS_enable_disable(const char *cmd_arg, uint16_t enable_disable_flag, uint8_t rx_buf[],
+                           uint16_t rx_buf_len, const uint16_t rx_buf_max_size);
 
 #endif /* INCLUDE_GUARD__GPS_INTERNAL_DRIVERS_H__ */
--- firmware/Core/Inc/telecommands/gps_telecommand_defs.h
+++ firmware/Core/Inc/telecommands/gps_telecommand_defs.h
@@ -9,5 +9,9 @@ uint8_t TCMDEXEC_gps_send_cmd_ascii(const char *args_str, TCMD_TelecommandChanne
                                  char *response_output_buf, uint16_t response_output_buf_len);
 
 uint8_t TCMDEXEC_gps_enable_disable_command(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
-                                    char *response_output_buf, uint16_t response_output_buf_len);
+                                            char *response_output_buf, uint16_t response_output_buf_len);
+
+uint8_t TCMDEXEC_gps_enable_disable_all(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                                char *response_output_buf, uint16_t response_output_buf_len);
+
 #endif // __INCLUDE_GUARD__GPS_TELECOMMAND_DEFS_H
