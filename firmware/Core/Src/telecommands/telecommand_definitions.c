460e43cb7f429910a20c1f336bb90fcad78a0610
MrBear101 <64572053+MrBear101@users.noreply.github.com>
2025-04-05 14:32:43 -0600

    added telecommand for capture image

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -1211,6 +1211,12 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .number_of_args = 1,
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
+    {
+        .tcmd_name = "camera_capture",
+        .tcmd_func = TCMDEXEC_camera_capture,
+        .number_of_args = 2,
+        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION
+    },
     // ****************** END SECTION: camera_telecommand_defs *******************
 };
 
