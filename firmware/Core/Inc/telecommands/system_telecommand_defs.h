27a4e3cf14103ba43428994c9cf84c1b1e23eaf8
Kevin Hoang <kevinhoang9268@gmail.com>
2025-04-09 04:10:15 -0600

    Add TCMD get all thermo info

--- firmware/Core/Inc/telecommands/system_telecommand_defs.h
+++ firmware/Core/Inc/telecommands/system_telecommand_defs.h
@@ -16,4 +16,7 @@ uint8_t TCMDEXEC_available_telecommands(const char *args_str, TCMD_TelecommandCh
 uint8_t TCMDEXEC_reboot(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
     char *response_output_buf, uint16_t response_output_buf_len);
 
+uint8_t TCMDEXEC_get_all_system_thermal_info(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    char *response_output_buf, uint16_t response_output_buf_len);
+
 #endif /* INCLUDE_GUARD__SYSTEM_TELECOMMAND_DEFINITIONS_H */
