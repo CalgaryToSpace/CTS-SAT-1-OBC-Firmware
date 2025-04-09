ccc6275a8fb5a8d4c1e39598de4657f6f0b22598
NuclearTea <muhammad.ali4@ucalgary.ca>
2025-04-08 21:38:50 -0600

    fix indentation

--- firmware/Core/Src/telecommands/camera_telecommand_defs.c
+++ firmware/Core/Src/telecommands/camera_telecommand_defs.c
@@ -97,37 +97,37 @@ uint8_t TCMDEXEC_camera_change_baud_rate(
 uint8_t TCMDEXEC_camera_capture(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
     char *response_output_buf, uint16_t response_output_buf_len)
 {
-const int args_str_len = strlen(args_str);
-char lighting[2];
-// memset(config_var_name, 0, CONFIG_MAX_VARIABLE_NAME_LENGTH);
-const uint8_t parse_result = TCMD_extract_string_arg(args_str, 0, lighting, sizeof(lighting));
-if (parse_result > 0)
-{
-snprintf(response_output_buf, response_output_buf_len, "Could not parse arg 0 for: %s", args_str);
-return 1;
-}
+    const int args_str_len = strlen(args_str);
+    char lighting[2];
+    // memset(config_var_name, 0, CONFIG_MAX_VARIABLE_NAME_LENGTH);
+    const uint8_t parse_result = TCMD_extract_string_arg(args_str, 0, lighting, sizeof(lighting));
+    if (parse_result > 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Could not parse arg 0 for: %s", args_str);
+        return 1;
+    }
 
-uint64_t flash_TCMD = 2;
-const uint8_t parse_result2 = TCMD_extract_uint64_arg(args_str, args_str_len, 1, &flash_TCMD);
-if (parse_result2 > 0)
-{
-snprintf(response_output_buf, response_output_buf_len, "Could not parse arg 1 for: %s", args_str);
-return 1;
-}
-uint8_t flash = (uint8_t)flash_TCMD;
-if (flash != 1 && flash != 0){
-snprintf(response_output_buf, response_output_buf_len, "Value must be 0 or 1, not %d", flash);
-return 1;
-}
+    uint64_t flash_TCMD = 2;
+    const uint8_t parse_result2 = TCMD_extract_uint64_arg(args_str, args_str_len, 1, &flash_TCMD);
+    if (parse_result2 > 0)
+    {
+        snprintf(response_output_buf, response_output_buf_len, "Could not parse arg 1 for: %s", args_str);
+        return 1;
+    }
+    uint8_t flash = (uint8_t)flash_TCMD;
+    if (flash != 1 && flash != 0){
+        snprintf(response_output_buf, response_output_buf_len, "Value must be 0 or 1, not %d", flash);
+        return 1;
+    }
 
-enum Capture_Status img = CAM_Capture_Image((bool)flash, lighting[0]);
+    enum Capture_Status img = CAM_Capture_Image((bool)flash, lighting[0]);
 
-if (img != Transmit_Success){
-snprintf(response_output_buf, response_output_buf_len, "Wrong lighting input.\n");
-return 1;
-}
+    if (img != Transmit_Success){
+        snprintf(response_output_buf, response_output_buf_len, "Wrong lighting input.\n");
+        return 1;
+    }
 
-// snprintf(response_output_buf, response_output_buf_len, "SUCCESS: Set config var: %s to: %lu", config_var_name, (uint32_t)config_var_new_value);
-snprintf(response_output_buf, response_output_buf_len, "Successfully captured image\n");
-return 0;
+    // snprintf(response_output_buf, response_output_buf_len, "SUCCESS: Set config var: %s to: %lu", config_var_name, (uint32_t)config_var_new_value);
+    snprintf(response_output_buf, response_output_buf_len, "Successfully captured image\n");
+    return 0;
 }
