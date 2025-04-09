6d5fc01ad6a6a80d69f70f90c0435737fcf413ea
DeemDeem52 <42725035+DeemDeem52@users.noreply.github.com>
2025-04-08 22:01:24 -0600

    Add commissioning commands telecommand

--- firmware/Core/Inc/telecommands/telecommand_adcs.h
+++ firmware/Core/Inc/telecommands/telecommand_adcs.h
@@ -234,4 +234,7 @@ uint8_t TCMDEXEC_adcs_set_sd_log_config(const char *args_str, TCMD_TelecommandCh
 uint8_t TCMDEXEC_adcs_get_sd_log_config(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
                         char *response_output_buf, uint16_t response_output_buf_len);
 
+uint8_t TCMDEXEC_adcs_set_commissioning_modes(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+                        char *response_output_buf, uint16_t response_output_buf_len);
+
 #endif // INCLUDE_GUARD__TELECOMMAND_adcs_H
