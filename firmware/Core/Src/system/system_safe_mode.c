4ed54d78c0dfce49356f5eea23c99fcdbafad505
NuclearTea <muhammad.ali4@ucalgary.ca>
2025-04-13 13:43:22 -0600

    Disable ADCS, add error value with enum and enum->string functions

--- firmware/Core/Src/system/system_safe_mode.c
+++ firmware/Core/Src/system/system_safe_mode.c
@@ -1,5 +1,6 @@
 #include "system/system_safe_mode.h"
 #include "eps_drivers/eps_channel_control.h"
+#include "adcs_drivers/adcs_commands.h"
 #include "log/log.h"
 
 
@@ -9,36 +10,45 @@
 /// @return 0 on success, more than 0 on failure.
 uint8_t SYS_enter_safe_mode()
 {
+    uint8_t error_ret = 0;
     // disable GPS
 
+    // disable ADCS: 
+    // TODO: There are 2 "low power modes" that we can enter
+    // stable-attitude mode and all-peripheral turned off mode
+    // in stable-attitude mode, the adcs will still be stable however, turn off everything else
+    // in all-peripheral turned off mode, the adcs will not be stable (y-thomson spin AKA slowly de-tumbling) and, everything is also turned off
 
-    // disable ADCS 
-
+    // for now, turning off everything, which will be the lowest power mode
+    const uint8_t disable_adcs = ADCS_disable_peripherals_and_SD_logs_without_stabilisation();
+    if (disable_adcs != 0) {
+        error_ret |= SYS_SAFE_MODE_ERROR_ADCS;
+    }
 
     // disable MPI
     const uint8_t disable_mpi_5v = EPS_set_channel_enabled(EPS_CHANNEL_5V_MPI, 0);
     if (disable_mpi_5v != 0) {
-        return 3;
+        error_ret |= SYS_SAFE_MODE_ERROR_MPI_5V;
     }
 
     const uint8_t disable_mpi_12v = EPS_set_channel_enabled(EPS_CHANNEL_12V_MPI, 0);
     if (disable_mpi_12v != 0) {
-        return 4;
+        error_ret |= SYS_SAFE_MODE_ERROR_MPI_12V;
     }
 
     // disable Camera
     const uint8_t disable_camera = EPS_set_channel_enabled(EPS_CHANNEL_3V3_CAMERA, 0);
     if (disable_camera != 0) {
-        return 5;
+        error_ret |= SYS_SAFE_MODE_ERROR_CAMERA;
     }
 
     // disable Boom
     const uint8_t disable_boom = EPS_set_channel_enabled(EPS_CHANNEL_12V_BOOM, 0);
     if (disable_boom != 0) {
-        return 6;
+        error_ret |= SYS_SAFE_MODE_ERROR_BOOM;
     }
 
-    return 0;
+    return error_ret;
 }
 
 /// @brief Checks if the EPS is in low power mode and enters safe mode if it is.
@@ -99,4 +109,48 @@ uint8_t SYS_battery_safe_mode_check() {
         }
         const uint8_t result_safe_mode = SYS_enter_safe_mode();
         return result_safe_mode == 0 ? 0 : 1 + result_safe_mode;
+}
+
+char *SYS_safe_mode_error_enum_to_string(SYS_safe_mode_error_enum_t error_mask)
+{
+    switch (error_mask) {
+        case SYS_SAFE_MODE_ERROR_GPS:
+            return "GPS";
+        case SYS_SAFE_MODE_ERROR_ADCS:
+            return "ADCS";
+        case SYS_SAFE_MODE_ERROR_MPI_5V:
+            return "MPI 5V";
+        case SYS_SAFE_MODE_ERROR_MPI_12V:
+            return "MPI 12V";
+        case SYS_SAFE_MODE_ERROR_CAMERA:
+            return "Camera";
+        case SYS_SAFE_MODE_ERROR_BOOM:
+            return "Boom";
+        default:
+            return "Unknown"; // Unknown error
+    }
+}
+
+uint8_t SYS_safe_mode_error_enum_to_json(SYS_safe_mode_error_enum_t error, char *buffer, size_t buffer_size) 
+{
+    if (buffer == NULL || buffer_size == 0) {
+        return 1;
+    }
+
+    snprintf(buffer, buffer_size, "{");
+
+    for (uint8_t i = 0; i < 6; i++) {
+        SYS_safe_mode_error_enum_t error_mask = (SYS_safe_mode_error_enum_t)(1 << i);
+        const char *error_string = SYS_safe_mode_error_enum_to_string(error_mask);
+        const char *status_string = (error & error_mask) ? "Error Disabling" : "Successfully Disabled";
+
+        if (i != 0) {
+            strncat(buffer, ", ", buffer_size - strlen(buffer) - 1);
+        }
+
+        snprintf(buffer + strlen(buffer), buffer_size - strlen(buffer) - 1, "\"%s\":\"%s\"", error_string, status_string);
+    }
+
+    strncat(buffer, "}", buffer_size - strlen(buffer) - 1);
+    return 0;
 }
