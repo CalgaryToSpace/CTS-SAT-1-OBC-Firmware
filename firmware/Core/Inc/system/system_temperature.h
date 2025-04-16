fb9d3a09001de66f121e2f222d252134681d5e92
Kevin Hoang <kevinhoang9268@gmail.com>
2025-04-15 20:24:30 -0600

    Renaming variable and structure names for system thermal info

--- firmware/Core/Inc/system/system_temperature.h
+++ firmware/Core/Inc/system/system_temperature.h
@@ -14,7 +14,7 @@ typedef struct {
     float system_eps_battery_percent;
     uint16_t system_eps_battery_heater_status_bit;
     int16_t system_eps_battery_each_sensor_temperature_cC[EPS_BATTERY_PACK_SENSOR_SIZE];
-} SYS_TEMP_temperature_output_t;
+} SYS_TEMP_thermal_info_t;
 
 typedef struct {
     int32_t system_OBC_temperature_C;
@@ -22,9 +22,9 @@ typedef struct {
     uint16_t system_ANT_temperature_i2c_bus_B_raw;
     EPS_conditioning_channel_datatype_eng_t system_eps_conditioning_channel_info_each_channel[EPS_COND_CHANNEL_SIZE];
     EPS_battery_pack_datatype_eng_t system_eps_battery_datatype_struct;
-} SYS_TEMP_parameter_t;
+} SYS_TEMP_raw_thermal_info_t;
 
-uint8_t SYS_TEMP_get_raw_thermal_info(SYS_TEMP_parameter_t* result);
-uint8_t SYS_TEMP_get_processed_thermal_info(SYS_TEMP_parameter_t* input, SYS_TEMP_temperature_output_t* result);
+uint8_t SYS_TEMP_get_raw_thermal_info(SYS_TEMP_raw_thermal_info_t* result);
+uint8_t SYS_TEMP_pack_to_system_thermal_info(SYS_TEMP_raw_thermal_info_t* input, SYS_TEMP_thermal_info_t* result);
 
 #endif
