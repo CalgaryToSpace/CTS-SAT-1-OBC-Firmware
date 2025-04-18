9e3cdc9aa216b81fd893e0a404de4c701c829ebc
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-04-18 15:53:04 -0600

    Compliance on `obc_read_temperature_complex` telecommand

--- firmware/Core/Src/telecommands/obc_temperature_sensor_telecommand_defs.c
+++ firmware/Core/Src/telecommands/obc_temperature_sensor_telecommand_defs.c
@@ -17,45 +17,52 @@ uint8_t TCMDEXEC_obc_read_temperature_complex(
     char *response_output_buf, uint16_t response_output_buf_len
 ) {
     int32_t temperature;
-    uint8_t status;
     uint32_t temp_precision_conversion_delay_ms;
     uint32_t temp_scaling_factor;
 
-    // convert user requested precision
+    // Convert user requested precision.
     const uint8_t temp_precision = atoi(args_str);
 
-    status = OBC_TEMP_SENSOR_set_temp_precision(temp_precision, &temp_precision_conversion_delay_ms, &temp_scaling_factor);
+    const uint8_t status = OBC_TEMP_SENSOR_set_temp_precision(
+        temp_precision, &temp_precision_conversion_delay_ms, &temp_scaling_factor
+    );
     switch (status)
     {
         case 0:
             break;
         case 1:
         {
-            snprintf(response_output_buf, response_output_buf_len, "Error writing config register.\n");
-            return 0;
+            snprintf(response_output_buf, response_output_buf_len, "Error writing config register.");
+            return 1;
         }
         case 2:
         {
-            snprintf(response_output_buf, response_output_buf_len, "Invalid temperature precision provided. Arg must be in range 9-12.\n");
-            return 0;
+            snprintf(
+                response_output_buf, response_output_buf_len,
+                "Invalid temperature precision provided. Arg must be in range 9-12."
+            );
+            return 2;
         }
     }
 
-    // give time for sensor to convert value
+    // Give time for sensor to convert value.
     HAL_Delay(temp_precision_conversion_delay_ms);
 
     // Read temperature.
     uint8_t success_result = OBC_TEMP_SENSOR_read_temperature(&temperature);
 
-    // handle result
-    if (success_result == 0)
-    {
-        snprintf(response_output_buf, response_output_buf_len, "Temperature(C * %lu): %ld\n", temp_scaling_factor, temperature);
+    // Handle result.
+    if (success_result != 0) {
+        snprintf(response_output_buf, response_output_buf_len, "Temperature read fail!");
+        return 3;
     }
-    else
-    {
-        snprintf(response_output_buf, response_output_buf_len, "Temperature read fail!\n");
-    }
+
+    snprintf(
+        response_output_buf, response_output_buf_len,
+        "Temperature(C * %lu): %ld",
+        temp_scaling_factor,
+        temperature
+    );
 
     return 0;
 }
