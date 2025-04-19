455265c197a05e88e9bf28fc2266c207c5a5334f
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2025-04-19 00:45:01 -0600

    rename rx buffer

--- firmware/Core/Src/cts_csp/cts_comms_rx.c
+++ firmware/Core/Src/cts_csp/cts_comms_rx.c
@@ -12,8 +12,8 @@
 */
 
 int rx_count = 0;
-#define CSP_RX_BUFF_SIZE 255
-uint8_t csp_rx_buffer[CSP_RX_BUFF_SIZE];
+#define COMMS_RX_BUFF_SIZE 255
+uint8_t COMMS_rx_buffer[COMMS_RX_BUFF_SIZE];
 
 /// @brief Strips the csp packet header (first 4 bytes) from the packet.
 /// @return pointer to the packet data without the header
@@ -25,7 +25,7 @@ static uint8_t* strip_csp_packet(uint8_t *packet, size_t packet_size) {
 /// @param packet csp packet
 /// @return result, 0 on success 1 on failure 
 static int32_t schedule_csp_packet_data_for_tcmd_execution(uint8_t * packet, size_t packet_size) {
-	char* packet_data = (char*) strip_csp_packet( csp_rx_buffer, packet_size);
+	char* packet_data = (char*) strip_csp_packet( COMMS_rx_buffer, packet_size);
 
 	TCMD_parsed_tcmd_to_execute_t parsed_tcmd;
 	uint8_t parse_result = TCMD_parse_full_telecommand(
@@ -55,14 +55,14 @@ static int32_t schedule_csp_packet_data_for_tcmd_execution(uint8_t * packet, siz
 /// @param hi2c  the I2C handle
 void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c)
 {	
-	csp_rx_buffer[CSP_RX_BUFF_SIZE -1] = '\0'; // enusure the buffer is null terminated
+	COMMS_rx_buffer[COMMS_RX_BUFF_SIZE -1] = '\0'; // enusure the buffer is null terminated
 
 	for (int i = 0; i < rx_count; i++) {
 	LOG_message(
 		LOG_SYSTEM_UHF_RADIO, LOG_SEVERITY_NORMAL, LOG_SINK_ALL,
-		"I2C listen complete, data: %x", csp_rx_buffer[i]);
+		"I2C listen complete, data: %x", COMMS_rx_buffer[i]);
 	}
-	schedule_csp_packet_data_for_tcmd_execution(csp_rx_buffer, rx_count);
+	schedule_csp_packet_data_for_tcmd_execution(COMMS_rx_buffer, rx_count);
 	
 	rx_count =0;
 	HAL_I2C_EnableListen_IT(hi2c);
@@ -74,7 +74,7 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, ui
 	if (TransferDirection == I2C_DIRECTION_TRANSMIT)  // if the master wants to transmit the data
 	{
 		// The I2C_FIRST_FRAME implies that the first byte is to be received
-		HAL_I2C_Slave_Seq_Receive_IT(hi2c, csp_rx_buffer + rx_count, 1, I2C_FIRST_FRAME);
+		HAL_I2C_Slave_Seq_Receive_IT(hi2c, COMMS_rx_buffer + rx_count, 1, I2C_FIRST_FRAME);
 		LOG_message(
 			LOG_SYSTEM_UHF_RADIO, LOG_SEVERITY_NORMAL, LOG_SINK_ALL,
 			"first byte received"
@@ -90,13 +90,13 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, ui
 void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
 	rx_count++;
-	if (rx_count < CSP_RX_BUFF_SIZE-1) {
+	if (rx_count < COMMS_RX_BUFF_SIZE-1) {
 		// The I2C_NEXT_FRAME implies that we are expecting the next byte of the same transfer
-		HAL_I2C_Slave_Seq_Receive_IT(hi2c, csp_rx_buffer + rx_count, 1, I2C_NEXT_FRAME);
+		HAL_I2C_Slave_Seq_Receive_IT(hi2c, COMMS_rx_buffer + rx_count, 1, I2C_NEXT_FRAME);
 	}
 	else {
 		// if we have read the maximum number of bytes -1 , the I2C_LAST_FRAME indicates that the slave will not accept any more bytes after the next
-		HAL_I2C_Slave_Seq_Receive_IT(hi2c, csp_rx_buffer + rx_count, 1, I2C_LAST_FRAME);
+		HAL_I2C_Slave_Seq_Receive_IT(hi2c, COMMS_rx_buffer + rx_count, 1, I2C_LAST_FRAME);
 	}
 }
 
