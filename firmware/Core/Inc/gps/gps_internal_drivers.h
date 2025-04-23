d545b5bde857a37e0a9bba493a2dd8567a6af819
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-04-23 00:08:31 -0600

    fix: GNSS buffer overrun, `rx_buf_len_dest` assignment (#336)
    * Docs, unnecessary `extern`
    
    * Fix GNSS buffer overrun, `rx_buf_len_dest` assignment

--- firmware/Core/Inc/gps/gps_internal_drivers.h
+++ firmware/Core/Inc/gps/gps_internal_drivers.h
@@ -4,8 +4,12 @@
 
 #include <stdint.h>
 
-uint8_t GPS_send_cmd_get_response(const char *cmd_buf, uint8_t cmd_buf_len, uint8_t rx_buf[],
-                                  uint16_t rx_buf_len, const uint16_t rx_buf_max_size);
+uint8_t GPS_send_cmd_get_response(
+    const char *cmd_buf, uint8_t cmd_buf_len,
+    uint8_t rx_buf[],
+    const uint16_t rx_buf_max_size,
+    uint16_t* rx_buf_len_dest
+);
 
 uint8_t GPS_enable_disable(const char *cmd_arg, uint16_t enable_disable_flag, uint8_t rx_buf[],
                            uint16_t rx_buf_len, const uint16_t rx_buf_max_size);
--- firmware/Core/Src/telecommands/gps_telecommand_defs.c
+++ firmware/Core/Src/telecommands/gps_telecommand_defs.c
@@ -48,18 +48,10 @@ const char *TCMD_GPS_TRACKSIGNAL_NAMES[] = {
 /// @param response_output_buf The buffer to write the response to
 /// @param response_output_buf_len The maximum length of the response_output_buf (its size)
 /// @return 0 on success, > 0 error
-uint8_t TCMDEXEC_gps_send_cmd_ascii(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
-                                    char *response_output_buf, uint16_t response_output_buf_len)
-{
-
-    if (args_str == NULL)
-    {
-        snprintf(response_output_buf, response_output_buf_len, "Error: Empty args_str");
-        return 1;
-    }
-
-    // TODO : Determine if we need to perform extra error checks on arg_str ie log command format etc
-
+uint8_t TCMDEXEC_gps_send_cmd_ascii(
+    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    char *response_output_buf, uint16_t response_output_buf_len
+) {
     // Adding a new line character to the log command
     char gps_log_cmd[128];
     snprintf(gps_log_cmd, sizeof(gps_log_cmd), "%s\n", args_str);
@@ -72,23 +64,30 @@ uint8_t TCMDEXEC_gps_send_cmd_ascii(const char *args_str, TCMD_TelecommandChanne
     memset(GPS_rx_buffer, 0, GPS_rx_buffer_max_size); // Initialize all elements to 0
 
     // Send log command to GPS and receive response
-    const uint8_t gps_cmd_response = GPS_send_cmd_get_response(
-        gps_log_cmd, gps_log_cmd_len, GPS_rx_buffer, GPS_rx_buffer_len, GPS_rx_buffer_max_size);
+    const uint8_t gps_cmd_status = GPS_send_cmd_get_response(
+        gps_log_cmd, gps_log_cmd_len, GPS_rx_buffer, GPS_rx_buffer_max_size,
+        &GPS_rx_buffer_len // Will be mutated.
+    );
 
-    // Handle the gps_cmd_response: Perform the error checks
+    // Handle the gps_cmd_status: Perform the error checks
     // TODO: Potentially add GPS_validate_log_response function in here to validate response from the gps receiver
 
-    if (gps_cmd_response != 0)
-    {
+    if (gps_cmd_status != 0) {
         LOG_message(
             LOG_SYSTEM_GPS,
             LOG_SEVERITY_NORMAL,
             LOG_SINK_ALL,
-            "GPS Response Code: %d",
-            gps_cmd_response);
+            "GPS_send_cmd_get_response failed -> %d",
+            gps_cmd_status
+        );
     }
 
-    snprintf(response_output_buf, response_output_buf_len, "GPS Command: '%s' successfully transmitted", args_str);
+    snprintf(
+        response_output_buf, response_output_buf_len,
+        "GPS Response (%d bytes): %s",
+        GPS_rx_buffer_len,
+        GPS_rx_buffer
+    );
 
     return 0;
 }
