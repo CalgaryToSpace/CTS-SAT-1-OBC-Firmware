7abee1bcc1951da884b03bd7eb2efd1d1783c692
Marko Vujic <81124080+Makiv1@users.noreply.github.com>
2025-04-23 00:43:00 -0600

    Add downlink stub (#346)

---
+++ firmware/Core/Inc/comms_drivers/ax100_tx.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_GUARD__AX100_TX_H__
+#define INCLUDE_GUARD__AX100_TX_H__
+
+#include <stdint.h>
+//FIXME: This need to actually be implemented!
+uint8_t AX100_downlink_bytes(uint8_t *data, uint32_t data_len);
+
+
+
+#endif // INCLUDE_GUARD__AX100_TX_H__
---
+++ firmware/Core/Src/comms_drivers/ax100_tx.c
@@ -0,0 +1,4 @@
+#include "comms_drivers/ax100_tx.h"
+
+
+uint8_t AX100_downlink_bytes(uint8_t *data, uint32_t data_len) {return 0;}
