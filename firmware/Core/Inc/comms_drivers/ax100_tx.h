7e74512dda83d85b3b258d9262263a7adf2773b0
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2025-04-23 20:44:56 -0600

    Declare downlink max size

--- firmware/Core/Inc/comms_drivers/ax100_tx.h
+++ firmware/Core/Inc/comms_drivers/ax100_tx.h
@@ -2,6 +2,10 @@
 #define INCLUDE_GUARD__AX100_TX_H__
 
 #include <stdint.h>
+
+//FIXME: confirm this
+#define AX100_DOWNLINK_MAX_BYTES 200
+
 //FIXME: This need to actually be implemented!
 uint8_t AX100_downlink_bytes(uint8_t *data, uint32_t data_len);
 
