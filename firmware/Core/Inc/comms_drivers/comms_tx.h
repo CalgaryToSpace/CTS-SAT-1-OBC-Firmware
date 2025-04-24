653511c8b4319752b058f2956b90bc47214b6adf
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2025-04-23 20:49:29 -0600

    Implement downlink tcmd response function

---
+++ firmware/Core/Inc/comms_drivers/comms_tx.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDE_GUARD__COMMS_TX_H__
+#define INCLUDE_GUARD__COMMS_TX_H__
+#include "comms_drivers/ax100_tx.h"
+
+typedef enum {
+    COMMS_PACKET_TYPE_BEACON_MINIMAL = 0x01,
+    COMMS_PACKET_TYPE_BEACON_FULL = 0x02,
+    COMMS_PACKET_TYPE_LOG_MESSAGE = 0x03,
+    COMMS_PACKET_TYPE_TCMD_RESPONSE = 0x04,
+    COMMS_PACKET_TYPE_DOWNLINK_FIRST_PACKET = 0x05,
+    COMMS_PACKET_TYPE_DOWNLINK_NEXT_PACKET = 0x06,
+    COMMS_PACKET_TYPE_DOWNLINK_LAST_PACKET = 0x07
+} COMMS_packet_type_t;
+
+typedef struct {
+    COMMS_packet_type_t packet_type;
+    uint8_t data[AX100_DOWNLINK_MAX_BYTES];
+} CTS_packet_t;
+
+uint8_t COMMS_downlink_tcmd_response(
+    uint64_t ts_sent, 
+    uint8_t respose_code, 
+    uint16_t duration_ms, 
+    uint8_t *respose,
+    uint32_t respose_len
+);
+#endif // INCLUDE_GUARD__COMMS_TX_H__
---
+++ firmware/Core/Src/comms_drivers/comms_tx.c
@@ -0,0 +1,32 @@
+#include "comms_drivers/comms_tx.h"
+#include "memory.h"
+
+uint8_t COMMS_downlink_tcmd_response(
+    uint64_t ts_sent, 
+    uint8_t respose_code, 
+    uint16_t duration_ms, 
+    uint8_t *respose,
+    uint32_t respose_len
+) {
+    CTS_packet_t packet;
+    packet.packet_type = COMMS_PACKET_TYPE_TCMD_RESPONSE;
+
+    uint8_t header_len = sizeof(ts_sent) + sizeof(respose_code) + sizeof(duration_ms) + sizeof(respose_len);
+    if( respose_len + header_len > AX100_DOWNLINK_MAX_BYTES) {return 1;}
+
+    uint8_t packet_len = 0;
+    memcpy(packet.data + packet_len, &ts_sent, sizeof(ts_sent)); // 8 bytes of ts_sent
+    packet_len += sizeof(ts_sent);
+
+    memcpy(packet.data + packet_len, &respose_code, sizeof(respose_code)); // 1 byte of respose_code
+    packet_len += sizeof(respose_code);
+
+    memcpy(packet.data + packet_len, &duration_ms, sizeof(duration_ms)); // 2 bytes of duration_ms
+    packet_len += sizeof(duration_ms);
+
+    memcpy(packet.data + packet_len, respose, sizeof(respose_len)); // 1 byte of respose
+    packet_len += sizeof(respose_len);
+    
+
+    return AX100_downlink_bytes(packet.data, packet_len);
+}
