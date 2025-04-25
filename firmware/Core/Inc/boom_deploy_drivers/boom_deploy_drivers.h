34b79c23393a00d3b35bb57b888e0c20240e6549
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-04-25 13:49:06 -0600

    Switch to new 2-channel setup

--- firmware/Core/Inc/boom_deploy_drivers/boom_deploy_drivers.h
+++ firmware/Core/Inc/boom_deploy_drivers/boom_deploy_drivers.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-uint8_t BOOM_get_pgood_status();
+uint8_t BOOM_set_burn_enabled(uint8_t boom_channel_num, uint8_t enabled);
 
-void BOOM_set_burn_enabled(uint8_t enabled);
+void BOOM_disable_all_burns(void);
 
 #endif /* INCLUDE_GUARD__BOOM_DEPLOY_DRIVERS_H */
--- firmware/Core/Src/boom_deploy_drivers/boom_deploy_drivers.c
+++ firmware/Core/Src/boom_deploy_drivers/boom_deploy_drivers.c
@@ -3,14 +3,26 @@
 #include "main.h"
 #include <stdint.h>
 
-/// @brief Returns 1 if boom power is reported as "good", by the regulator. 0 otherwise.
-/// @return 1 if good. 0 if less good.
-uint8_t BOOM_get_pgood_status() {
-    return HAL_GPIO_ReadPin(PIN_BOOM_PGOOD_IN_GPIO_Port, PIN_BOOM_PGOOD_IN_Pin);
-}
 
 /// @brief Sets the boom deploy enable pin state (on or off).
+/// @param boom_channel_num 1 for channel 1 (BOOM_CTRL_2), 2 for channel 2 (BOOM_CTRL_2).
 /// @param enabled 0 to disable, 1 to enable.
-void BOOM_set_burn_enabled(uint8_t enabled) {
-    HAL_GPIO_WritePin(PIN_BOOM_DEPLOY_EN_OUT_GPIO_Port, PIN_BOOM_DEPLOY_EN_OUT_Pin, enabled);
+/// @return 0 on success, 1 on failure (invalid channel).
+uint8_t BOOM_set_burn_enabled(uint8_t boom_channel_num, uint8_t enabled) {
+    if (boom_channel_num == 1) {
+        HAL_GPIO_WritePin(PIN_BOOM_DEPLOY_EN_1_OUT_GPIO_Port, PIN_BOOM_DEPLOY_EN_1_OUT_Pin, enabled);
+    }
+    else if (boom_channel_num == 2) {
+        HAL_GPIO_WritePin(PIN_BOOM_DEPLOY_EN_2_OUT_GPIO_Port, PIN_BOOM_DEPLOY_EN_2_OUT_Pin, enabled);
+    }
+    else {
+        return 1; // Invalid channel
+    }
+    return 0;
+}
+
+/// @brief Disable all boom deploy enable channels. No EPS change.
+void BOOM_disable_all_burns(void) {
+    HAL_GPIO_WritePin(PIN_BOOM_DEPLOY_EN_1_OUT_GPIO_Port, PIN_BOOM_DEPLOY_EN_1_OUT_Pin, 0);
+    HAL_GPIO_WritePin(PIN_BOOM_DEPLOY_EN_2_OUT_GPIO_Port, PIN_BOOM_DEPLOY_EN_2_OUT_Pin, 0);
 }
