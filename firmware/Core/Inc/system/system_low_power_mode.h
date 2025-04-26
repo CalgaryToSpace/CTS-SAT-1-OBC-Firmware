01b2aafd22f8e613b662276f035b4f087d48b3c6
NuclearTea <muhammad.ali4@ucalgary.ca>
2025-04-26 12:25:44 -0600

    Add Boom Pins, remove LFS, update tests

--- firmware/Core/Inc/system/system_low_power_mode.h
+++ firmware/Core/Inc/system/system_low_power_mode.h
@@ -2,16 +2,22 @@
 #define INCLUDE_GUARD__SYS_LOW_POWER_MODE
 
 #include <stdint.h>
-// Order matters here as all the systems disabled using channels are first
+
+// Number of channels that are disabled when entering low power mode
+#define LOW_POWER_MODE_DISABLED_CHANNEL_COUNT 5
+#define LOW_POWER_MODE_JSON_STRING_LEN 275
+
+// Order matters here as all channels are disabled first
 typedef enum {
     SYS_LOW_POWER_MODE_ERROR_MPI_5V = 1 << 0,
     SYS_LOW_POWER_MODE_ERROR_MPI_12V = 1 << 1,
     SYS_LOW_POWER_MODE_ERROR_CAMERA = 1 << 2,
-    SYS_LOW_POWER_MODE_ERROR_BOOM = 1 << 3,
-    SYS_LOW_POWER_MODE_ERROR_GPS = 1 << 4,
-    SYS_LOW_POWER_MODE_ERROR_ADCS = 1 << 5,
-    SYS_LOW_POWER_MODE_ERROR_LFS = 1 << 6,
+    SYS_LOW_POWER_MODE_ERROR_GPS = 1 << 3,
+    SYS_LOW_POWER_MODE_ERROR_BOOM_12V = 1 << 4,
+    SYS_LOW_POWER_MODE_ERROR_BOOM_PINS = 1 << 5,
+    SYS_LOW_POWER_MODE_ERROR_ADCS = 1 << 6,
 } SYS_low_power_mode_error_enum_t;
+
 
 uint8_t SYS_enter_low_power_mode();
 uint8_t SYS_check_eps_and_enter_low_power_mode();
--- firmware/Core/Src/unit_tests/test_system.c
+++ firmware/Core/Src/unit_tests/test_system.c
@@ -30,23 +30,23 @@ uint8_t TEST_EXEC__SYS_low_power_mode_error_enum_to_string(void)
     const char* mpi_12v_str = SYS_low_power_mode_error_enum_to_string(mpi_12v_error);
     TEST_ASSERT(strcmp(mpi_12v_str, mpi_12v_expected_string) == 0);
 
-    // Boom
+    // Camera
     SYS_low_power_mode_error_enum_t camera_error = SYS_LOW_POWER_MODE_ERROR_CAMERA;
     const char* camera_expected_string = "CAMERA";
     const char* camera_str = SYS_low_power_mode_error_enum_to_string(camera_error);
     TEST_ASSERT(strcmp(camera_str, camera_expected_string) == 0);
 
-    // Camera
-    SYS_low_power_mode_error_enum_t boom_error = SYS_LOW_POWER_MODE_ERROR_BOOM;
-    const char* boom_expected_string = "BOOM";
+    // BOOM 12V
+    SYS_low_power_mode_error_enum_t boom_error = SYS_LOW_POWER_MODE_ERROR_BOOM_12V;
+    const char* boom_expected_string = "BOOM_12V";
     const char* boom_str = SYS_low_power_mode_error_enum_to_string(boom_error);
     TEST_ASSERT(strcmp(boom_str, boom_expected_string) == 0);
 
-    // LFS
-    SYS_low_power_mode_error_enum_t lfs_error = SYS_LOW_POWER_MODE_ERROR_LFS;
-    const char* lfs_expected_string = "LFS";
-    const char* lfs_str = SYS_low_power_mode_error_enum_to_string(lfs_error);
-    TEST_ASSERT(strcmp(lfs_str, lfs_expected_string) == 0);
+    // BOOM PINS
+    SYS_low_power_mode_error_enum_t boom_pins_error = SYS_LOW_POWER_MODE_ERROR_BOOM_PINS;
+    const char* boom_pins_expected_string = "BOOM_PINS";
+    const char* boom_pins_str = SYS_low_power_mode_error_enum_to_string(boom_pins_error);
+    TEST_ASSERT(strcmp(boom_pins_str, boom_pins_expected_string) == 0);
 
     // Unknown 
     SYS_low_power_mode_error_enum_t invalid_error = 0xFF;
@@ -61,7 +61,15 @@ uint8_t TEST_EXEC__SYS_low_power_mode_error_result_to_json(void)
     char buffer[256] = {0};
 
     // Test case 1: All systems successfully disabled
-    const char *all_success_json = "{\"MPI_5V\":\"Successfully Disabled\", \"MPI_12V\":\"Successfully Disabled\", \"CAMERA\":\"Successfully Disabled\", \"BOOM\":\"Successfully Disabled\", \"GPS\":\"Successfully Set Low Power Mode\", \"ADCS\":\"Successfully Set Low Power Mode\", \"LFS\":\"Successfully Unmounted\"}";
+    const char *all_success_json = 
+    "{\"MPI_5V\":\"Successfully Disabled\", "
+    "\"MPI_12V\":\"Successfully Disabled\", "
+    "\"CAMERA\":\"Successfully Disabled\", "
+    "\"BOOM_12V\":\"Successfully Disabled\", "
+    "\"BOOM_PINS\":\"Successfully Disabled Boom Pins\", "
+    "\"GPS\":\"Successfully Set Low Power Mode\", "
+    "\"ADCS\":\"Successfully Set Low Power Mode\"}";
+
     SYS_low_power_mode_error_enum_t all_success = 0;
     uint8_t all_success_result = SYS_low_power_mode_error_result_to_json(all_success, buffer, sizeof(buffer));
     TEST_ASSERT(all_success_result == 0);
@@ -70,22 +78,59 @@ uint8_t TEST_EXEC__SYS_low_power_mode_error_result_to_json(void)
     memset(buffer, 0, sizeof(buffer));
 
     // Test case 2: All systems failed
-    const char *all_error_json = "{\"MPI_5V\":\"Error Disabling\", \"MPI_12V\":\"Error Disabling\", \"CAMERA\":\"Error Disabling\", \"BOOM\":\"Error Disabling\", \"GPS\":\"Error Setting Low Power Mode\", \"ADCS\":\"Error Setting Low Power Mode\", \"LFS\":\"Error Unmounting\"}";
-    SYS_low_power_mode_error_enum_t all_error = SYS_LOW_POWER_MODE_ERROR_MPI_5V | SYS_LOW_POWER_MODE_ERROR_MPI_12V |
-                                           SYS_LOW_POWER_MODE_ERROR_CAMERA | SYS_LOW_POWER_MODE_ERROR_BOOM |
-                                           SYS_LOW_POWER_MODE_ERROR_GPS | SYS_LOW_POWER_MODE_ERROR_ADCS |
-                                           SYS_LOW_POWER_MODE_ERROR_LFS;
+    const char *all_error_json = 
+    "{\"MPI_5V\":\"Error Disabling\", "
+    "\"MPI_12V\":\"Error Disabling\", "
+    "\"CAMERA\":\"Error Disabling\", "
+    "\"BOOM_12V\":\"Error Disabling\", "
+    "\"BOOM_PINS\":\"Error Disabling Boom Pins\", "
+    "\"GPS\":\"Error Setting Low Power Mode\", "
+    "\"ADCS\":\"Error Setting Low Power Mode\"}";
+
+    SYS_low_power_mode_error_enum_t all_error = SYS_LOW_POWER_MODE_ERROR_MPI_5V | 
+                                               SYS_LOW_POWER_MODE_ERROR_MPI_12V | 
+                                               SYS_LOW_POWER_MODE_ERROR_CAMERA | 
+                                               SYS_LOW_POWER_MODE_ERROR_BOOM_12V | 
+                                               SYS_LOW_POWER_MODE_ERROR_BOOM_PINS | 
+                                               SYS_LOW_POWER_MODE_ERROR_GPS | 
+                                               SYS_LOW_POWER_MODE_ERROR_ADCS;
     uint8_t all_error_result = SYS_low_power_mode_error_result_to_json(all_error, buffer, sizeof(buffer));
     TEST_ASSERT(all_error_result == 0);
     TEST_ASSERT(strcmp(buffer, all_error_json) == 0);
+
     memset(buffer, 0, sizeof(buffer));
-    
-    // Test case 3: Mixed results, ADCS and MPI 12V failed
-    const char *adcs_mpi12v_error_json = "{\"MPI_5V\":\"Successfully Disabled\", \"MPI_12V\":\"Error Disabling\", \"CAMERA\":\"Successfully Disabled\", \"BOOM\":\"Successfully Disabled\", \"GPS\":\"Successfully Set Low Power Mode\", \"ADCS\":\"Error Setting Low Power Mode\", \"LFS\":\"Successfully Unmounted\"}";
+
+    // Test case 3.0: Mixed results, ADCS and MPI 12V failed
+    const char *adcs_mpi12v_error_json = 
+    "{\"MPI_5V\":\"Successfully Disabled\", "
+    "\"MPI_12V\":\"Error Disabling\", "
+    "\"CAMERA\":\"Successfully Disabled\", "
+    "\"BOOM_12V\":\"Successfully Disabled\", "
+    "\"BOOM_PINS\":\"Successfully Disabled Boom Pins\", "
+    "\"GPS\":\"Successfully Set Low Power Mode\", "
+    "\"ADCS\":\"Error Setting Low Power Mode\"}";
+
     SYS_low_power_mode_error_enum_t adcs_mpi12v_error = SYS_LOW_POWER_MODE_ERROR_ADCS | SYS_LOW_POWER_MODE_ERROR_MPI_12V;
     uint8_t adcs_mpi12v_result = SYS_low_power_mode_error_result_to_json(adcs_mpi12v_error, buffer, sizeof(buffer));
     TEST_ASSERT(adcs_mpi12v_result == 0);
     TEST_ASSERT(strcmp(buffer, adcs_mpi12v_error_json) == 0);
+
+    memset(buffer, 0, sizeof(buffer));
+
+    // Test case 3.1: Mixed results, BOOM Pins, and GPS failed
+    const char *boom_gps_error_json = 
+    "{\"MPI_5V\":\"Successfully Disabled\", "
+    "\"MPI_12V\":\"Successfully Disabled\", "
+    "\"CAMERA\":\"Successfully Disabled\", "
+    "\"BOOM_12V\":\"Successfully Disabled\", "
+    "\"BOOM_PINS\":\"Error Disabling Boom Pins\", "
+    "\"GPS\":\"Error Setting Low Power Mode\", "
+    "\"ADCS\":\"Successfully Set Low Power Mode\"}";
+
+    SYS_low_power_mode_error_enum_t boom_gps_error = SYS_LOW_POWER_MODE_ERROR_BOOM_PINS | SYS_LOW_POWER_MODE_ERROR_GPS;
+    uint8_t boom_gps_result = SYS_low_power_mode_error_result_to_json(boom_gps_error, buffer, sizeof(buffer));
+    TEST_ASSERT(boom_gps_result == 0);
+    TEST_ASSERT(strcmp(buffer, boom_gps_error_json) == 0);
 
     memset(buffer, 0, sizeof(buffer));
 
@@ -94,7 +139,6 @@ uint8_t TEST_EXEC__SYS_low_power_mode_error_result_to_json(void)
     uint8_t zero_size_result = SYS_low_power_mode_error_result_to_json(zero_size_error, buffer, 0);
     TEST_ASSERT(zero_size_result == 1); // Expecting failure due to zero buffer size
     TEST_ASSERT(strcmp(buffer, "") == 0); // Buffer should remain empty
-
 
     // Test case 5: Buffer too small
     char small_buffer[10] = {0};
