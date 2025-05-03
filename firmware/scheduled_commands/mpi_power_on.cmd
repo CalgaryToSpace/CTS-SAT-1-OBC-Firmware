82ba289f2fb845eb45ad56a9d7fe8597d3c6e535
Johnathan Burchill <j.burchill@ucalgary.ca>
2025-05-02 18:15:13 -0600

    MPI auto power on at start

---
+++ firmware/scheduled_commands/mpi_power_on.cmd
@@ -0,0 +1,8 @@
+r5000 .sync_time
+r10000 get_system_time()
+r13000 eps_get_enabled_channels_json()
+r15000 eps_set_channel_enabled(mpi_12v,1)
+r17000 eps_set_channel_enabled(mpi_5v,1)
+r20000 eps_get_enabled_channels_json()
+r30000 mpi_send_command_hex(54 43 08)
+
