291ba50a2a6fb470bab7cbed1ad104d44a01d9ea
NuclearTea <muhammad.ali4@ucalgary.ca>
2025-05-05 18:14:54 -0600

    Add new eps channel to disable gnss, update names, update tests

--- firmware/Core/Inc/system/system_low_power_mode.h
+++ firmware/Core/Inc/system/system_low_power_mode.h
@@ -4,17 +4,17 @@
 #include <stdint.h>
 
 // Number of channels that are disabled when entering low power mode
-#define LOW_POWER_MODE_DISABLED_CHANNEL_COUNT 4
+#define LOW_POWER_MODE_DISABLED_CHANNEL_COUNT 5
 #define LOW_POWER_MODE_JSON_STRING_LEN 275
 
 // Order matters here as all channels are disabled first
 typedef enum {
     SYS_LOW_POWER_MODE_ERROR_MPI_5V = 1 << 0,
     SYS_LOW_POWER_MODE_ERROR_MPI_12V = 1 << 1,
-    SYS_LOW_POWER_MODE_ERROR_CAMERA = 1 << 2,
-    SYS_LOW_POWER_MODE_ERROR_BOOM_12V = 1 << 3,
-    SYS_LOW_POWER_MODE_ERROR_BOOM_PINS = 1 << 4,
-    SYS_LOW_POWER_MODE_ERROR_GPS = 1 << 5,
+    SYS_LOW_POWER_MODE_ERROR_CAMERA_3V3 = 1 << 2,
+    SYS_LOW_POWER_MODE_ERROR_GNSS_3V3 = 1 << 3,
+    SYS_LOW_POWER_MODE_ERROR_BOOM_12V = 1 << 4,
+    SYS_LOW_POWER_MODE_ERROR_BOOM_PINS = 1 << 5,
     SYS_LOW_POWER_MODE_ERROR_ADCS = 1 << 6,
 } SYS_low_power_mode_error_enum_t;
 
