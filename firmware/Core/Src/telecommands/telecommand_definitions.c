939d9e5345e247b598fdd9064bab74f96349f2ff
DeemDeem52 <42725035+DeemDeem52@users.noreply.github.com>
2025-05-07 07:14:26 -0600

    ADCS: Free up some unused telecommands (#379)
    * Delete unused ADCS GPS telecommands
    
    * Remove done todo
    
    * Remove another unused command
    
    * Finish removing unused ADCS GPS telecommands

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -695,36 +695,6 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
     {
-        .tcmd_name = "adcs_raw_gps_status",
-        .tcmd_func = TCMDEXEC_adcs_raw_gps_status,
-        .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
-    },
-    {
-        .tcmd_name = "adcs_raw_gps_time",
-        .tcmd_func = TCMDEXEC_adcs_raw_gps_time,
-        .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
-    },
-    {
-        .tcmd_name = "adcs_raw_gps_x",
-        .tcmd_func = TCMDEXEC_adcs_raw_gps_x,
-        .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
-    },
-    {
-        .tcmd_name = "adcs_raw_gps_y",
-        .tcmd_func = TCMDEXEC_adcs_raw_gps_y,
-        .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
-    },
-    {
-        .tcmd_name = "adcs_raw_gps_z",
-        .tcmd_func = TCMDEXEC_adcs_raw_gps_z,
-        .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
-    },
-    {
         .tcmd_name = "adcs_measurements",
         .tcmd_func = TCMDEXEC_adcs_measurements,
         .number_of_args = 0,
@@ -734,7 +704,7 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .tcmd_name = "adcs_download_sd_file",
         .tcmd_func = TCMDEXEC_adcs_download_sd_file,
         .number_of_args = 1,
-        .readiness_level = TCMD_READINESS_LEVEL_IN_PROGRESS, // TODO: change this once the function is done
+        .readiness_level = TCMD_READINESS_LEVEL_IN_PROGRESS,
     }, 
     {
         .tcmd_name = "adcs_generic_command",
@@ -760,12 +730,6 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .number_of_args = 0,
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
-    {
-        .tcmd_name = "adcs_get_raw_star_tracker_data",
-        .tcmd_func = TCMDEXEC_adcs_get_raw_star_tracker_data,
-        .number_of_args = 0,
-        .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
-    }, 
     {
         .tcmd_name = "adcs_save_image_to_sd",
         .tcmd_func = TCMDEXEC_adcs_save_image_to_sd,
