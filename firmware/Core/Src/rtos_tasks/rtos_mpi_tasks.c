a9b92680ec16caaa1f7d393a1014f688ca0ca76b
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-05-08 22:26:51 -0600

    style: Fix long lines, logging in RTOS task

--- firmware/Core/Src/rtos_tasks/rtos_mpi_tasks.c
+++ firmware/Core/Src/rtos_tasks/rtos_mpi_tasks.c
@@ -11,21 +11,21 @@
 void TASK_service_write_mpi_data(void *argument) {
     TASK_HELP_start_of_task();
     osDelay(1000);
-    
-    uint32_t start_time;
 
     while(1) {
         // Add a check that would see if there is data to be written regardless of RX Mode, we write
-        if (MPI_buffer_state == MPI_MEMORY_WRITE_STATUS_PENDING && MPI_receive_prepared) {
+        if ((MPI_buffer_state == MPI_MEMORY_WRITE_STATUS_PENDING) && MPI_receive_prepared) {
 
-            start_time = HAL_GetTick();
+            const uint32_t start_time = HAL_GetTick();
             if (!LFS_is_lfs_mounted) {
-                LFS_mount();
-                LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_all_sinks_except(LOG_SINK_FILE), 
-                "Had to mount LFS (System wasn't prepared for MPI data)");
+                const int8_t mount_result = LFS_mount();
+                LOG_message(
+                    LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_SINK_ALL, 
+                    "MPI Task: Had to mount LFS. LFS_mount->%d. Took %lu ms",
+                    mount_result,
+                    HAL_GetTick() - start_time
+                );
             }
-            LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_NORMAL, LOG_all_sinks_except(LOG_SINK_FILE), "Mount Time: %lu", HAL_GetTick() - start_time);
-
 
             // Write data to file
             const lfs_ssize_t write_result = lfs_file_write(
@@ -34,31 +34,49 @@ void TASK_service_write_mpi_data(void *argument) {
                 MPI_active_data_median_buffer_len
             );
 
-            if (write_result < 0)
-            {
-                LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_all_sinks_except(LOG_SINK_FILE), "Error writing to file: %ld", write_result);
-            } else {
-                LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_NORMAL, LOG_all_sinks_except(LOG_SINK_FILE), "Successfully Wrote 4096 bytes");
+            if (write_result < 0) {
+                LOG_message(
+                    LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_SINK_ALL,
+                    "MPI Task: Error writing to file: %ld", write_result
+                );
+            }
+            else {
+                LOG_message(
+                    LOG_SYSTEM_MPI, LOG_SEVERITY_DEBUG, LOG_SINK_ALL,
+                    "MPI Task: Successfully wrote %ld bytes to file",
+                    write_result
+                );
                 MPI_buffer_state = MPI_MEMORY_WRITE_STATUS_READY;
             }
-            LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_NORMAL, LOG_all_sinks_except(LOG_SINK_FILE), "Write Time: %lu", HAL_GetTick() - start_time);
-
-        } else {
-            if (MPI_receive_prepared == 2 && MPI_current_uart_rx_mode != MPI_RX_MODE_SENSING_MODE) {
+            LOG_message(
+                LOG_SYSTEM_MPI, LOG_SEVERITY_DEBUG, LOG_SINK_ALL,
+                "MPI Task: Write Time: %lu", HAL_GetTick() - start_time
+            );
+        }
+        else {
+            if ((MPI_receive_prepared == 2) && (MPI_current_uart_rx_mode != MPI_RX_MODE_SENSING_MODE)) {
                 // Close the File, the storage is not updated until the file is closed successfully
                 const int8_t close_result = lfs_file_close(&LFS_filesystem, &MPI_science_data_file_pointer);
-                if (close_result < 0)
-                {
-                    LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_all_sinks_except(LOG_SINK_FILE), "Error closing file: %d", close_result);
-                } 
-                LOG_message(LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_all_sinks_except(LOG_SINK_FILE), "File Successfully closed");
+                if (close_result < 0) {
+                    LOG_message(
+                        LOG_SYSTEM_MPI, LOG_SEVERITY_WARNING, LOG_SINK_ALL,
+                        "MPI Task: Error closing file: %d", close_result
+                    );
+                }
+                else {
+                    LOG_message(
+                        LOG_SYSTEM_MPI, LOG_SEVERITY_NORMAL, LOG_SINK_ALL,
+                        "MPI Task: File closed successfully"
+                    );
+                }
                 MPI_receive_prepared = 0;
                 MPI_science_data_file_is_open = 0;
             }
         }
         if (MPI_current_uart_rx_mode == MPI_RX_MODE_SENSING_MODE) {
             osDelay(100);
-        } else {
+        }
+        else {
             osDelay(1000);
         }
     }
