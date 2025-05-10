c9134fb0fc134d8b3f88b54f53271725c408fdb6
fardin.mahid <fardin.mahid@ucalgary.ca>
2025-05-09 20:07:17 -0600

    Error fixing

--- firmware/Core/Src/obc_systems/adc_vbat_monitor.c
+++ firmware/Core/Src/obc_systems/adc_vbat_monitor.c
@@ -4,11 +4,11 @@
 
 
 
-float return_VBAT_Voltage(){
+double return_VBAT_Voltage(){
 
     HAL_ADC_Start(&hadc1);
     HAL_ADC_PollForConversion(&hadc1, 1000);
-    float v_raw =  (HAL_ADC_GetValue(&hadc1) *3.3) / 4095.0; 
+    double v_raw =  (HAL_ADC_GetValue(&hadc1) *3.3) / 4095.0; 
 
     return v_raw * 1000;
 }
