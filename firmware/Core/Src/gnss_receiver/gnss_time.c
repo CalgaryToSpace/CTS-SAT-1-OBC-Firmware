c2d3241da61f979eeb83a6f4d3b62beea616f0ee
celinahy <113138309+celinahy@users.noreply.github.com>
2025-05-10 13:53:36 -0600

    base files for 'obc time sync from gnss time' telecommand setup

---
+++ firmware/Core/Src/gnss_receiver/gnss_time.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <string.h>
+
+/// @brief Sets the OBC's time, based on the GNS receiver's current time.
+/// @return 0 on success, >0 on failure.
+uint8_t GNSS_set_obc_time_based_on_gnss_time() {
+
+    return 0;
+}
