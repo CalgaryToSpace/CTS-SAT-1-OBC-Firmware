07bc6e1243724f9738397fa7c4a169ac254cafa3
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2025-05-10 00:34:23 -0600

    Allowed file name to be passed for MPI active mode

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -1083,7 +1083,7 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
     {
         .tcmd_name = "mpi_enable_active_mode",
         .tcmd_func = TCMDEXEC_mpi_enable_active_mode,
-        .number_of_args = 0,
+        .number_of_args = 1,
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
     {
