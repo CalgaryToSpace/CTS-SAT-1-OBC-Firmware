319f3774afb6b6b98ac342bbb2325fd30c1d8222
celinahy <113138309+celinahy@users.noreply.github.com>
2025-05-10 13:53:36 -0600

    base files for 'obc time sync from gnss time' telecommand setup

---
+++ firmware/Core/Src/gps/gps_time.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <string.h>
+
+/// @brief Sets the OBC's time, based on the GPS's current time.
+/// @return 0 on success, >0 on failure.
+uint8_t GPS_set_obc_time_based_on_gps_time() {
+
+    return 0;
+}
