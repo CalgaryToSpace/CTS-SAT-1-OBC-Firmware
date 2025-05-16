6740f5a1cdeac93454fe18347a5476ed0766f7a7
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-05-15 20:04:59 -0600

    fix: Swap ANT1/ANT2 switch control so ANT1=RF1=J20 when UHF_CTL=LOW
    Fixes #417. See #417 for details/proof.

--- firmware/Core/Src/comms_drivers/comms_drivers.c
+++ firmware/Core/Src/comms_drivers/comms_drivers.c
@@ -2,18 +2,18 @@
 #include "log/log.h"
 #include "main.h"
 
-uint8_t COMMS_current_active_antenna = 2; // Default: LOW. Thus, Antenna 2.
+uint8_t COMMS_current_active_antenna = 1; // Default: LOW. Thus, Antenna 1.
 
 /// @brief Sets the state of the dipole switch on the OBC to either Antenna 1 or Antenna 2.
 /// @param dipole_switch_antenna_num Either 1 or 2. If not 1 or 2, then remains unchanged.
-/// @note Created per https://github.com/CalgaryToSpace/CTS-SAT-1-OBC-Firmware/issues/226.
+/// @note Specification in https://github.com/CalgaryToSpace/CTS-SAT-1-OBC-Firmware/issues/417.
 void COMMS_set_dipole_switch_state(uint8_t dipole_switch_antenna_num) {
     if (dipole_switch_antenna_num == 1) {
-        HAL_GPIO_WritePin(PIN_UHF_CTRL_OUT_GPIO_Port, PIN_UHF_CTRL_OUT_Pin, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(PIN_UHF_CTRL_OUT_GPIO_Port, PIN_UHF_CTRL_OUT_Pin, GPIO_PIN_RESET);
         COMMS_current_active_antenna = 1;
     }
     else if (dipole_switch_antenna_num == 2) {
-        HAL_GPIO_WritePin(PIN_UHF_CTRL_OUT_GPIO_Port, PIN_UHF_CTRL_OUT_Pin, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(PIN_UHF_CTRL_OUT_GPIO_Port, PIN_UHF_CTRL_OUT_Pin, GPIO_PIN_SET);
         COMMS_current_active_antenna = 2;
     }
     else {
