bf78c1f5dacdfc29b241d82c579c0efbc62c9d37
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-05-17 16:30:50 -0600

    Implement bootup operation FSM per Issue #420

---
+++ firmware/Core/Inc/rtos_tasks/rtos_bootup_operation_fsm_task.h
@@ -0,0 +1,26 @@
+#ifndef INCLUDE_GUARD__RTOS_BOOTUP_OPERATION_FSM_TASK_H
+#define INCLUDE_GUARD__RTOS_BOOTUP_OPERATION_FSM_TASK_H
+
+typedef enum {
+    // Entry point. Booted, and awaiting a trigger to deployment. Emulate a 30-minute countdown to antenna deployment.
+    CTS1_OPERATION_STATE_BOOTED_AND_WAITING,
+
+    // Intermediate state. Very short-lived nominally. Deploy deployable antennas here.
+    CTS1_OPERATION_STATE_DEPLOYING,
+
+    // Normal operation mode, with radio transmission enabled (e.g., flying through space).
+    CTS1_OPERATION_STATE_NOMINAL_WITH_RADIO_TX,
+
+    // Normal operation mode, with radio transmission disabled (e.g., during bench testing).
+    CTS1_OPERATION_STATE_NOMINAL_WITHOUT_RADIO_TX
+} CTS1_operation_state_enum_t;
+
+extern CTS1_operation_state_enum_t CTS1_operation_state;
+
+
+char* CTS1_operation_state_enum_TO_str(CTS1_operation_state_enum_t mode);
+
+
+void TASK_bootup_operation_fsm(void *argument);
+
+#endif // INCLUDE_GUARD__RTOS_BOOTUP_OPERATION_FSM_TASK_H
