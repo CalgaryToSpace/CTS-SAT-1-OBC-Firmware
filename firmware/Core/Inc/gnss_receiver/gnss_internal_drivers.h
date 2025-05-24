2e8f6d78f6ae3c02ace5c13f504d35f9b637d2b0
NuclearTea <muhammad.ali4@ucalgary.ca>
2025-05-24 10:56:44 -0600

    Rename functions, types and some variables in gnss_receiver folders

--- firmware/Core/Inc/gnss_receiver/gnss_internal_drivers.h
+++ firmware/Core/Inc/gnss_receiver/gnss_internal_drivers.h
@@ -1,15 +1,13 @@
-
-#ifndef INCLUDE_GUARD__GPS_INTERNAL_DRIVERS_H__
-#define INCLUDE_GUARD__GPS_INTERNAL_DRIVERS_H__
+#ifndef INCLUDE_GUARD__GNSS_INTERNAL_DRIVERS_H__
+#define INCLUDE_GUARD__GNSS_INTERNAL_DRIVERS_H__
 
 #include <stdint.h>
 
-uint8_t GPS_send_cmd_get_response(
+uint8_t GNSS_send_cmd_get_response(
     const char *cmd_buf, uint8_t cmd_buf_len,
     uint8_t rx_buf[],
     const uint16_t rx_buf_max_size,
     uint16_t* rx_buf_len_dest
 );
 
-
-#endif /* INCLUDE_GUARD__GPS_INTERNAL_DRIVERS_H__ */
+#endif /* INCLUDE_GUARD__GNSS_INTERNAL_DRIVERS_H__ */
