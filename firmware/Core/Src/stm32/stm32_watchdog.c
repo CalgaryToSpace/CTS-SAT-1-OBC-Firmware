95bc91dc96d72a3861af650f543e109573be1729
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-05-30 14:00:14 -0600

    Fix operation order bug

--- firmware/Core/Src/stm32/stm32_watchdog.c
+++ firmware/Core/Src/stm32/stm32_watchdog.c
@@ -8,37 +8,40 @@
 extern IWDG_HandleTypeDef hiwdg;
 
 /// @brief Uptime at the last time the watchdog was petted, in milliseconds.
-uint32_t STM32_watchdog_uptime_last_pet_ms = 0;
+volatile uint32_t STM32_watchdog_uptime_last_pet_ms = 0;
 
 /// @brief Total number of times the STM32 watchdog has been petted since boot.
-uint32_t STM32_watchdog_total_pets_since_boot = 0;
+volatile uint32_t STM32_watchdog_total_pets_since_boot = 0;
 
 /// @brief Pet the STM32 Independent Watchdog (IWDG).
 /// @note Also, updates tracking variables and logs warnings if the petting took too long or too short.
 /// @note The watchdog must be petted at least once every 16 seconds, and no more than once every 200ms.
-/// @note A warning is issued if the time since the last pet is more than 15 seconds or less than 1 second.
+/// @note A warning is issued if the time since the last pet is more than 15 seconds or less than 240ms.
 void STM32_pet_watchdog() {
     // Actually pet the watchdog.
     HAL_IWDG_Refresh(&hiwdg);
 
     // Do checks about stats of watchdog petting.
     const uint32_t cur_time_ms = HAL_GetTick();
+
+    // Store the time since the last pet. Must come BEFORE updating the global variables.
+    const int32_t time_since_last_pet_ms = cur_time_ms - STM32_watchdog_uptime_last_pet_ms;
+
     STM32_watchdog_uptime_last_pet_ms = cur_time_ms;
     STM32_watchdog_total_pets_since_boot++;
 
-    // Check the interval since the last pet.
-    const uint32_t time_since_last_pet_ms = cur_time_ms - STM32_watchdog_uptime_last_pet_ms;
+    // Validate the interval since the last pet.
     if (time_since_last_pet_ms > 15000) {
         LOG_message(
             LOG_SYSTEM_OBC, LOG_SEVERITY_WARNING, LOG_SINK_ALL,
-            "STM32 watchdog petting took a long time: %lu ms since last pet (>15 sec)",
+            "STM32 watchdog petting took a long time: %ld ms since last pet (>15 sec)",
             time_since_last_pet_ms
         );
     }
-    else if (time_since_last_pet_ms < 1000) {
+    else if (time_since_last_pet_ms < 240) {
         LOG_message(
             LOG_SYSTEM_OBC, LOG_SEVERITY_DEBUG, LOG_SINK_ALL,
-            "STM32 watchdog petting took a short time: %lu ms since last pet (<1000ms)",
+            "STM32 watchdog petting took a short time: %ld ms since last pet (<240ms)",
             time_since_last_pet_ms
         );
     }
