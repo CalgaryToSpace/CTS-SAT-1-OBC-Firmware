915ed8a1a3304b3018c66349b29ebff177c10f61
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-06-03 01:16:58 -0600

    fix: bad syntax from rebase

--- firmware/Core/Src/telecommands/telecommand_definitions.c
+++ firmware/Core/Src/telecommands/telecommand_definitions.c
@@ -1092,6 +1092,7 @@ const TCMD_TelecommandDefinition_t TCMD_telecommand_definitions[] = {
         .number_of_args = 0,
         .readiness_level = TCMD_READINESS_LEVEL_FOR_OPERATION,
     },
+    {
         .tcmd_name = "mpi_send_firmware_upgrade",
         .tcmd_func = TCMDEXEC_mpi_send_firmware_upgrade,
         .number_of_args = 1,
