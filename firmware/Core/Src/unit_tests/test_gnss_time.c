2ad06dd8fdd1c4f0db2714825f6bba8a47dc8bc9
meagan-davies <128192488+meagan-davies@users.noreply.github.com>
2025-06-04 17:02:34 -0600

    static portable time and function rename

--- firmware/Core/Src/unit_tests/test_gnss_time.c
+++ firmware/Core/Src/unit_tests/test_gnss_time.c
@@ -10,26 +10,16 @@
 #include <string.h>
 
 /**
- * @brief Unit test for GNSS_format_and_convert_to_unix_epoch
+ * @brief Unit test for GNSS_parse_timea_response_and_convert_to_unix_time_ms
  */
-uint8_t TEST_EXEC__GNSS_format_and_convert_to_unix_epoch() {
+uint8_t TEST_EXEC__GNSS_parse_timea_response_and_convert_to_unix_time_ms() {
     // Example valid GNSS TIMEA message
     char test_str_valid[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,18,20,28526,VALID*a2ff113a";
 
-    // Expected epoch milliseconds for 2025-05-22 18:20:28.526 UTC
-    struct tm t = {0};
-    t.tm_year = 2025 - 1900;
-    t.tm_mon  = 5 - 1;
-    t.tm_mday = 22;
-    t.tm_hour = 18;
-    t.tm_min  = 20;
-    t.tm_sec  = 28;
-
-    time_t expected_seconds = portable_timegm(&t);
-    uint64_t expected_ms = (uint64_t)expected_seconds * 1000 + 526;
+    const uint64_t expected_ms = 1747947628526;
 
     // Test valid case
-    uint64_t result = GNSS_format_and_convert_to_unix_epoch(test_str_valid);
+    uint64_t result = GNSS_parse_timea_response_and_convert_to_unix_time_ms(test_str_valid);
     TEST_ASSERT_TRUE(result == expected_ms);
 
     return 0;  // Indicate test passed
