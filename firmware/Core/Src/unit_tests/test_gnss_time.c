cb73062daf8398d5c4460d6ae1bce4601b7715a7
meagan-davies <128192488+meagan-davies@users.noreply.github.com>
2025-06-05 22:13:34 -0600

    Fixed commenting and unit test strings

--- firmware/Core/Src/unit_tests/test_gnss_time.c
+++ firmware/Core/Src/unit_tests/test_gnss_time.c
@@ -9,26 +9,24 @@
 #include <stdint.h>
 #include <string.h>
 
-/**
- * @brief Unit test for GNSS_parse_timea_response_and_convert_to_unix_time_ms
- */
+/// @brief Unit test for GNSS_parse_timea_response_and_convert_to_unix_time_ms
 uint8_t TEST_EXEC__GNSS_parse_timea_response_and_convert_to_unix_time_ms() {
     // Example valid GNSS TIMEA message
-    char test_str_valid_1[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,18,20,28526,VALID*a2ff113a";
-    char test_str_valid_2[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2027,10,10,05,09,19000,VALID*a2ff113a";
+    char test_str_valid_1[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,9,0,28526,VALID*a2ff113a";
+    char test_str_valid_2[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2027,10,10,11,9,19000,VALID*a2ff113a";
 
     const uint64_t expected_ms_1 = 1747947628526; // UTC Time: Thu May 22 2025 21:00:28
     const uint64_t expected_ms_2 = 1823166559000; // UTC Time: 
 
     // Example of invalid and partially invalid GNSS TIMEA message
-    char test_str_invalid_1[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,18,20,28526,INVALID*a2ff113a";
-    char test_str_invalid_2[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;INVALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,18,20,28526,VALID*a2ff113a";
+    char test_str_invalid_1[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;VALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,9,0,28526,INVALID*a2ff113a";
+    char test_str_invalid_2[] = "<OK[COM1]#TIMEA,COM1,0,80.0,FINESTEERING,2367,411646.000,02040000,9924,17402;INVALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,9,0,28526,VALID*a2ff113a";
     
     const uint8_t error_response = 1; // Error for invalid response 1 (and subsequent)
     // test_str_invalid_2 should have same response as expected_ms_1
 
     // Example of invalid structure of string for GNSS TIMEA message
-    char test_str_invalid_3[] = "<OK[COM1]n/#TIMEA,COM1,0,80.0,HI,0,2367,411646.000,02040000,9924,17402;INVALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,18,20,28526,VALID*a2ff113a";
+    char test_str_invalid_3[] = "<OK[COM1]n/#TIMEA,COM1,0,80.0,HI,0,2367,411646.000,02040000,9924,17402;INVALID,-4.474795457e-08,9.384349250e-09,-18.00000000000,2025,5,22,9,0,28526,VALID*a2ff113a";
     // test_str_invalid_3 should have same response as error_response
 
     // Test valid cases
