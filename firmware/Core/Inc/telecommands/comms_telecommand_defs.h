7a43a958d6bf830e2d63c5c3015e371274297903
celinahy <113138309+celinahy@users.noreply.github.com>
2025-06-14 13:50:56 -0600

    boom_deploy_telecommand,camera_telecommand,comms_telecommand have TCMD_TelecommandChannel_enum_t removed

--- firmware/Core/Inc/telecommands/comms_telecommand_defs.h
+++ firmware/Core/Inc/telecommands/comms_telecommand_defs.h
@@ -6,25 +6,25 @@
 #include <stdint.h>
 
 uint8_t TCMDEXEC_comms_set_rf_switch_control_mode(
-    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    const char *args_str,
     char *response_output_buf, uint16_t response_output_buf_len
 );
 
 uint8_t TCMDEXEC_comms_get_rf_switch_info(
-    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    const char *args_str,
     char *response_output_buf, uint16_t response_output_buf_len
 );
 
 uint8_t TCMDEXEC_comms_bulk_file_downlink_start(
-    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    const char *args_str,
     char *response_output_buf, uint16_t response_output_buf_len
 );
 uint8_t TCMDEXEC_comms_bulk_file_downlink_pause(
-    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    const char *args_str,
     char *response_output_buf, uint16_t response_output_buf_len
 );
 uint8_t TCMDEXEC_comms_bulk_file_downlink_resume(
-    const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
+    const char *args_str,
     char *response_output_buf, uint16_t response_output_buf_len
 );
 
