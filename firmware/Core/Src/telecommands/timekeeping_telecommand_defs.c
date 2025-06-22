ac1368d686f6e3b0dc1999f5a3d12cd367c0f3e2
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-06-21 18:26:56 -0600

    fix: One more new telecommand

--- firmware/Core/Src/telecommands/timekeeping_telecommand_defs.c
+++ firmware/Core/Src/telecommands/timekeeping_telecommand_defs.c
@@ -80,8 +80,10 @@ uint8_t TCMDEXEC_set_obc_time_based_on_eps_time(const char *args_str,
 
 /// @brief Sync's OBC time to gnss time (+/- ~100ms)
 /// @return 0 on success, >0 on failure.
-uint8_t TCMDEXEC_set_obc_time_based_on_gnss_time(const char *args_str, TCMD_TelecommandChannel_enum_t tcmd_channel,
-                        char *response_output_buf, uint16_t response_output_buf_len) {
+uint8_t TCMDEXEC_set_obc_time_based_on_gnss_time(
+    const char *args_str,
+    char *response_output_buf, uint16_t response_output_buf_len
+) {
     const uint8_t result = GNSS_set_obc_time_based_on_gnss_time();
     if (result != 0 ) {
         snprintf(response_output_buf, response_output_buf_len,
