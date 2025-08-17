0d9f798222c50a8194b406f63901aed297c44e08
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2025-08-16 18:32:13 -0600

    feat(lua): add `print` binding

---
+++ firmware/Core/Inc/lua/lua_bindings.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_GUARD_LUA_BINDINGS_H
+#define INCLUDE_GUARD_LUA_BINDINGS_H 
+
+#include "lua.h"
+// #include "lua/lualib.h"
+// #include "lua/lauxlib.h"
+int l_print(lua_State *L);
+
+
+#endif // INCLUDE_GUARD_LUA_BINDINGS_H
