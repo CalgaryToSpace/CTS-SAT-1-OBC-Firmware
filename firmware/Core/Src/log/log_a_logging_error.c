d88c6df9bc736c536a6aa829fc1f17e839c0fb7f
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-02-02 22:28:40 -0700

    Improve docstring

--- firmware/Core/Src/log/log_a_logging_error.c
+++ firmware/Core/Src/log/log_a_logging_error.c
@@ -8,8 +8,9 @@
 
 /// @brief Log an error which occurs while writing to the file log sink.
 /// @param message Message explaining the error.
-/// @note Why? We can't use the normal LOG_message function from within log
-///     sinks, because the log message gets stored into a temporary log buffer.
+/// @note Why? Because we can't use the normal LOG_message function from within log
+///     sink implementations, because the log message gets formatted/stored into a
+///     statically-allocated buffer.
 void LOG_log_a_logging_error_if_file_is_broken(const char message[]) {
     // File logging is broken. Log to everything not a file!
     LOG_to_umbilical_uart("\n");
