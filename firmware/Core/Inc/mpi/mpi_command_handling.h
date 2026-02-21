10b64724e78e59cc3131befca47f541133e96e3e
parker-research <166864283+parker-research@users.noreply.github.com>
2026-02-21 12:28:25 -0700

    MPI: Monitor temperature, report max temperature during collection

--- firmware/Core/Inc/mpi/mpi_command_handling.h
+++ firmware/Core/Inc/mpi/mpi_command_handling.h
@@ -17,6 +17,12 @@ extern uint32_t MPI_science_data_bytes_lost;
 extern lfs_file_t MPI_science_data_file_pointer;
 extern uint32_t MPI_recording_start_uptime_ms;
 
+typedef enum {
+    MPI_REASON_FOR_STOPPING_TEMPERATURE_EXCEEDED,
+    MPI_REASON_FOR_STOPPING_TELECOMMAND,
+    MPI_REASON_FOR_STOPPING_MAX_TIME_EXCEEDED // TODO: Use this https://github.com/CalgaryToSpace/CTS-SAT-1-OBC-Firmware/issues/488
+} MPI_reason_for_stopping_active_mode;
+
 uint8_t MPI_send_command_get_response(
     const uint8_t *bytes_to_send, const size_t bytes_to_send_len, 
     uint8_t *rx_buffer, const size_t rx_buffer_max_size, 
@@ -24,9 +30,11 @@ uint8_t MPI_send_command_get_response(
 );
 
 uint8_t MPI_enable_active_mode(const char output_file_path[]);
-uint8_t MPI_disable_active_mode();
+uint8_t MPI_disable_active_mode(MPI_reason_for_stopping_active_mode reason_for_stopping);
 uint8_t MPI_validate_command_response(
     const uint8_t command_code, uint8_t *rx_buffer, const uint16_t rx_buffer_len
 );
+
+char *MPI_reason_for_stopping_active_mode_enum_to_str(MPI_reason_for_stopping_active_mode reason);
 
 #endif /* INC_MPI_COMMAND_HANDLING_H_ */
