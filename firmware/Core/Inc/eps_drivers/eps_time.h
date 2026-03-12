be7dbf1a984b0fab0147341a70272dea6bb4f652
parker-research <166864283+parker-research@users.noreply.github.com>
2026-03-12 17:40:10 -0600

    fix bad include guard

--- firmware/Core/Inc/eps_drivers/eps_time.h
+++ firmware/Core/Inc/eps_drivers/eps_time.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_GUARD__EPS_TIME_H__
-#define ICLUDE_GUARD__EPS_TIME_H__
+#define INCLUDE_GUARD__EPS_TIME_H__
 
 uint8_t EPS_set_eps_time_based_on_obc_time();
 uint8_t EPS_set_obc_time_based_on_eps_time();
 
-#endif
+#endif /* INCLUDE_GUARD__EPS_TIME_H__ */
