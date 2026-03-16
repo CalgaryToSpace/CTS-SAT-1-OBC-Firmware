7b692ca6d4f61455f201b9958670621652bd1c0b
parker-research <166864283+parker-research@users.noreply.github.com>
2026-03-15 20:59:54 -0600

    Timekeeping: Allow <50ms-precision time sync via telecommand

--- firmware/Core/Inc/telecommands/timekeeping_telecommand_defs.h
+++ firmware/Core/Inc/telecommands/timekeeping_telecommand_defs.h
@@ -8,8 +8,15 @@
 uint8_t TCMDEXEC_get_system_time(const char *args_str,
                         char *response_output_buf, uint16_t response_output_buf_len);
 
-uint8_t TCMDEXEC_set_system_time(const char *args_str,
-                        char *response_output_buf, uint16_t response_output_buf_len);
+uint8_t TCMDEXEC_set_system_time_approx(
+    const char *args_str,
+    char *response_output_buf, uint16_t response_output_buf_len
+);
+
+uint8_t TCMDEXEC_set_system_time(
+    const char *args_str,
+    char *response_output_buf, uint16_t response_output_buf_len
+);
 
 uint8_t TCMDEXEC_correct_system_time(const char *args_str,
                         char *response_output_buf, uint16_t response_output_buf_len);
