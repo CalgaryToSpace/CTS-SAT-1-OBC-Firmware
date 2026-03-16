cbb4f7e202cf292f2ba1b29b381d626d31600f43
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-03-15 21:36:36 -0600

    IOC: Change LSE crystal drive capability

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -431,7 +431,7 @@ void SystemClock_Config(void)
   /** Configure LSE Drive Capability
   */
   HAL_PWR_EnableBkUpAccess();
-  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_HIGH);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
