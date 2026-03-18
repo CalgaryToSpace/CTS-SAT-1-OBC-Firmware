35de9565371c1d8ecdefdd19b9daa9bb8d607df7
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-03-18 16:50:04 -0600

    IOC: Reconfigure to HSE Ext. Clock

--- firmware/Core/Src/stm32l4xx_hal_msp.c
+++ firmware/Core/Src/stm32l4xx_hal_msp.c
@@ -107,8 +107,8 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
   */
     PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
     PeriphClkInit.AdcClockSelection = RCC_ADCCLKSOURCE_PLLSAI1;
-    PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
-    PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+    PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSE;
+    PeriphClkInit.PLLSAI1.PLLSAI1M = 2;
     PeriphClkInit.PLLSAI1.PLLSAI1N = 8;
     PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV2;
     PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
