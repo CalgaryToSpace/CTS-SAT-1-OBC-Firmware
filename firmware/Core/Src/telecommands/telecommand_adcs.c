6f5a7dc442baf73e578c9c2c86b702ad472f8349
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-03-18 19:36:55 -0600

    Update firmware/Core/Src/telecommands/telecommand_adcs.c
    Co-authored-by: Nadeem M <42725035+DeemDeem52@users.noreply.github.com>

--- firmware/Core/Src/telecommands/telecommand_adcs.c
+++ firmware/Core/Src/telecommands/telecommand_adcs.c
@@ -693,7 +693,7 @@ uint8_t TCMDEXEC_adcs_track_sun(const char *args_str,
     const uint8_t set_power_control_status = ADCS_set_power_control(
         ADCS_POWER_SELECT_ON, ADCS_POWER_SELECT_ON,
         ADCS_POWER_SELECT_ON, ADCS_POWER_SELECT_ON, // Fine sensors on.
-        ADCS_POWER_SELECT_OFF, // Cube Start always off.
+        ADCS_POWER_SELECT_OFF, // CubeStar always off.
         ADCS_POWER_SELECT_ON, ADCS_POWER_SELECT_ON, ADCS_POWER_SELECT_ON, // Wheels on.
         ADCS_POWER_SELECT_OFF, // Motor off. // TODO: Confirm.
         ADCS_POWER_SELECT_OFF // GPS always off.
