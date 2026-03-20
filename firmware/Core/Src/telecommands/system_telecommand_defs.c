678de22782d2954512786649d005dc896f32fff3
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-03-20 15:22:06 -0600

    Set build version comment to `sat-1-rc1`

--- firmware/Core/Src/telecommands/system_telecommand_defs.c
+++ firmware/Core/Src/telecommands/system_telecommand_defs.c
@@ -86,7 +86,7 @@ uint8_t TCMDEXEC_obc_firmware_version(
         __VERSION__, // "compiler"
         optimize_str, // "optimize"
         asserts_str, // "asserts" (least-useful one)
-        "dev" // "comment"
+        "sat-1-rc1" // "comment"
     );
 
     return 0;
