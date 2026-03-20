103e1203b6bc9ea24f0fd60cc122d91d16e820ea
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2026-03-20 12:48:01 -0600

    IOC: Set NVIC TIM6 priority to 3 (more urgent than all others)

--- firmware/Core/Src/stm32l4xx_hal_msp.c
+++ firmware/Core/Src/stm32l4xx_hal_msp.c
@@ -947,7 +947,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* Peripheral clock enable */
     __HAL_RCC_TIM6_CLK_ENABLE();
     /* TIM6 interrupt Init */
-    HAL_NVIC_SetPriority(TIM6_DAC_IRQn, 5, 0);
+    HAL_NVIC_SetPriority(TIM6_DAC_IRQn, 3, 0);
     HAL_NVIC_EnableIRQ(TIM6_DAC_IRQn);
   /* USER CODE BEGIN TIM6_MspInit 1 */
 
