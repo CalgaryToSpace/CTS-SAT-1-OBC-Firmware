10cf89a1f8fa81688423d9ab93f50539f3cfad06
parker-research <166864283+parker-research@users.noreply.github.com>
2026-03-22 18:14:15 -0600

    Use uwrite/ufmt for string interpolation

--- misc_tools/exec_blob/demo_rust_blob_1/src/blob_main.rs
+++ misc_tools/exec_blob/demo_rust_blob_1/src/blob_main.rs
@@ -1,9 +1,9 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write as _;
 use core::panic::PanicInfo;
 use heapless::String;
+use ufmt::uwrite;
 
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text.entry")]
@@ -12,14 +12,14 @@ pub extern "C" fn blob_main(
     response_buf: *mut u8,
     response_buf_len: u16,
 ) -> u8 {
-    let args = unsafe { core::ffi::CStr::from_ptr(args_str) }
-        .to_str()
-        .unwrap_or("<invalid utf8>");
-
-    let (output_str, return_code): (String<256>, u8) = match run(args) {
-        Ok(s) => (s, 0u8),
-        Err(s) => (s, 10u8),
-    };
+    let (output_str, return_code): (String<256>, u8) =
+        match unsafe { core::ffi::CStr::from_ptr(args_str) }.to_str() {
+            Ok(safe_args_str) => match run(safe_args_str) {
+                Ok(safe_output_str) => (safe_output_str, 0u8),
+                Err(safe_err_str) => (safe_err_str, 10u8),
+            },
+            Err(_) => (make_err("Invalid args_str - not UTF-8"), 99u8),
+        };
 
     // Copy into response_buf as a CStr, respecting response_buf_len.
     let bytes = output_str.as_bytes();
@@ -35,14 +35,14 @@ pub extern "C" fn blob_main(
 
 fn run(args: &str) -> Result<String<256>, String<256>> {
     let mut out: String<256> = String::new();
-    write!(out, "Hello world from Rust blob! args_str=<{}>", args)
-        .map_err(|_| make_err("write!"))?;
+    uwrite!(out, "Hello world from Rust blob! args_str=<{}>", args)
+        .map_err(|_| make_err("uwrite! macro failed"))?;
     Ok(out)
 }
 
 fn make_err(msg: &str) -> String<256> {
     let mut s: String<256> = String::new();
-    _ = write!(s, "ERROR: {}", msg);
+    _ = uwrite!(s, "ERROR: {}", msg);
     s
 }
 
